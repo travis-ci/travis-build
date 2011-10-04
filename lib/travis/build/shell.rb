6dbf33d12374c9e1b5a406d7d81d878382b11a2b
Sven Fuchs <svenfuchs@artweb-design.de>
2011-10-04 23:14:12 +0200

    copy Shell::Buffer, Helpers and Session from travis-worker

--- lib/travis/build/shell.rb
+++ lib/travis/build/shell.rb
@@ -2,26 +2,27 @@ module Travis
   module Build
     class Shell
       autoload :Session, 'travis/build/shell/session'
+
       attr_reader :session
 
-      def initialize(config, &block)
-        @session = Session.new(config, &block)
+      def initialize(session)
+        @session = session
       end
 
       def export(name, value)
-        session.execute("export name=value")
+        session.execute("export #{name}=#{value}")
       end
 
       def chdir(dir)
-        shell.execute("mkdir -p #{dir}; cd #{dir}", :echo => false)
+        session.execute("mkdir -p #{dir}; cd #{dir}", :echo => false)
       end
 
       def cwd
         session.evaluate('pwd').strip
       end
 
-      def file_exists?
-        session.execute("test -f #{file_name}", :echo => false)
+      def file_exists?(filename)
+        session.execute("test -f #{filename}", :echo => false)
       end
 
       def evaluate(*args)
@@ -31,8 +32,6 @@ module Travis
       def execute(*args)
         session.execute(*args)
       end
-      alias :run :execute
     end
   end
 end
-
---
+++ lib/travis/build/shell/buffer.rb
@@ -0,0 +1,46 @@
+module Travis
+  module Worker
+    module Shell
+      class Buffer < String
+        attr_reader :callback
+
+        def initialize(&callback)
+          @callback = callback
+          start
+        end
+
+        def flush
+          read.tap do |string|
+            callback.call(string) if callback
+          end if !empty?
+        end
+
+        def empty?
+          pos == length
+        end
+
+        protected
+
+          def pos
+            @pos ||= 0
+          end
+
+          def read
+            string = self[pos, length - pos]
+            @pos += string.length
+            string
+          end
+
+          def start
+            @thread = Thread.new do
+              loop do
+                flush
+                sleep(Travis::Worker.config.shell.buffer)
+              end
+            end
+          end
+      end
+    end
+  end
+end
+
