87d06ace8a4a30cc7b23a11a1c110085affe4461
Sven Fuchs <svenfuchs@artweb-design.de>
2011-10-05 06:29:10 +0200

    do not automatically include spec/support files, let's require them

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,6 @@ require 'mocha'
 require 'travis/build'
 include Travis::Build
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| load f }
-
 RSpec.configure do |config|
   config.mock_with :mocha
 end
