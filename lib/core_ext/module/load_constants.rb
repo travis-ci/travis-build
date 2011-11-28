3800f512704f0b6d70827af7e673f1d2c75ce89c
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-28 17:43:25 +0100

    fix load_constants! to use the namespaced constant name for excluding duplicates

--- lib/core_ext/module/load_constants.rb
+++ lib/core_ext/module/load_constants.rb
@@ -1,10 +1,12 @@
 class Module
   def load_constants!(loaded = [])
     constants.each do |name|
-      next if loaded.include?(name)
-      loaded << name
-      const = const_get(name)
-      const.load_constants!(loaded) if const.is_a?(Class) || const.is_a?(Module)
+      full_name = [self.name, name].join('::')
+      unless loaded.include?(full_name)
+        loaded << full_name
+        const = const_get(name)
+        const.load_constants!(loaded) if const.is_a?(Class) || const.is_a?(Module)
+      end
     end
   end
 end
