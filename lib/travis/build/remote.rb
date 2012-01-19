9605079dabad662645b3488d8e71d79fb528f6c5
Michael S. Klishin <michael@novemberain.com>
2012-01-19 22:33:03 +0400

    Initial version of the log trimmer
    We will extract a separate class that will do the trimming and will be dead easy to unit test.
    We will probably colorize the output, too. It is too early to worry about that.

--- lib/travis/build/remote.rb
+++ lib/travis/build/remote.rb
@@ -1,7 +1,10 @@
 module Travis
   class Build
     class Remote < Build
-      attr_reader :name, :vm, :shell
+      # 2 MB per discussion with svenfuchs. MK.
+      BUILD_LOG_LENGTH_LIMIT = 2 * 1024 * 1024
+
+      attr_reader :name, :vm, :shell, :output_length
 
       log_header { "#{Thread.current[:log_header]}:build:remote" }
 
@@ -10,6 +13,17 @@ module Travis
         @name = vm.name
         @vm = vm
         @shell = shell
+
+        @output_length = 0
+        @injected_log_trimming_message = false
+      end
+
+      def injected_log_trimming_message?
+        @injected_log_trimming_message
+      end
+
+      def drop_log_output?
+        @output_length >= BUILD_LOG_LENGTH_LIMIT
       end
 
       def name
@@ -18,29 +32,47 @@ module Travis
 
       protected
 
-        def perform
-          log "Using worker: #{name}\n\n"
-          result = vm.sandboxed do
-            with_shell do
-              job.run
-            end
-          end
-          log "\nDone. Build script exited with: #{result[:status]}\n"
-          result
-        end
-
-        def with_shell
-          shell.connect
-          shell.on_output(&method(:on_output))
-
-          yield.tap do
-            shell.close
+      def perform
+        log "Using worker: #{name}\n\n"
+        result = vm.sandboxed do
+          with_shell do
+            job.run
           end
         end
+        log "\nDone. Build script exited with: #{result[:status]}\n"
+        result
+      end
 
-        def on_output(output, options = {})
+      def with_shell
+        shell.connect
+        shell.on_output(&method(:on_output))
+
+        yield.tap do
+          shell.close
+        end
+      end
+
+      def inject_log_limit_message!
+        log("\n\n")
+        log("[WARNING] Build log length has exceeded the limit (2 MB). This usually means that test suite is raising the same exception over and over. Trimming all subsequent output.")
+        log("\n\n")
+
+        @injected_log_trimming_message = true
+      end
+
+      def maybe_log(output, options = {})
+        if drop_log_output?
+          inject_log_limit_message! unless injected_log_trimming_message?
+        else
           log(output, options)
         end
+
+          @output_length += output.length
+      end
+
+      def on_output(output, options = {})
+        maybe_log(output, options)
+      end
     end
   end
 end
