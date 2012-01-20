157d65c49e6daec37df3408214e3ad2c51f603ce
Michael S. Klishin <michael@novemberain.com>
2012-01-20 16:53:20 +0400

    Revisit Event spec examples

--- spec/build/event_spec.rb
+++ spec/build/event_spec.rb
@@ -15,6 +15,42 @@ describe 'Event' do
       event.name.should == 'job:test:started'
     end
 
+    it 'returns "job:test:started" for a start event on a erlang test job' do
+      job = Travis::Build::Job::Test::Erlang.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
+    it 'returns "job:test:started" for a start event on a php test job' do
+      job = Travis::Build::Job::Test::Php.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
+    it 'returns "job:test:started" for a start event on a NodeJS test job' do
+      job = Travis::Build::Job::Test::NodeJs.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
+    it 'returns "job:test:started" for a start event on a Java test job' do
+      job = Travis::Build::Job::Test::PureJava.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
+    it 'returns "job:test:started" for a start event on a Groovy test job' do
+      job = Travis::Build::Job::Test::Groovy.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
+    it 'returns "job:test:started" for a start event on a Scala test job' do
+      job = Travis::Build::Job::Test::Scala.new(nil, nil, nil)
+      event = Travis::Build::Event.new(:started, job, nil)
+      event.name.should == 'job:test:started'
+    end
+
     it 'returns "job:test:ruby:started" for a start event on a configure job' do
       job = Travis::Build::Job::Configure.new(nil, nil)
       event = Travis::Build::Event.new(:started, job, nil)
