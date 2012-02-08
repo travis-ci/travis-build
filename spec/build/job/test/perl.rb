1b1ee74f191588c815aeb5933a7170174093ef3c
Jonathan "Duke" Leto <jonathan@leto.net>
2012-02-08 12:58:47 -0800

    add some specs for perl support

---
+++ spec/build/job/test/perl.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'travis/build'
+
+describe Travis::Build::Job::Test::Perl do
+  let(:shell) { stub('shell') }
+  let(:config) { Travis::Build::Job::Test::Perl::Config.new }
+  let(:job) { Travis::Build::Job::Test::Perl.new(shell, nil, config) }
+
+  describe 'install' do
+    context "when project uses Build.PL" do
+      it 'returns "perl Build.PL && ./Build test"' do
+        job.expects(:uses_module_build?).returns(true)
+        job.expects(:uses_eumm?).returns(false)
+        job.install.should == 'perl Build.PL && ./Build test'
+      end
+    end
+    context "when project uses Makefile.PL" do
+      it 'returns "perl Makefile.PL && make test"' do
+        job.expects(:uses_module_build?).returns(false)
+        job.expects(:uses_eumm?).returns(true)
+        job.install.should == 'perl Makefile.PL && make test'
+      end
+    end
+  end
+
+end
