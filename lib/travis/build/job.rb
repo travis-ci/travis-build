1ede301f988d949373b20f84d0a8a5f7609d50e0
Michael S. Klishin <michael@novemberain.com>
2012-02-23 01:37:13 +0400

    Image is still on a topic branch, was added here too early?

--- lib/travis/build/job.rb
+++ lib/travis/build/job.rb
@@ -4,7 +4,6 @@ module Travis
   class Build
     module Job
       autoload :Configure, 'travis/build/job/configure'
-      autoload :Image,     'travis/build/job/image'
       autoload :Test,      'travis/build/job/test'
     end
   end
