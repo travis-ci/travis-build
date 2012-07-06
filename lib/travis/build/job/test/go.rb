49f663cb3404bdf33e4d66ab94b1c54ea4e06adc
Michael S. Klishin <michael@novemberain.com>
2012-07-06 17:31:20 +0400

    cd into GOPATH/src/[package] before running go get

--- lib/travis/build/job/test/go.rb
+++ lib/travis/build/job/test/go.rb
@@ -7,9 +7,10 @@ module Travis
           end
 
           def setup
+            # Here we set up GOPATH + subdirectories structure Go build tool expects
             shell.execute "mkdir -p #{gopath}/src"
             # For example, ln -s ~/builds/peterbourgon/g2g ~/gopath/src/g2g
-            shell.execute "ln -s #{home_directory}/builds/#{repository_slug} #{gopath}/src/#{repository_name}"
+            shell.execute "ln -s #{home_directory}/builds/#{repository_slug} #{go_src}"
             shell.export_line "GOPATH=#{gopath}"
           end
 
@@ -17,7 +18,7 @@ module Travis
             if uses_make?
               # no-op
             else
-              "go get -v ."
+              "cd #{go_src} && go get -v ."
             end
           end
 
@@ -37,6 +38,11 @@ module Travis
 
             def gopath
               "#{home_directory}/gopath"
+            end
+
+            # GOPATH/src/[package] location
+            def go_src
+              "#{gopath}/src/#{repository_name}"
             end
         end
       end
