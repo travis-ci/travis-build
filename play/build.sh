dfa14111d32cf120f3a73921229729215f632620
Sven Fuchs <me@svenfuchs.com>
2013-01-08 05:22:15 +0100

    update example script

--- play/build.sh
+++ play/build.sh
@@ -1,96 +1,15 @@
-    echo \#\!/usr/bin/env\ ruby'
-''
-'require\ \'net/http\''
-'require\ \'uri\''
-''
-'source,\ target,\ interval\ \=\ ARGV'
-'target\ \=\ URI.parse\(target\)'
-'interval\ \|\|\=\ 0.5'
-''
-'file\ \=\ File.open\(source,\ \'r\+\'\)'
-'buff\ \=\ \'\''
-''
-'post\ \=\ -\>\(data\)\ do'
-'\ \ Net::HTTP.post_form\(target,\ data\)'
-'end'
-''
-'at_exit\ do'
-'\ \ file.close'
-'\ \ post.call\(final:\ true,\ log:\ buff\)'
-'end'
-''
-'loop\ do'
-'\ \ buff\ \<\<\ file.getc\ until\ file.eof\?'
-'\ \ post.call\(log:\ buff\)\ unless\ buff.empty\?'
-'\ \ buff.clear'
-'\ \ sleep\ interval'
-'end'
-''
-' > ~/travis_report_log.rb
-    ruby ~/travis_report_log.rb ~/build.log http://192.168.2.100:3000/jobs/1804637/log &
-
-
-
-    echo \#\!/usr/bin/env\ ruby'
-''
-'require\ \'net/http\''
-'require\ \'uri\''
-'require\ \'json\''
-''
-'source,\ target\ \=\ ARGV'
-'target\ \=\ URI.parse\(target\)'
-''
-'file\ \=\ File.open\(source,\ \'r\+\'\)'
-'last_state,\ last_stage\ \=\ nil,\ nil'
-''
-'post\ \=\ -\>\(data\)\ do'
-'\ \ p\ data'
-'\ \ Net::HTTP.post_form\(target,\ data\)'
-'end'
-''
-'on_start\ \=\ -\>\(line\)\ do'
-'\ \ post.call\(event:\ :start,\ started_at:\ Time.now,\ worker:\ \`hostname\`\)'
-'end'
-''
-'on_finish\ \=\ -\>\(line,\ result\)\ do'
-'\ \ state\ \=\ last_state\ \=\=\ \'start\'\ \?\ :errored\ :\ \(result\ \=\=\ 0\ \?\ :passed\ :\ :failed\)'
-'\ \ data\ \=\ \{\ event:\ :finish,\ state:\ state,\ finished_at:\ Time.now\ \}'
-'\ \ data\[:error\]\ \=\ :\"\#\{last_stage\}_failed\"\ if\ state\ \=\=\ :errored'
-'\ \ post.call\(data\)'
-'end'
-''
-'report\ \=\ -\>\(line\)\ do'
-'\ \ case\ line'
-'\ \ when\ /\\\[build:start\\\]/'
-'\ \ \ \ on_start.call\(line\)'
-'\ \ when\ /\\\[build:finish\\\]\ result:\ \(\[\\d\]\+\)/'
-'\ \ \ \ on_finish.call\(line,\ \$1.to_i\)'
-'\ \ when\ /\\\[\(.\+\):\(.\+\)\\\]/'
-'\ \ \ \ last_stage,\ last_state\ \=\ \$1,\ \$2'
-'\ \ end'
-'end'
-''
-'loop\ do'
-'\ \ sleep\ 0.1\ while\ file.eof\?'
-'\ \ report.call\(file.readline\)'
-'end'
-' > ~/travis_report_state.rb
-    ruby ~/travis_report_state.rb ~/state.log http://192.168.2.100:3000/jobs/1804637/state &
-
 travis_start() {
-  echo "travis_start $1"
-  echo "`date +%s.%N` [$1:start]" >> ~/state.log
+  echo "[travis:$1:start]" >> ~/state.log
 }
 
 travis_finish() {
-  echo "travis_finish $1 ($?)"
-  echo "`date +%s.%N` [$1:finish] result: $?" >> ~/state.log
+  echo "[travis:$1:finish:result=$2]" >> ~/state.log
   sleep 1
 }
 
 travis_assert() {
   if [ $? != 0 ]; then
-    echo "Command did not exit with 0. Exiting." >> ~/build.log
+    echo "Command did not exit with 0. Exiting."
     travis_terminate 1
   fi
 }
@@ -100,7 +19,7 @@ travis_timeout() {
   local start=$(date +%s)
   while ps aux | awk '{print $2 }' | grep -q $pid 2> /dev/null; do
     if [ $(expr $(date +%s) - $start) -gt $1 ]; then
-      echo "Command timed out after $1 seconds. Exiting." >> ~/build.log
+      echo "Command timed out after $1 seconds. Exiting."
       travis_terminate 1
     fi
   done
@@ -108,8 +27,8 @@ travis_timeout() {
 }
 
 travis_terminate() {
-  travis_finish build
-  pkill -9 -P $$
+  travis_finish build $1
+  pkill -9 -P $$ > /dev/null 2>&1
   exit $1
 }
 
@@ -117,13 +36,10 @@ rm -rf   ~/build
 mkdir -p ~/build
 cd       ~/build
 
-touch ~/build.log; > ~/build.log
-touch ~/state.log; > ~/state.log
-
-trap 'travis_finish build' TERM
+trap 'travis_finish build 1' TERM
 
 travis_start build
-travis_start 'export'
+travis_start export
 TRAVIS_PULL_REQUEST=false
 TRAVIS_SECURE_ENV_VARS=false
 TRAVIS_BUILD_ID=1804636
@@ -133,74 +49,75 @@ TRAVIS_JOB_NUMBER=141.1
 TRAVIS_BRANCH=master
 TRAVIS_COMMIT=1e76ebd1108dd918bd0d17c5f241a7984292b31f
 TRAVIS_COMMIT_RANGE=ad079609a408...1e76ebd1108d
-TRAVIS_RUBY_VERSION=1.9.2
-travis_finish 'export'
+TRAVIS_RUBY_VERSION=1.9.3
+travis_finish export $?
 
-travis_start 'checkout'
+travis_start checkout
 GIT_ASKPASS=echo
-echo \$\ git\ clone\ --depth\=100\ --quiet\ git://github.com/travis-ci/travis-support.git\ . >> ~/build.log 2>&1
-((git clone --depth=100 --quiet git://github.com/travis-ci/travis-support.git .) >> ~/build.log 2>&1) &
+echo \$\ git\ clone\ --depth\=100\ --quiet\ git://github.com/travis-ci/travis-support.git\ .
+(git clone --depth=100 --quiet git://github.com/travis-ci/travis-support.git .) &
 travis_timeout 300
 travis_assert
 rm -f ~/.ssh/source_rsa
-echo \$\ git\ checkout\ -qf\ 1e76ebd1108dd918bd0d17c5f241a7984292b31f >> ~/build.log 2>&1
-(git checkout -qf 1e76ebd1108dd918bd0d17c5f241a7984292b31f) >> ~/build.log 2>&1
+echo \$\ git\ checkout\ -qf\ 1e76ebd1108dd918bd0d17c5f241a7984292b31f
+git checkout -qf 1e76ebd1108dd918bd0d17c5f241a7984292b31f
 travis_assert
 if [[ -s .gitmodules ]]; then
-  (echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config) >> ~/build.log 2>&1
-  echo \$\ git\ submodule\ init >> ~/build.log 2>&1
-  (git submodule init) >> ~/build.log 2>&1
-  echo \$\ git\ submodule\ update >> ~/build.log 2>&1
-  ((git submodule update) >> ~/build.log 2>&1) &
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+  echo \$\ git\ submodule\ init
+  git submodule init
+  echo \$\ git\ submodule\ update
+  (git submodule update) &
   travis_timeout 300
   travis_assert
 fi
-travis_finish 'checkout'
+travis_finish checkout $?
 
-travis_start 'setup'
-echo \$\ rvm\ use\ 1.9.2 >> ~/build.log 2>&1
-(rvm use 1.9.2) >> ~/build.log 2>&1
+travis_start setup
+echo \$\ rvm\ use\ 1.9.3
+rvm use 1.9.3
 travis_assert
 if [[ -f Gemfile ]]; then
   BUNDLE_GEMFILE=$pwd/Gemfile
 fi
-travis_finish 'setup'
+travis_finish setup $?
 
-travis_start 'announce'
-echo \$\ java\ -version >> ~/build.log 2>&1
-(java -version) >> ~/build.log 2>&1
-echo \$\ javac\ -version >> ~/build.log 2>&1
-(javac -version) >> ~/build.log 2>&1
-echo \$\ ruby\ --version >> ~/build.log 2>&1
-(ruby --version) >> ~/build.log 2>&1
-echo \$\ gem\ --version >> ~/build.log 2>&1
-(gem --version) >> ~/build.log 2>&1
-travis_finish 'announce'
+travis_start announce
+echo \$\ java\ -version
+java -version
+echo \$\ javac\ -version
+javac -version
+echo \$\ ruby\ --version
+ruby --version
+echo \$\ gem\ --version
+gem --version
+travis_finish announce $?
 
-travis_start 'install'
+travis_start install
 if [[ -f Gemfile ]]; then
-  echo \$\ bundle\ install\  >> ~/build.log 2>&1
-  ((bundle install ) >> ~/build.log 2>&1) &
+  echo \$\ bundle\ install\
+  (bundle install ) &
   travis_timeout 600
   travis_assert
 fi
-travis_finish 'install'
+travis_finish install $?
 
-travis_start 'script'
+travis_start script
 if [[ -f Gemfile ]]; then
-  echo \$\ bundle\ exec\ rake >> ~/build.log 2>&1
-  ((bundle exec rake) >> ~/build.log 2>&1) &
+  echo \$\ bundle\ exec\ rake
+  (bundle exec rake) &
   travis_timeout 1500
 else
-  echo \$\ rake >> ~/build.log 2>&1
-  ((rake) >> ~/build.log 2>&1) &
+  echo \$\ rake
+  (rake) &
   travis_timeout 1500
 fi
 TRAVIS_TEST_RESULT=$?
-travis_finish 'script'
+travis_finish script $TRAVIS_TEST_RESULT
 
 
 
-echo "\n\nDone. Build script exited with $TRAVIS_TEST_RESULT" >> ~/build.log
+echo
+echo "Done. Build script exited with $TRAVIS_TEST_RESULT"
 
 travis_terminate $TRAVIS_TEST_RESULT
