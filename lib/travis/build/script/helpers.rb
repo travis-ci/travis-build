464e6460a7042bc6d01809442d9975788e1f5c41
Sven Fuchs <me@svenfuchs.com>
2013-01-11 13:18:14 +0100

    make timeouts skippable by setting timeouts config to false

--- lib/travis/build/script/helpers.rb
+++ lib/travis/build/script/helpers.rb
@@ -5,7 +5,7 @@ module Travis
         Shell::Dsl.instance_methods(false).each do |name|
           define_method(name) do |*args, &block|
             options = args.last if args.last.is_a?(Hash)
-            args.last[:timeout] = data.timeout(options[:timeout]) if options && options.key?(:timeout)
+            args.last[:timeout] = data.timeouts[options[:timeout]] if options && options.key?(:timeout)
             sh.send(name, *args, &stacking(&block))
           end
         end
