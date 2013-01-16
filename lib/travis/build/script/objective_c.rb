af594f10fb37cb58413f0688ab475131488e806d
Henrik Hodne <me@henrikhodne.com>
2013-01-15 19:11:51 -0600

    First attempt at Objective-C support

---
+++ lib/travis/build/script/objective_c.rb
@@ -0,0 +1,28 @@
+module Travis
+  module Build
+    class Script
+      class ObjectiveC < Script
+        def announce
+          super
+          cmd 'xcodebuild -version -sdk'
+        end
+
+        def install
+          uses_cocoapods? 'pod install'
+        end
+
+        def script
+          workspace = config[:xcode_workspace] ? "-workspace #{config[:xcode_workspace]}" : ''
+          cmd "xcodebuild #{workspace} -scheme #{config[:xcode_scheme]} clean test"
+        end
+
+        private
+
+        def uses_cocoapods?(*args)
+          sh_if '-f Podfile', *args
+        end
+      end
+    end
+  end
+end
+
