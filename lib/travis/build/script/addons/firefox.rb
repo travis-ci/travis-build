688ab5547583f67244edb60dedb419c2d261b8ba
Henrik Hodne <me@henrikhodne.com>
2013-04-08 14:06:56 -0500

    Add {} around interpolated instance variables
    This isn't required, but it makes the strings a little bit clearer.

--- lib/travis/build/script/addons/firefox.rb
+++ lib/travis/build/script/addons/firefox.rb
@@ -10,10 +10,10 @@ module Travis
 
           def before_install
             @script.fold('install_firefox') do |script|
-              script.cmd "echo -e \"\033[33;1mInstalling Firefox v#@firefox_version\033[0m\"; ", assert: false, echo: false
+              script.cmd "echo -e \"\033[33;1mInstalling Firefox v#{@firefox_version}\033[0m\"; ", assert: false, echo: false
               script.cmd "sudo mkdir -p /usr/local/firefox", assert: false
               script.cmd "sudo chown travis /usr/local/firefox", assert: false
-              script.cmd "wget -O /tmp/firefox.tar.bz2 ftp://ftp.mozilla.org/pub/firefox/releases/#@firefox_version/linux-x86_64/en-US/firefox-#@firefox_version.tar.bz2", assert: false
+              script.cmd "wget -O /tmp/firefox.tar.bz2 ftp://ftp.mozilla.org/pub/firefox/releases/#{@firefox_version}/linux-x86_64/en-US/firefox-#{@firefox_version}.tar.bz2", assert: false
               script.cmd "pushd /usr/local/firefox", assert: false
               script.cmd "tar xf /tmp/firefox.tar.bz2", assert: false
               script.cmd "sudo ln -s /usr/local/firefox/firefox /usr/local/bin/firefox", assert: false
