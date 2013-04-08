6ff9c84f07cdda8d9c09687f0085deb0808fb539
Henrik Hodne <me@henrikhodne.com>
2013-04-08 02:01:36 -0500

    Install Firefox to a system-wide location

--- lib/travis/build/script/addons/firefox.rb
+++ lib/travis/build/script/addons/firefox.rb
@@ -10,13 +10,13 @@ module Travis
 
           def before_install
             @script.fold('install_firefox') do |script|
-              script.cmd "echo -e \"\033[33;1mInstalling Firefox v20.0\033[0m\"; ", assert: false, echo: false
-              script.cmd "mkdir -p ~/usr/firefox ~/bin", assert: false
-              script.cmd "wget -O /tmp/firefox.tar.bz2 ftp://ftp.mozilla.org/pub/firefox/releases/20.0/linux-x86_64/en-US/firefox-20.0.tar.bz2", assert: false
-              script.cmd "pushd ~/usr/firefox", assert: false
+              script.cmd "echo -e \"\033[33;1mInstalling Firefox v#@firefox_version\033[0m\"; ", assert: false, echo: false
+              script.cmd "sudo mkdir -p /usr/local/firefox", assert: false
+              script.cmd "sudo chown travis /usr/local/firefox", assert: false
+              script.cmd "wget -O /tmp/firefox.tar.bz2 ftp://ftp.mozilla.org/pub/firefox/releases/#@firefox_version/linux-x86_64/en-US/firefox-#@firefox_version.tar.bz2", assert: false
+              script.cmd "pushd /usr/local/firefox", assert: false
               script.cmd "tar xf /tmp/firefox.tar.bz2", assert: false
-              script.cmd "sudo ln -s ~/usr/firefox/firefox ~/bin/firefox", assert: false
-              script.set 'PATH', '~/bin:$PATH', echo: false, assert: false
+              script.cmd "sudo ln -s /usr/local/firefox/firefox /usr/local/bin/firefox", assert: false
               script.cmd "popd", assert: false
             end
           end
--- spec/script/addons/firefox_spec.rb
+++ spec/script/addons/firefox_spec.rb
@@ -12,12 +12,12 @@ describe Travis::Build::Script::Addons::Firefox do
   it 'runs the command' do
     script.expects(:fold).with('install_firefox').yields(script)
     script.expects(:cmd).with("echo -e \"\033[33;1mInstalling Firefox v20.0\033[0m\"; ", assert: false, echo: false)
-    script.expects(:cmd).with("mkdir -p ~/usr/firefox ~/bin", assert: false)
+    script.expects(:cmd).with("sudo mkdir -p /usr/local/firefox", assert: false)
+    script.expects(:cmd).with("sudo chown travis /usr/local/firefox", assert: false)
     script.expects(:cmd).with("wget -O /tmp/firefox.tar.bz2 ftp://ftp.mozilla.org/pub/firefox/releases/20.0/linux-x86_64/en-US/firefox-20.0.tar.bz2", assert: false)
-    script.expects(:cmd).with("pushd ~/usr/firefox", assert: false)
+    script.expects(:cmd).with("pushd /usr/local/firefox", assert: false)
     script.expects(:cmd).with("tar xf /tmp/firefox.tar.bz2", assert: false)
-    script.expects(:cmd).with("sudo ln -s ~/usr/firefox/firefox ~/bin/firefox", assert: false)
-    script.expects(:set).with('PATH', '~/bin:$PATH', echo: false, assert: false)
+    script.expects(:cmd).with("sudo ln -s /usr/local/firefox/firefox /usr/local/bin/firefox", assert: false)
     script.expects(:cmd).with("popd", assert: false)
     subject
   end
