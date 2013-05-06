f049588299de45dc459ff362c57f472cdc8a686a
Henrik Hodne <me@henrikhodne.com>
2013-05-06 03:33:32 -0500

    Insert appropriate amount of backslashes

--- spec/script/addons/hosts_spec.rb
+++ spec/script/addons/hosts_spec.rb
@@ -9,8 +9,8 @@ describe Travis::Build::Script::Addons::Hosts do
   subject { described_class.new(script, config).setup }
 
   it "runs the commands" do
-    script.expects(:cmd).with("sudo sed -e 's/^\(127.0.0.1.*\)$/\\1 #{config}/' -i '' /etc/hosts")
-    script.expects(:cmd).with("sudo sed -e 's/^\(::1.*\)$/\\1 #{config}/' -i '' /etc/hosts")
+    script.expects(:cmd).with("sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 #{config}/' -i '' /etc/hosts")
+    script.expects(:cmd).with("sudo sed -e 's/^\\(::1.*\\)$/\\1 #{config}/' -i '' /etc/hosts")
     subject
   end
 
@@ -18,8 +18,8 @@ describe Travis::Build::Script::Addons::Hosts do
     let(:config) { %w[johndoe.local example.local] }
 
     it "runs the command" do
-      script.expects(:cmd).with("sudo sed -e 's/^\(127.0.0.1.*\)$/\\1 johndoe.local example.local/' -i '' /etc/hosts")
-      script.expects(:cmd).with("sudo sed -e 's/^\(::1.*\)$/\\1 johndoe.local example.local/' -i '' /etc/hosts")
+      script.expects(:cmd).with("sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 johndoe.local example.local/' -i '' /etc/hosts")
+      script.expects(:cmd).with("sudo sed -e 's/^\\(::1.*\\)$/\\1 johndoe.local example.local/' -i '' /etc/hosts")
 
       subject
     end
