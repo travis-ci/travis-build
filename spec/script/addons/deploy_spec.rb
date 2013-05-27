b3ee98911cca7e4be11ee191f4ff354cefaf795b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-05-27 16:49:12 +0200

    add spec

---
+++ spec/script/addons/deploy_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Travis::Build::Script::Addons::Deploy do
+  let(:config) {{ provider: "heroku", password: 'foo', email: 'user@host' }}
+  let(:script) { stub('script') }
+  subject { described_class.new(script, config) }
+
+  it 'runs the command' do
+    script.expects(:if).with('($TRAVIS_PULL_REQUEST = false) && ($TRAVIS_BRANCH = master)').yields(script)
+    script.expects(:cmd).with('gem install dpl', assert: true, echo: false)
+    script.expects(:cmd).with('dpl --provider="heroku" --password="foo" --email="user@host"', assert: true, echo: false)
+    subject.after_success
+  end
+end
