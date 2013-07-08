a8a1e51d85b5d46771cb4d5437ec63b4fbb95915
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-07-08 11:24:16 +0200

    use after_script and check $TRAVIS_TEST_RESULT

--- lib/travis/build/script/addons/deploy.rb
+++ lib/travis/build/script/addons/deploy.rb
@@ -12,7 +12,7 @@ module Travis
             @config = config.respond_to?(:to_hash) ? config.to_hash : {}
           end
 
-          def after_success
+          def after_script
             script.if(want) { run }
           end
 
@@ -21,8 +21,12 @@ module Travis
               on          = config.delete(:on) || config.delete(:true) || {}
               on          = { branch: on.to_str } if on.respond_to? :to_str
               on[:ruby] ||= on[:rvm] if on.include? :rvm
-              conditions  = [ want_push(on), want_repo(on), want_branch(on), want_runtime(on) ]
+              conditions  = [ want_success(on), want_push(on), want_repo(on), want_branch(on), want_runtime(on) ]
               conditions.flatten.compact.map { |c| "(#{c})" }.join(" && ")
+            end
+
+            def want_success(on)
+              '$TRAVIS_TEST_RESULT = 0'
             end
 
             def want_push(on)
