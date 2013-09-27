733ac98fc78a49077b48a94ed7a93192306f67db
Gilles Cornu <git@gilles.cornu.name>
2013-09-27 20:38:06 +0200

    PostgreSQL Addon: Rename fold as 'postgresql'

--- lib/travis/build/script/addons/postgresql.rb
+++ lib/travis/build/script/addons/postgresql.rb
@@ -9,7 +9,7 @@ module Travis
           end
 
           def before_install
-            @script.fold('restart_postgresql') do |script|
+            @script.fold('postgresql') do |script|
               script.cmd "echo -e \"\033[33;1mStart PostgreSQL v#{@postgresql_version}\033[0m\"; ", assert: false, echo: false
               script.cmd "sudo service postgresql stop", assert: false
               script.cmd "sudo service postgresql start #{@postgresql_version}", assert: false
--- spec/script/addons/postgresql_spec.rb
+++ spec/script/addons/postgresql_spec.rb
@@ -10,7 +10,7 @@ describe Travis::Build::Script::Addons::Postgresql do
   let(:config) { '9.3' }
 
   it 'runs the command' do
-    script.expects(:fold).with('restart_postgresql').yields(script)
+    script.expects(:fold).with('postgresql').yields(script)
     script.expects(:cmd).with("echo -e \"\033[33;1mStart PostgreSQL v9.3\033[0m\"; ", assert: false, echo: false)
     script.expects(:cmd).with("sudo service postgresql stop", assert: false)
     script.expects(:cmd).with("sudo service postgresql start 9.3", assert: false)
