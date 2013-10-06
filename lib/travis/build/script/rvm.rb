1872d98f871b5bfd063f67d46a582eba995d6f05
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-10-07 00:33:28 +0200

    add custom bucket for binary installs

--- lib/travis/build/script/rvm.rb
+++ lib/travis/build/script/rvm.rb
@@ -2,6 +2,12 @@ module Travis
   module Build
     class Script
       module RVM
+        USER_DB = %w[
+          rvm_remote_server_url3=https://s3.amazonaws.com/travis-rubies
+          rvm_remote_server_path3=binary
+          rvm_remote_server_verify_downloads3=1
+        ].join("\n")
+
         def cache_slug
           super << "--rvm-" << ruby_version.to_s
         end
@@ -13,6 +19,8 @@ module Travis
 
         def setup
           super
+          cmd "echo '#{USER_DB}' > $rvm_path/user/db", echo: false
+          cmd "rvm remove #{ruby_version} 2>/dev/null >/dev/null", echo: false, assert: false if ruby_version =~ /ruby-head/
           cmd "rvm use #{ruby_version} --install --binary --fuzzy"
         end
 
