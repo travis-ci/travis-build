7725ab96cb14788748a524cd609c1d2d594b9a54
Brian Shirai <brixen@gmail.com>
2013-11-20 15:41:13 -0800

    Removed rbx mapping.

--- lib/travis/build/script/rvm.rb
+++ lib/travis/build/script/rvm.rb
@@ -46,9 +46,7 @@ module Travis
 
         def ruby_version
           config[:rvm].to_s.
-            gsub(/-(1[89]|2[01])mode$/, '-d\1').
-            gsub(/^rbx$/, 'rbx-weekly-d18').
-            gsub(/^rbx-d(\d{2})$/, 'rbx-weekly-d\1')
+            gsub(/-(1[89]|2[01])mode$/, '-d\1')
         end
       end
     end
