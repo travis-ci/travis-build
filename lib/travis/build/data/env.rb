7851a7f0408e568317c42e6efd31f6ff1f4b30d1
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-01-31 14:51:41 +1300

    remove the commit message for the time being utf-8 chars seem to interfere with the build script execution, causing builds to hang https://github.com/travis-ci/travis-ci/issues/1915

--- lib/travis/build/data/env.rb
+++ lib/travis/build/data/env.rb
@@ -31,7 +31,6 @@ module Travis
               TRAVIS_BRANCH:          job[:branch].shellescape,
               TRAVIS_COMMIT:          job[:commit],
               TRAVIS_COMMIT_RANGE:    job[:commit_range],
-              TRAVIS_COMMIT_MESSAGE:  job[:commit_message].shellescape,
               TRAVIS_REPO_SLUG:       repository[:slug].shellescape,
               TRAVIS_OS_NAME:         job[:os],
             )
