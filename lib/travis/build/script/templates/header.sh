be7cb9b286f5fb4f5d1064f7e2a6b7b86527099b
Hiro Asari <asari.ruby@gmail.com>
2014-02-11 09:22:32 -0500

    Add travis_wait
    Make it easier to run a task that is expected to run for a long
    time without output.
    
    The idea is to spawn a child process which writes to STDOUT to
    prevent the process from getting killed.

--- lib/travis/build/script/templates/header.sh
+++ lib/travis/build/script/templates/header.sh
@@ -31,6 +31,44 @@ travis_terminate() {
   exit $1
 }
 
+travis_wait() {
+  local cmd="$@"
+  local log_file=travis_wait_$$.log
+
+  $cmd 2>&1 >$log_file &
+  local cmd_pid=$!
+
+  travis_jigger $! $cmd &
+  local jigger_pid=$!
+  local result
+
+  {
+    wait $cmd_pid 2>/dev/null
+    result=$?
+    ps -p$jigger_pid 2>&1>/dev/null && kill $jigger_pid
+  } || exit 1
+
+  exit $result
+}
+
+travis_jigger() {
+  # helper method for travis_wait()
+  local timeout=20 # in minutes
+  local count=0
+
+  local cmd_pid=$1
+  shift
+
+  while [ $count -lt $timeout ]; do
+    count=$(($count + 1))
+    echo "Still running ($count of $timeout): $@"
+    sleep 60
+  done
+
+  echo -e "\n\033[31;1mTimeout reached. Terminating $@\033[0m\n"
+  kill -9 $cmd_pid
+}
+
 travis_retry() {
   local result=0
   local count=1
