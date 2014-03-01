8d93b44c665bd80c56e2d199098b320c12d41aa0
Henrik Hodne <henrik@hodne.io>
2014-02-28 23:13:43 -0600

    feat(api): add authentication
    While there's nothing sensitive being provided by the API right now,
    this effectively enforces that this is a private API and that it might
    change at any time.

--- lib/travis/build/app.rb
+++ lib/travis/build/app.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 
 require 'travis/build'
 
+before do
+  return if ENV['API_TOKEN'].nil? || ENV['API_TOKEN'].empty?
+
+  type, token = env['HTTP_AUTHORIZATION'].to_s.split(' ', 2)
+
+  unless type == 'token' && token == ENV['API_TOKEN']
+    halt 403, 'access denied'
+  end
+end
+
 post '/script' do
   data = JSON.parse(request.body.read)
 
