a5d15a6f9fc8375ac1ac6f1b6b10b8d75c430fe7
Sven Fuchs <me@svenfuchs.com>
2014-03-13 21:08:03 +0100

    fix fix_etc_hosts to work on both linux and mac os x

--- lib/travis/build/script/templates/header/fix_etc_hosts.sh
+++ lib/travis/build/script/templates/header/fix_etc_hosts.sh
@@ -1,1 +1,6 @@
-sudo sed -e 's/^\(127\.0\.0\.1.*\)$/\1 '`hostname`'/' -i /etc/hosts
+if sed --version 2> /dev/null | grep GNU > /dev/null; then
+  sed_inline="-i"
+else
+  sed_inline="-i ''"
+fi
+sudo sed -e 's/^\(127\.0\.0\.1.*\)$/\1 '`hostname`'/' $sed_inline /etc/hosts
