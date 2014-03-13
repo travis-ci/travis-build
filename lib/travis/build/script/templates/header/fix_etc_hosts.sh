4ded9e2aaf3f19fa53dbf65414b03682e89fc4e6
Sven Fuchs <me@svenfuchs.com>
2014-03-13 21:14:00 +0100

    shorten fix_etc_hosts.sh

--- lib/travis/build/script/templates/header/fix_etc_hosts.sh
+++ lib/travis/build/script/templates/header/fix_etc_hosts.sh
@@ -1,6 +1,2 @@
-if sed --version 2> /dev/null | grep GNU > /dev/null; then
-  sed_inline="-i"
-else
-  sed_inline="-i ''"
-fi
-sudo sed -e 's/^\(127\.0\.0\.1.*\)$/\1 '`hostname`'/' $sed_inline /etc/hosts
+sudo sed -e 's/^\(127\.0\.0\.1.*\)$/\1 '`hostname`'/' -i.bak /etc/hosts
+rm /etc/hosts.bak
