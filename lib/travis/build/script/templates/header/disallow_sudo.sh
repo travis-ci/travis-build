6d6af2de43e059a45d3d29c98d9849b31fc69f5e
Sven Fuchs <me@svenfuchs.com>
2014-03-14 16:25:25 +0100

    remove the %sudo group setting, too, just to be sure

--- lib/travis/build/script/templates/header/disallow_sudo.sh
+++ lib/travis/build/script/templates/header/disallow_sudo.sh
@@ -1,4 +1,7 @@
+sudo sed -e 's/^%sudo.*//' -i.bak /etc/sudoers
+sudo rm /etc/sudoers.bak
 sudo rm -f /etc/sudoers.d/travis
+
 if sudo ls > /dev/null 2>&1; then
   echo "Failed to remove sudo access."
   travis_terminate 2
