0f12177a4bec6c71c0b17a66401efea3ce7a99a1
Sven Fuchs <me@svenfuchs.com>
2014-03-15 22:55:50 +0100

    fix sed regexes for hosts addon bin

--- lib/travis/build/script/templates/install/hosts.sh
+++ lib/travis/build/script/templates/install/hosts.sh
@@ -3,6 +3,6 @@ sudo su -c "cat > <%= Addons::BIN_PATH %>/travis-<%= File.basename(filename, '.s
 
 hosts=$@
 
-sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 $hosts/' -i.bak /etc/hosts
-sed -e 's/^\\(::1.*\\)$/\\1 $hosts/' -i.bak /etc/hosts
+sed -e 's/^\(127\.0\.0\.1.*\)$/\1 '"$hosts"'/' -i.bak /etc/hosts
+sed -e 's/^\(::1.*\)$/\1 '"$hosts"'/' -i.bak /etc/hosts
 sh
