9f7d2cfdfd126370e11e41719102123a74f8b807
Avrohom Katz <iambpentameter@gmail.com>
2014-04-09 21:51:47 -0400

    Naive cocoapods caching implementation
    - This is a straight copy of the ruby bundler code
    - It also allows caching bundler in obj-c projects if you want
    - cocoapods doesn't support --deployment for obvious reasons
    
    I think long term it makes sense to modularize the caching code but I
    couldn't come up with a decent abstraction trivially and I really miss
    the caching having moved to an ios project from ruby

--- lib/travis/build/script/objective_c.rb
+++ lib/travis/build/script/objective_c.rb
@@ -5,16 +5,27 @@ module Travis
     class Script
       class ObjectiveC < Script
         DEFAULTS = {
-          rvm:     'default'
+          rvm:     'default',
+          gemfile: 'Gemfile',
+          podfile: 'Podfile'
         }
 
         include RVM
+
+        def cache_slug
+          # ruby version is added by RVM]
+          super << "--podfile-" << config[:podfile].to_s
+        end
+
+        def use_directory_cache?
+          super or data.cache?(:bundler) or data.cache?(:cocoapods)
+        end
 
         def announce
           super
           cmd 'xcodebuild -version -sdk', fold: 'announce'
           uses_rubymotion? then: 'motion --version'
-          has_podfile? then: 'pod --version'
+          podfile? then: 'pod --version'
         end
 
         def export
@@ -35,8 +46,24 @@ module Travis
         end
 
         def install
-          has_gemfile? then: 'bundle install', fold: 'install.bundler', retry: true
-          has_podfile? then: 'pod install', fold: 'install.cocoapods', retry: true
+          gemfile? do |sh|
+            sh.if "-f #{config[:gemfile]}.lock" do |sub|
+              directory_cache.add(sub, bundler_path) if data.cache? :bundler
+              sub.cmd bundler_command("--deployment"), fold: 'install.bundler', retry: true
+            end
+
+            sh.else do |sub|
+              # cache bundler if it has been explicitely enabled
+              directory_cache.add(sub, bundler_path) if data.cache? :bundler, false
+              sub.cmd bundler_command, fold: 'install.bundler', retry: true
+            end
+          end
+
+          podfile? do |sh|
+            # cache cocoapods if it has been explicitely enabled
+            directory_cache.add(sh, cocoapods_path) if data.cache? :cocoapods, false
+            sh.cmd cocoapods_command, fold: 'install.cocoapods', retry: true
+          end
         end
 
         def script
@@ -55,12 +82,46 @@ module Travis
 
         private
 
-        def has_podfile?(*args)
-          self.if '-f Podfile', *args
+        def bundler_args_path
+          args = Array(bundler_args).join(" ")
+          path = args[/--path[= ](\S+)/, 1]
+          path ||= 'vendor/bundle' if args.include?('--deployment')
+          path
         end
 
-        def has_gemfile?(*args)
-          self.if '-f Gemfile', *args
+        def bundler_path
+          bundler_args_path || "${BUNDLE_PATH:-vendor/bundle}"
+        end
+
+        def bundler_args
+          config[:bundler_args]
+        end
+
+        def bundler_command(args = nil)
+          args = bundler_args if bundler_args
+          args = [args].flatten << "--path=#{bundler_path}" if data.cache?(:bundler) and !bundler_args_path
+          ["bundle install", *args].compact.join(" ")
+        end
+
+        def gemfile?(*args, &block)
+          self.if "-f #{config[:gemfile]}", *args, &block
+        end
+
+        def cocoapods_path
+          "${POD_PATH:-Pods}"
+        end
+
+        def cocoapods_args
+          config[:cocoapods_args]
+        end
+
+        def cocoapods_command(args = nil)
+          args = cocoapods_args if cocoapods_args
+          ["pod install", *args].compact.join(" ")
+        end
+
+        def podfile?(*args, &block)
+          self.if "-f #{config[:podfile]}", *args, &block
         end
 
         def uses_rubymotion?(*args)
