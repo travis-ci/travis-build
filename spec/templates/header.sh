e00bd8248c4f55bcf2425ab55dfa91bd1623173d
Mathias Meyer <meyer@paperplanes.de>
2014-05-02 18:09:54 +0200

    Stub chruby for tests.

--- spec/templates/header.sh
+++ spec/templates/header.sh
@@ -21,6 +21,7 @@ stubs=(
   curl cp bash mv tar
   /Users/travis/travis-utils/osx-cibuild.sh xcodebuild pod motion xctool osascript
   sudo
+  chruby
 )
 for stub in ${stubs[*]}; do
   eval "$stub() { builtin echo $stub \$@; }"
