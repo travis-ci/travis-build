cc6f8c948e40661092538976826918d4e68db980
Dan Buch <d.buch@modcloth.com>
2014-05-06 10:58:49 -0700

    And now bumping to go1.2.2 since it's out :smiley_cat:

--- lib/travis/build/script/go.rb
+++ lib/travis/build/script/go.rb
@@ -4,7 +4,7 @@ module Travis
       class Go < Script
         DEFAULTS = {
           gobuild_args: '-v',
-          go: '1.2.1'
+          go: '1.2.2'
         }
 
         def cache_slug
@@ -57,7 +57,7 @@ module Travis
             version = config[:go].to_s
             case version
             when '1'
-              'go1.2.1'
+              'go1.2.2'
             when '1.0'
               'go1.0.3'
             when /^[0-9]\.[0-9\.]+/
