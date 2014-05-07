5d2959af4199673b8874792a80c7d3baa3133684
Henrik Hodne <henrik@travis-ci.org>
2014-05-07 11:44:20 -0500

    feat(api): move API to a class using Sinatra::Base

--- lib/travis/build/app.rb
+++ lib/travis/build/app.rb
@@ -1,21 +1,27 @@
 require 'json'
-require 'sinatra'
-
+require 'sinatra/base'
 require 'travis/build'
 
-before do
-  return if ENV['API_TOKEN'].nil? || ENV['API_TOKEN'].empty?
+module Travis
+  module Build
+    class App < Sinatra::Base
+      before do
+        return if ENV["API_TOKEN"].nil? || ENV["API_TOKEN"].empty?
 
-  type, token = env['HTTP_AUTHORIZATION'].to_s.split(' ', 2)
+        type, token = env["HTTP_AUTHORIZATION"].to_s.split(" ", 2)
 
-  unless type == 'token' && token == ENV['API_TOKEN']
-    halt 403, 'access denied'
+        unless type == "token" && token == ENV["API_TOKEN"]
+          halt 403, "access denied"
+        end
+      end
+
+      post "/script" do
+        data = JSON.parse(request.body.read)
+
+        content_type :txt
+        Travis::Build.script(data, logs: { build: false, state: true }).compile
+      end
+    end
   end
 end
 
-post '/script' do
-  data = JSON.parse(request.body.read)
-
-  content_type :txt
-  Travis::Build.script(data, logs: { build: false, state: true }).compile
-end
