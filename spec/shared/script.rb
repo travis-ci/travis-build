a8d2f6129a85d23ac49a0385aa3c6f990d4c9099
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-05-15 14:27:12 +1200

    oops, should be ‘paranoid’

--- spec/shared/script.rb
+++ spec/shared/script.rb
@@ -105,7 +105,7 @@ shared_examples_for 'a build script' do
   end
 
   it "removes access to sudo if in paranoid mode" do
-    data['skip_etc_hosts_fix'] = true
+    data['paranoid'] = true
     subject.should include(%Q{sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"})
   end
 
