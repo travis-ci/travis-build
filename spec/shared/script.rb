ff21274240e44a6fd2449b2c8d725033f4c5e328
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-05-15 14:09:25 +1200

    add some simple tests for paranoid mode

--- spec/shared/script.rb
+++ spec/shared/script.rb
@@ -100,6 +100,15 @@ shared_examples_for 'a build script' do
     subject.should_not include(%Q{sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 '`hostname`'/' -i'.bak' /etc/hosts})
   end
 
+  it "does not remove access to sudo by default" do
+    subject.should_not include(%Q{sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"})
+  end
+
+  it "removes access to sudo if in paranoid mode" do
+    data['skip_etc_hosts_fix'] = true
+    subject.should include(%Q{sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"})
+  end
+
   describe "result" do
     before do
       data['config']['.result'] = result
