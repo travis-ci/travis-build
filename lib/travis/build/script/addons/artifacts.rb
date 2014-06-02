66faa3a5efeb565c25eed442c583f790ca03fb89
Dan Buch <d.buch@modcloth.com>
2014-06-02 10:05:43 -0400

    Correcting artifacts install script to work os OSX as well

--- lib/travis/build/script/addons/artifacts.rb
+++ lib/travis/build/script/addons/artifacts.rb
@@ -80,9 +80,14 @@ module Travis
           def install_script
             @install_script ||= <<-EOF.gsub(/^\s{14}/, '')
               ARTIFACTS_DEST=$HOME/bin/artifacts
+              OS=$(uname | tr '[:upper:]' '[:lower:]')
+              ARCH=$(uname -m)
+              if [[ $ARCH == x86_64 ]] ; then
+                ARCH=amd64
+              fi
               mkdir -p $(dirname "$ARTIFACTS_DEST")
               curl -sL -o "$ARTIFACTS_DEST" \
-                https://s3.amazonaws.com/meatballhat/artifacts/stable/build/linux/amd64/artifacts
+                https://s3.amazonaws.com/meatballhat/artifacts/stable/build/$OS/$ARCH/artifacts
               chmod +x "$ARTIFACTS_DEST"
               PATH="$(dirname "$ARTIFACTS_DEST"):$PATH" artifacts -v
             EOF
