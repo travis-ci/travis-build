cfce2c658bbc7cc95e2d042dbc88c6a91f5607d2
Hiro Asari <asari.ruby@gmail.com>
2014-06-16 20:30:12 -0400

    Make --depth flag optional for 'git submodule update'
    The opts path `git.submodules_depth` is up for debate

--- lib/travis/build/script/git.rb
+++ lib/travis/build/script/git.rb
@@ -84,9 +84,12 @@ module Travis
 
           def submodules
             self.if '-f .gitmodules' do
+              if config[:git][:submodules_depth]
+                depth_opt = " --depth=#{config[:git][:submodules_depth]}"
+              end
               cmd 'echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config', echo: false
               cmd 'git submodule init', fold: "git.#{next_git_fold_number}"
-              cmd "git submodule update --depth=#{config[:git][:depth]}", assert: true, fold: "git.#{next_git_fold_number}", retry: true
+              cmd "git submodule update #{depth_opt}", assert: true, fold: "git.#{next_git_fold_number}", retry: true
             end
           end
 
