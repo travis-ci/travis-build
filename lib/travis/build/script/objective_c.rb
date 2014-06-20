752c29339e9d38ac76b6e00dbf52b93dee6200ce
Henrik Hodne <henrik@hodne.io>
2014-06-20 06:07:03 -0500

    refactor out common bundler logic

--- lib/travis/build/script/objective_c.rb
+++ lib/travis/build/script/objective_c.rb
@@ -6,19 +6,14 @@ module Travis
       class ObjectiveC < Script
         DEFAULTS = {
           rvm:     'default',
-          gemfile: 'Gemfile',
-          podfile: 'Podfile'
+          gemfile: 'Gemfile'
         }
 
         include RVM
-
-        def cache_slug
-          # ruby version is added by RVM]
-          super << "--podfile-" << config[:podfile].to_s
-        end
+        include Bundler
 
         def use_directory_cache?
-          super or data.cache?(:bundler) or data.cache?(:cocoapods)
+          super || data.cache?(:cocoapods)
         end
 
         def announce
@@ -46,23 +41,12 @@ module Travis
         end
 
         def install
-          gemfile? do |sh|
-            sh.if "-f #{config[:gemfile]}.lock" do |sub|
-              directory_cache.add(sub, bundler_path) if data.cache? :bundler
-              sub.cmd bundler_command("--deployment"), fold: 'install.bundler', retry: true
-            end
-
-            sh.else do |sub|
-              # cache bundler if it has been explicitely enabled
-              directory_cache.add(sub, bundler_path) if data.cache? :bundler, false
-              sub.cmd bundler_command, fold: 'install.bundler', retry: true
-            end
-          end
+          super
 
           podfile? do |sh|
             # cache cocoapods if it has been explicitely enabled
-            directory_cache.add(sh, cocoapods_path) if data.cache? :cocoapods, false
-            sh.cmd cocoapods_command, fold: 'install.cocoapods', retry: true
+            directory_cache.add(sh, 'Pods') if data.cache? :cocoapods, false
+            sh.cmd 'pod install', fold: 'install.cocoapods', retry: true
           end
         end
 
@@ -82,46 +66,8 @@ module Travis
 
         private
 
-        def bundler_args_path
-          args = Array(bundler_args).join(" ")
-          path = args[/--path[= ](\S+)/, 1]
-          path ||= 'vendor/bundle' if args.include?('--deployment')
-          path
-        end
-
-        def bundler_path
-          bundler_args_path || "${BUNDLE_PATH:-vendor/bundle}"
-        end
-
-        def bundler_args
-          config[:bundler_args]
-        end
-
-        def bundler_command(args = nil)
-          args = bundler_args if bundler_args
-          args = [args].flatten << "--path=#{bundler_path}" if data.cache?(:bundler) and !bundler_args_path
-          ["bundle install", *args].compact.join(" ")
-        end
-
-        def gemfile?(*args, &block)
-          self.if "-f #{config[:gemfile]}", *args, &block
-        end
-
-        def cocoapods_path
-          "${POD_PATH:-Pods}"
-        end
-
-        def cocoapods_args
-          config[:cocoapods_args]
-        end
-
-        def cocoapods_command(args = nil)
-          args = cocoapods_args if cocoapods_args
-          ["pod install", *args].compact.join(" ")
-        end
-
         def podfile?(*args, &block)
-          self.if "-f #{config[:podfile]}", *args, &block
+          self.if "-f Podfile", *args, &block
         end
 
         def uses_rubymotion?(*args)
