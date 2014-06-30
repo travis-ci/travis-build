92b49aad10b3feb6b7b558e98ca8bbbc0459928c
Henrik Hodne <henrik@travis-ci.com>
2014-06-30 08:24:58 -0500

    api: pull configuration from environment variables

--- lib/travis/build/app.rb
+++ lib/travis/build/app.rb
@@ -21,10 +21,40 @@ module Travis
       end
 
       post "/script" do
-        data = JSON.parse(request.body.read)
+        payload = JSON.parse(request.body.read)
+
+        config = { hosts: {} }
+        config[:hosts][:npm_cache] = ENV["NPM_CACHE_HOST"] if ENV.key?("NPM_CACHE_HOST")
+        config[:hosts][:apt_cache] = ENV["APT_CACHE_HOST"] if ENV.key?("APT_CACHE_HOST")
+        config[:paranoid] = true if ENV["PARANOID_MODE"] == "true"
+        config[:skip_resolv_updates] = true if ENV["SKIP_RESOLV_UPDATES"] == "true"
+        config[:skip_etc_hosts_fix] = true if ENV["SKIP_ETC_HOSTS_FIX"] == "true"
 
         content_type :txt
-        Travis::Build.script(data).compile
+        Travis::Build.script(payload.merge(config)).compile
+      end
+
+      def config
+        config = { hosts: {}, cache_options: {} }
+        config[:hosts][:npm_cache] = ENV["NPM_CACHE_HOST"] if ENV.key?("NPM_CACHE_HOST")
+        config[:hosts][:apt_cache] = ENV["APT_CACHE_HOST"] if ENV.key?("APT_CACHE_HOST")
+        config[:paranoid] = true if ENV["PARANOID_MODE"] == "true"
+        config[:skip_resolv_updates] = true if ENV["SKIP_RESOLV_UPDATES"] == "true"
+        config[:skip_etc_hosts_fix] = true if ENV["SKIP_ETC_HOSTS_FIX"] == "true"
+
+        config[:cache_options][:fetch_timeout] = ENV["CACHE_FETCH_TIMEOUT"] if ENV.key?("CACHE_FETCH_TIMEOUT")
+        config[:cache_options][:push_timeout] = ENV["CACHE_FETCH_TIMEOUT"] if ENV.key?("CACHE_FETCH_TIMEOUT")
+        if ENV["CACHE_TYPE"] == "s3"
+          config[:cache_options][:type] = "s3"
+          config[:cache_options][:s3] = {}
+          config[:cache_options][:s3][:access_key_id] = ENV.fetch("CACHE_S3_ACCESS_KEY_ID")
+          config[:cache_options][:s3][:secret_access_key] = ENV.fetch("CACHE_S3_SECRET_ACCESS_KEY")
+          config[:cache_options][:s3][:bucket] = ENV.fetch("CACHE_S3_BUCKET")
+          config[:cache_options][:s3][:scheme] = ENV["CACHE_S3_SCHEME"] if ENV.key?("CACHE_S3_SCHEME")
+          config[:cache_options][:s3][:region] = ENV["CACHE_S3_REGION"] if ENV.key?("CACHE_S3_REGION")
+        end
+
+        config
       end
     end
   end
