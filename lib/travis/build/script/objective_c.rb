e5553110116f026075d47dfd922c5b7427539dc7
Henrik Hodne <henrik@travis-ci.com>
2014-07-01 02:18:31 -0500

    Convert to string before shellescaping

--- lib/travis/build/script/objective_c.rb
+++ lib/travis/build/script/objective_c.rb
@@ -20,10 +20,10 @@ module Travis
         def export
           super
 
-          set 'TRAVIS_XCODE_SDK', config[:xcode_sdk].shellescape, echo: false
-          set 'TRAVIS_XCODE_SCHEME', config[:xcode_scheme].shellescape, echo: false
-          set 'TRAVIS_XCODE_PROJECT', config[:xcode_project].shellescape, echo: false
-          set 'TRAVIS_XCODE_WORKSPACE', config[:xcode_workspace].shellescape, echo: false
+          set 'TRAVIS_XCODE_SDK', config[:xcode_sdk].to_s.shellescape, echo: false
+          set 'TRAVIS_XCODE_SCHEME', config[:xcode_scheme].to_s.shellescape, echo: false
+          set 'TRAVIS_XCODE_PROJECT', config[:xcode_project].to_s.shellescape, echo: false
+          set 'TRAVIS_XCODE_WORKSPACE', config[:xcode_workspace].to_s.shellescape, echo: false
         end
 
         def setup
