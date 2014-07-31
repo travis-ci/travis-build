ffc60e0ad17e3523ab67b88ee1376f3a02808558
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-07-31 17:14:42 +0200

    use a fold

--- lib/travis/build/script/php.rb
+++ lib/travis/build/script/php.rb
@@ -33,7 +33,7 @@ module Travis
         def install
           self.if '-f composer.json' do |sub|
             directory_cache.add(sub, "~/.composer") if data.cache?(:composer)
-            sub.cmd "composer install #{config[:composer_args]}".strip
+            sub.cmd "composer install #{config[:composer_args]}".strip, fold: 'install.composer'
           end
         end
 
