f58f6c20a3cb44f9c690ff4d4c85c25a315fa840
Henrik Hodne <henrik@travis-ci.com>
2014-07-31 10:18:52 +0200

    Extract metrics and Sentry to middleware

--- lib/travis/build/app.rb
+++ lib/travis/build/app.rb
@@ -16,50 +16,23 @@ module Travis
         unless type == "token" && token == ENV["API_TOKEN"]
           halt 403, "access denied"
         end
-
-        env['metriks.request.start'] ||= Time.now.utc
-      end
-
-      after do
-        if queue_start = time(env['HTTP_X_QUEUE_START']) || time(env['HTTP_X_REQUEST_START'])
-          time = env['metriks.request.start'] - queue_start
-          ::Metriks.timer('build_api.request_queue').update(time)
-        end
-
-        time = Time.now.utc - env['metriks.request.start']
-
-        ::Metriks.timer("build_api.requests").update(time)
-        ::Metriks.timer("build_api.request.#{request.request_method.downcase}").update(time)
-        ::Metriks.timer("build_api.request.status.#{response.status.to_s[0]}").update(time)
-      end
-
-      def time(value)
-        value = value.to_f
-        start = env['metriks.request.start'].to_f
-        value /= 1000 while value > start
-        Time.at(value) if value > 946684800
       end
 
       configure(:production, :staging) do
-        use(Rack::Config) { |env| env['metriks.request.start'] ||= Time.now.utc }
         use Rack::SSL
       end
 
       configure do
         if ENV["SENTRY_DSN"]
-          require "raven"
-          use Raven::Rack
+          require "travis/build/app_middleware/sentry"
+
+          use Travis::Build::AppMiddleware::Sentry
         end
 
         if ENV.key?("LIBRATO_EMAIL") && ENV.key?("LIBRATO_TOKEN") && ENV.key?("LIBRATO_SOURCE")
-          require "metriks/librato_metrics_reporter"
+          require 'travis/build/app_middleware/metriks'
 
-          Metriks::LibratoMetricsReporter.new(
-            ENV["LIBRATO_EMAIL"],
-            ENV["LIBRATO_TOKEN"],
-            source: ENV["LIBRATO_SOURCE"],
-            on_error: proc { |ex| puts "librato error: #{ex.message} (#{ex.response.body})" }
-          ).start
+          use Travis::Build::AppMiddleware::Metriks
         end
       end
 
