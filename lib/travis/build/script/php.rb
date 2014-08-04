3b0e75b0d61184fb7d74730f6d9c2f4a97a0d527
till <till@php.net>
2014-08-04 18:49:45 +0200

    Enhancement: allows install to be performed through checked in composer.phar
     * re-activates automatic composer install on travis-ci
     * supports running install with a checked in composer.phar
     * doesn't self-update the checked in composer.phar

--- lib/travis/build/script/php.rb
+++ lib/travis/build/script/php.rb
@@ -32,20 +32,35 @@ module Travis
         end
 
         def before_install
-          # self.if '-f composer.json' do |sub|
-          #   sub.cmd 'composer self-update', fold: 'before_install.update_composer'
-          # end
+          self.if '-f composer.json' do |sub|
+            sub.cmd 'composer self-update', fold: 'before_install.update_composer' unless has_phar?
+          end
         end
 
         def install
-          # self.if '-f composer.json' do |sub|
-          #   directory_cache.add(sub, '~/.composer') if data.cache?(:composer)
-          #   sub.cmd "composer install #{config[:composer_args]}".strip, fold: 'install.composer'
-          # end
+          self.if '-f composer.json' do |sub|
+            directory_cache.add(sub, '~/.composer') if data.cache?(:composer)
+
+            composer_exec = "composer" unless has_phar?
+            composer_exec = "composer.phar" if has_phar?
+
+            sub.cmd "#{composer_exec} install #{config[:composer_args]}".strip, fold: 'install.composer'
+          end
         end
 
         def script
           cmd 'phpunit'
+        end
+
+        private
+
+        def has_phar?
+          self.if '-f composer.phar' do
+            echo "Project contains composer.phar"
+            return true
+          end
+
+          false
         end
       end
     end
