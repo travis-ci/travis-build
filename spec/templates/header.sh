3cea8f822d87daa62e497db502d93bbaef4d7434
Henrik Hodne <henrik@travis-ci.com>
2014-08-05 00:36:20 +0200

    Add some docs to the new spec header.sh file

--- spec/templates/header.sh
+++ spec/templates/header.sh
@@ -10,12 +10,17 @@ function generate_stub() {
   echo "}"
 }
 
+# Generate stubs for env and cat so they still work after we blank out PATH
 eval "$(generate_stub env)"
 eval "$(generate_stub cat)"
 
+# Remove everything from PATH to help enforce that no external commands should
+# be run in the test script.
 export PATH=""
 
 # Let's just assume that everything always works
 export TRAVIS_TEST_RESULT=0
 
+# This trap prints out the command and allows the command to be run if it
+# matches the regexp at the end.
 trap 'printf '"'"'%s\0'"'"' "$BASH_COMMAND"; [[ "$BASH_COMMAND" =~ ^(env|export|\[\[) ]]' DEBUG
