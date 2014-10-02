4b03c80a5f3582fc67e23c125bdde8e0e475b3b4
Hiro Asari <asari.ruby@gmail.com>
2014-10-02 01:29:17 -0400

    Test if /var/ramfs exists when copying /var/ramfs/postgresql/*
    Some workers may not have /var/ramfs

--- lib/travis/build/script/addons/postgresql.rb
+++ lib/travis/build/script/addons/postgresql.rb
@@ -17,7 +17,7 @@ module Travis
               sh.set "PATH", "/usr/lib/postgresql/#{@postgresql_version}/bin:$PATH", echo: false
               sh.echo "Starting PostgreSQL v#{@postgresql_version}", ansi: :yellow
               sh.cmd "sudo service postgresql stop", assert: false
-              sh.if( "! -d /var/ramfs/postgresql/#{@postgresql_version}" ) do |no_ramfs|
+              sh.if( "-d /var/ramfs && ! -d /var/ramfs/postgresql/#{@postgresql_version}" ) do |no_ramfs|
                 no_ramfs.cmd "sudo cp -rp /var/lib/postgresql/#{@postgresql_version} /var/ramfs/postgresql/#{@postgresql_version}", assert: false, echo: false
               end
               sh.cmd "sudo service postgresql start #{@postgresql_version}", assert: false
