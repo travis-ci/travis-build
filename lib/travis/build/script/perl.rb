1da5457e0b5204c0f0a850211d74db9c1b08a384
Sven Fuchs <me@svenfuchs.com>
2014-10-27 00:16:14 +0100

    perl script

--- lib/travis/build/script/perl.rb
+++ lib/travis/build/script/perl.rb
@@ -6,18 +6,14 @@ module Travis
           perl: '5.14'
         }
 
-        def cache_slug
-          super << "--perl-" << config[:perl].to_s
-        end
-
         def export
           super
-          sh.export 'TRAVIS_PERL_VERSION', perl_version, echo: false
+          sh.export 'TRAVIS_PERL_VERSION', version, echo: false
         end
 
         def setup
           super
-          sh.cmd "perlbrew use #{perl_version}"
+          sh.cmd "perlbrew use #{version}"
         end
 
         def announce
@@ -31,21 +27,30 @@ module Travis
         end
 
         def script
-          sh.if   '-f Build.PL',    'perl Build.PL && ./Build && ./Build test'
-          sh.elif '-f Makefile.PL', 'perl Makefile.PL && make test'
-          sh.else                   'make test'
+          sh.if '-f Build.PL' do
+            sh.cmd 'perl Build.PL && ./Build && ./Build test'
+          end
+          sh.elif '-f Makefile.PL' do
+            sh.cmd 'perl Makefile.PL && make test'
+          end
+          sh.else do
+            sh.cmd 'make test'
+          end
         end
 
-        def perl_version
-          # this check is needed because safe_yaml parses the string 5.10 to 5.1
-          if config[:perl] == 5.1
-            "5.10"
-          # this check is needed because safe_yaml parses the string 5.20 to 5.2
-          elsif config[:perl] == 5.2
-            "5.20"
-          else
-            config[:perl]
-          end
+        def cache_slug
+          super << '--perl-' << version
+        end
+
+        # safe_yaml parses strings like 5.10 to 5.1
+        VERSIONS = {
+          5.1 => '5.10',
+          5.2 => '5.20'
+        }
+
+        def version
+          version = config[:perl]
+          VERSIONS[version] || version
         end
       end
     end
