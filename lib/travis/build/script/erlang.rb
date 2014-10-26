e50fddf71685af44316560fa0499445610978d24
Sven Fuchs <me@svenfuchs.com>
2014-10-26 23:05:48 +0100

    erlang script

--- lib/travis/build/script/erlang.rb
+++ lib/travis/build/script/erlang.rb
@@ -6,43 +6,49 @@ module Travis
           otp_release: 'R14B04'
         }
 
+        def cache_slug
+          super << '--otp-' << otp_release.to_s
+        end
+
         def export
           super
-          sh.export 'TRAVIS_OTP_RELEASE', config[:otp_release], echo: false
+          sh.export 'TRAVIS_OTP_RELEASE', otp_release, echo: false
         end
 
         def setup
           super
-          sh.cmd "source #{HOME_DIR}/otp/#{config[:otp_release]}/activate"
+          sh.cmd "source #{HOME_DIR}/otp/#{otp_release}/activate"
         end
 
         def install
-          sh.if   "#{rebar_configured} && -f ./rebar", install_rebar('./'), fold: 'install', retry: true
-          sh.elif rebar_configured, install_rebar, fold: 'install', retry: true
+          sh.if "#{rebar_configured} && -f ./rebar" do
+            sh.cmd './rebar get-deps', fold: 'install', retry: true
+          end
+          sh.elif rebar_configured do
+            sh.cmd 'rebar get-deps', fold: 'install', retry: true
+          end
         end
 
         def script
-          sh.if   "#{rebar_configured} && -f ./rebar", run_rebar('./')
-          sh.elif rebar_configured, run_rebar
-          sh.else 'make test'
-        end
-
-        def cache_slug
-          super << '--otp-' << config[:otp_release].to_s
+          sh.if "#{rebar_configured} && -f ./rebar" do
+            sh.cmd './rebar compile && ./rebar skip_deps=true eunit'
+          end
+          sh.elif rebar_configured do
+            sh.cmd 'rebar compile && rebar skip_deps=true eunit'
+          end
+          sh.else do
+            sh.cmd 'make test'
+          end
         end
 
         private
 
+          def otp_release
+            config[:otp_release]
+          end
+
           def rebar_configured
             '(-f rebar.config || -f Rebar.config)'
-          end
-
-          def install_rebar(path = nil)
-            "#{path}rebar get-deps"
-          end
-
-          def run_rebar(path = nil)
-            "#{path}rebar compile && #{path}rebar skip_deps=true eunit"
           end
       end
     end
