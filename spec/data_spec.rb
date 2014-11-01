d01ed0011dc3a5a73210b776240b422b28740ae3
Sven Fuchs <me@svenfuchs.com>
2014-11-01 18:29:53 +0100

    refactor data_spec

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -3,106 +3,91 @@ require 'spec_helper'
 describe Travis::Build::Data do
   describe 'parse' do
     %w(github.com localhost some.custom.endpoint.io).each do |host|
-      it "extracts the source_host from an authenticated git url #{host}" do
-        data = Travis::Build::Data.new(repository: { source_url: "git@#{host}:foo/bar.git" })
-        expect(data.source_host).to eq(host)
-      end
+      describe "for #{host}" do
+        let(:source_url) { "git://#{host}/foo/bar.git" }
+        let(:data)       { Travis::Build::Data.new(repository: { source_url: source_url }) }
 
-      it "extracts the source_host from an anonymous git url #{host}" do
-        data = Travis::Build::Data.new(repository: { source_url: "git://#{host}/foo/bar.git" })
-        expect(data.source_host).to eq(host)
-      end
+        it "extracts the source_host from an authenticated git url #{host}" do
+          expect(data.source_host).to eq(host)
+        end
 
-      it "extracts the source_host from an http url #{host}" do
-        data = Travis::Build::Data.new(repository: { source_url: "http://#{host}/foo/bar.git" })
-        expect(data.source_host).to eq(host)
-      end
+        it "extracts the source_host from an anonymous git url #{host}" do
+          expect(data.source_host).to eq(host)
+        end
 
-      it "extracts the source_host from an https url #{host}" do
-        data = Travis::Build::Data.new(repository: { source_url: "https://#{host}/foo/bar.git" })
-        expect(data.source_host).to eq(host)
+        it "extracts the source_host from an http url #{host}" do
+          expect(data.source_host).to eq(host)
+        end
+
+        it "extracts the source_host from an https url #{host}" do
+          expect(data.source_host).to eq(host)
+        end
       end
     end
   end
 
   describe 'ssh_key' do
-    it 'returns ssh_key from source_key as a fallback' do
-      data = Travis::Build::Data.new(config: { source_key: 'foo' })
-      expect(data.ssh_key.value).to eql('foo')
-      expect(data.ssh_key.source).to be_nil
-      expect(data.ssh_key).to be_encoded
+    describe 'returns ssh_key from source_key as a fallback' do
+      let(:data) { Travis::Build::Data.new(config: { source_key: 'foo' }) }
+
+      it { expect(data.ssh_key.value).to eql('foo') }
+      it { expect(data.ssh_key.source).to be_nil }
+      it { expect(data.ssh_key).to be_encoded }
     end
 
-    it 'returns nil if there is no ssh_key' do
-      data = Travis::Build::Data.new({ config: {} })
-      expect(data.ssh_key).to be_nil
+    describe 'returns nil if there is no ssh_key' do
+      let(:data) { Travis::Build::Data.new(config: {}) }
+      it { expect(data.ssh_key).to be_nil }
     end
 
-    it 'returns ssh_key from api if it is available' do
-      data = Travis::Build::Data.new(ssh_key: { value: 'foo', source: 'the source' })
-      expect(data.ssh_key.value).to eql('foo')
-      expect(data.ssh_key.source).to eql('the source')
+    describe 'returns ssh_key from api if it is available' do
+      let(:data) { Travis::Build::Data.new(ssh_key: { value: 'foo', source: 'the source' }) }
+      it { expect(data.ssh_key.value).to eql('foo') }
+      it { expect(data.ssh_key.source).to eql('the source') }
     end
   end
 
   describe 'cache' do
     subject(:data) { Travis::Build::Data.new(config: { cache: cache }) }
 
-    describe "single value" do
+    describe 'single value' do
       let(:cache) { 'bundler' }
 
-      describe '#cache' do
-        subject { super().cache }
-        it { should eq({ bundler: true }) }
-      end
-
       it { should be_cache(:bundler) }
       it { is_expected.not_to be_cache(:edge) }
+      it { expect(data.cache).to eq(bundler: true) }
     end
 
-    describe "array value" do
+    describe 'array value' do
       let(:cache) { ['bundler', 'edge'] }
 
-      describe '#cache' do
-        subject { super().cache }
-        it { should eq({ bundler: true, edge: true }) }
-      end
       it { should be_cache(:bundler) }
       it { should be_cache(:edge) }
+      it { expect(data.cache).to eq(bundler: true, edge: true) }
     end
 
-    describe "hash value" do
+    describe 'hash value' do
       let(:cache) {{ bundler: true, edge: false }}
 
-      describe '#cache' do
-        subject { super().cache }
-        it { should eq({ bundler: true, edge: false }) }
-      end
       it { should be_cache(:bundler) }
       it { is_expected.not_to be_cache(:edge) }
+      it { expect(data.cache).to eq(bundler: true, edge: false) }
     end
 
-    describe "hash value with strings" do
-      let(:cache) {{ "bundler" => true, "edge" => false }}
+    describe 'hash value with strings' do
+      let(:cache) {{ 'bundler' => true, 'edge' => false }}
 
-      describe '#cache' do
-        subject { super().cache }
-        it { should eq({ bundler: true, edge: false }) }
-      end
       it { should be_cache(:bundler) }
       it { is_expected.not_to be_cache(:edge) }
+      it { expect(data.cache).to eq(bundler: true, edge: false) }
     end
 
-    describe "false" do
+    describe 'false' do
       let(:cache) { false }
-
-      describe '#cache' do
-        subject { super().cache }
-        it { should eq(bundler: false, apt: false, cocoapods: false, composer: false) }
-      end
 
       it { is_expected.not_to be_cache(:bundler) }
       it { is_expected.not_to be_cache(:edge) }
+      it { expect(data.cache).to eq(bundler: false, apt: false, cocoapods: false, composer: false) }
     end
   end
 end
