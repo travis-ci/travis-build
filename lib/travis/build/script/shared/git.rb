49240578d54b75a99a3204b705dd6479e0595c4e
Sven Fuchs <me@svenfuchs.com>
2014-11-03 17:53:40 +0100

    do not assert git submodule init (remove when diffs are clean)

--- lib/travis/build/script/shared/git.rb
+++ lib/travis/build/script/shared/git.rb
@@ -91,7 +91,7 @@ module Travis
           def submodules
             sh.if '-f .gitmodules' do
               sh.file '~/.ssh/config', "Host github.com\n\tStrictHostKeyChecking no\n", append: true
-              sh.cmd 'git submodule init', fold: "git.#{next_git_fold_number}"
+              sh.cmd 'git submodule init', fold: "git.#{next_git_fold_number}", assert: false
               sh.cmd "git submodule update #{submodule_update_args}".strip, assert: true, fold: "git.#{next_git_fold_number}", retry: true
             end
           end
