6b633a9707e480372334808e58a0fb7d3a921ab8
Sven Fuchs <me@svenfuchs.com>
2014-11-05 22:44:04 +0100

    disable deprecation notice output (revert once diffs are clean)

--- lib/travis/build/appliances/deprecations.rb
+++ lib/travis/build/appliances/deprecations.rb
@@ -5,11 +5,11 @@ module Travis
     module Appliances
       class Deprecations < Base
         def apply
-          script.deprecations.map.with_index do |msg, ix|
-            sh.fold "deprecated.#{ix}", pos: ix do
-              sh.deprecate "DEPRECATED: #{msg.gsub /^#{msg[/\A\s*/]}/, ''}"
-            end
-          end
+          # script.deprecations.map.with_index do |msg, ix|
+          #   sh.fold "deprecated.#{ix}", pos: ix do
+          #     sh.deprecate "DEPRECATED: #{msg.gsub /^#{msg[/\A\s*/]}/, ''}"
+          #   end
+          # end
         end
       end
     end
