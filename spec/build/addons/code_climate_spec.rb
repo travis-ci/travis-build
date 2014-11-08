d565d74724d5593fadfd330203f6755e300e0130
Sven Fuchs <me@svenfuchs.com>
2014-11-08 20:37:21 +0100

    run code_climate at after_export, and sauce_connect at after_prepare

--- spec/build/addons/code_climate_spec.rb
+++ spec/build/addons/code_climate_spec.rb
@@ -7,7 +7,7 @@ describe Travis::Build::Addons::CodeClimate, :sexp do
   let(:sh)     { Travis::Shell::Builder.new }
   let(:addon)  { described_class.new(script, sh, Travis::Build::Data.new(data), config) }
   subject      { sh.to_sexp }
-  before       { addon.before_before_script }
+  before       { addon.after_export }
 
   let(:export_repo_token) { [:export, ['CODECLIMATE_REPO_TOKEN', '1234']] }
 
--- spec/build/addons/sauce_connect_spec.rb
+++ spec/build/addons/sauce_connect_spec.rb
@@ -7,7 +7,7 @@ describe Travis::Build::Addons::SauceConnect, :sexp do
   let(:sh)     { Travis::Shell::Builder.new }
   let(:addon)  { described_class.new(script, sh, Travis::Build::Data.new(data), config) }
   subject      { sh.to_sexp }
-  before       { addon.before_before_script }
+  before       { addon.after_prepare }
 
   it_behaves_like 'compiled script' do
     let(:code) { ['sauce_connect', 'TRAVIS_SAUCE_CONNECT=true'] }
