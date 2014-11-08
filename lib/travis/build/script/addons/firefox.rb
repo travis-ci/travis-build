4cc632cf825d7de7eab31e19273b30e4adb6f4c3
Hiro Asari <asari.ruby@gmail.com>
2014-11-07 21:06:12 -0500

    Clean up Firefox addon

--- lib/travis/build/script/addons/firefox.rb
+++ lib/travis/build/script/addons/firefox.rb
@@ -4,6 +4,7 @@ module Travis
       module Addons
         class Firefox
           SUPER_USER_SAFE = true
+          HOME_DIR = Travis::Build::HOME_DIR
 
           def initialize(script, config)
             @script = script
@@ -13,12 +14,12 @@ module Travis
           def before_install
             @script.fold 'install_firefox' do |sh|
               sh.echo "Installing Firefox v#{@firefox_version}", ansi: :yellow
-              sh.raw "mkdir -p \$HOME/firefox-#{@firefox_version}"
-              sh.raw "chown -R travis \$HOME/firefox-#{@firefox_version}"
+              sh.raw "mkdir -p #{HOME_DIR}/firefox-#{@firefox_version}"
+              sh.raw "chown -R travis #{HOME_DIR}/firefox-#{@firefox_version}"
               sh.cmd "wget -O /tmp/firefox.tar.bz2 http://releases.mozilla.org/pub/firefox/releases/#{@firefox_version}/linux-x86_64/en-US/firefox-#{@firefox_version}.tar.bz2", retry: true
-              sh.raw "pushd \$HOME/firefox-#{@firefox_version}"
+              sh.raw "pushd #{HOME_DIR}/firefox-#{@firefox_version}"
               sh.raw "tar xf /tmp/firefox.tar.bz2"
-              sh.raw "export PATH=\$HOME/firefox-#{@firefox_version}/firefox:\$PATH"
+              sh.raw "export PATH=#{HOME_DIR}/firefox-#{@firefox_version}/firefox:$PATH"
               sh.raw "popd"
             end
           end
--- spec/script/addons/firefox_spec.rb
+++ spec/script/addons/firefox_spec.rb
@@ -12,12 +12,12 @@ describe Travis::Build::Script::Addons::Firefox do
   it 'runs the command' do
     script.expects(:fold).with('install_firefox').yields(script)
     script.expects(:echo).with("Installing Firefox v#{config}", ansi: :yellow)
-    script.expects(:raw).with("mkdir -p \$HOME/firefox-#{config}")
-    script.expects(:raw).with("chown -R travis \$HOME/firefox-#{config}")
+    script.expects(:raw).with("mkdir -p #{Travis::Build::HOME_DIR}/firefox-#{config}")
+    script.expects(:raw).with("chown -R travis #{Travis::Build::HOME_DIR}/firefox-#{config}")
     script.expects(:cmd).with("wget -O /tmp/firefox.tar.bz2 http://releases.mozilla.org/pub/firefox/releases/#{config}/linux-x86_64/en-US/firefox-#{config}.tar.bz2", retry: true)
-    script.expects(:raw).with("pushd \$HOME/firefox-#{config}")
+    script.expects(:raw).with("pushd #{Travis::Build::HOME_DIR}/firefox-#{config}")
     script.expects(:raw).with("tar xf /tmp/firefox.tar.bz2")
-    script.expects(:raw).with("export PATH=\$HOME/firefox-#{config}/firefox:\$PATH")
+    script.expects(:raw).with("export PATH=#{Travis::Build::HOME_DIR}/firefox-#{config}/firefox:$PATH")
     script.expects(:raw).with("popd")
     subject
   end
