0b66acab9c47adbdfa42551ae58e22d213381828
Sven Fuchs <me@svenfuchs.com>
2014-11-08 15:48:47 +0100

    fix disable_sudo message

--- lib/travis/build/appliances/disable_sudo.rb
+++ lib/travis/build/appliances/disable_sudo.rb
@@ -4,7 +4,7 @@ module Travis
   module Build
     module Appliances
       class DisableSudo < Base
-        MSG = "\nSudo, the FireFox addon, setuid and setgid have been disabled.\n"
+        MSG = "Sudo, the FireFox addon, setuid and setgid have been disabled.\n"
         CMD = 'sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"'
 
         def apply
