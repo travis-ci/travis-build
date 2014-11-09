e62e29daff6276aeb77739d51851f0d34821f1e4
Sven Fuchs <me@svenfuchs.com>
2014-11-09 15:47:02 +0100

    improve spec coverage for Env

--- spec/build/env_spec.rb
+++ spec/build/env_spec.rb
@@ -4,12 +4,12 @@ describe Travis::Build::Env do
   let(:payload) do
     {
       pull_request: '100',
-      config: { env: 'FOO=foo' },
+      config: { env: ['FOO=foo', 'SECURE BAR=bar'] },
       build: { id: '1', number: '1' },
       job: { id: '1', number: '1.1', branch: 'foo-(dev)', commit: '313f61b', commit_range: '313f61b..313f61a', commit_message: 'the commit message', os: 'linux' },
       repository: { slug: 'travis-ci/travis-ci' },
       env_vars: [
-        { name: 'BAR', value: 'bar', public: true },
+        { name: 'BAM', value: 'bam', public: true },
         { name: 'BAZ', value: 'baz', public: false },
       ]
     }
@@ -18,56 +18,89 @@ describe Travis::Build::Env do
   let(:data) { Travis::Build::Data.new(payload) }
   let(:env)  { described_class.new(data) }
   let(:vars) { env.groups.flat_map(&:vars) }
+  let(:keys) { vars.map(&:key) }
 
   it 'includes travis env vars' do
     travis_vars = vars.select { |v| v.key =~ /^TRAVIS_/ }
     expect(travis_vars.length).to eq(14)
   end
 
-  it 'includes config env vars' do
-    var = vars.find { |v| v.key == 'FOO' }
-    expect(var.value).to eq('foo')
-    expect(var).not_to be_secure
-  end
+  describe 'config env vars' do
+    let(:vars) { super().select { |var| var.type == :config } }
 
-  it 'includes api env vars' do
-    var = vars.find { |v| v.key == 'BAR' }
-    expect(var.value).to eq('bar')
-    expect(var).not_to be_secure
-  end
-
-  it 'includes private api env vars for secure envs' do
-    data.stubs(:secure_env?).returns(true)
-    var = vars.find { |v| v.key == 'BAZ' }
-    expect(var.value).to eq('baz')
-    expect(var).to be_secure
-  end
-
-  it 'does not include private api env vars for non-secure envs' do
-    var = vars.find { |v| v.key == 'BAZ' }
-    expect(var).to be_nil
-  end
-
-  it 'does not include secure env vars for pull requests' do
-    payload[:config] = { env: 'SECURE FOO=foo' }
-    expect(vars.last.key).not_to eq('FOO')
-  end
-
-  it 'does not include env vars that do not have a valid key' do
-  end
-
-  it 'escapes TRAVIS_ vars as needed' do
-    expect(vars.find { |var| var.key == 'TRAVIS_BRANCH' }.value).to eq("foo-\\(dev\\)")
-  end
-
-  context "with TRAVIS_BUILD_DIR including $HOME" do
-    it "shouldn't escape $HOME" do
-      expect(vars.find {|var| var.key == 'TRAVIS_BUILD_DIR'}.value).to eq("$HOME/build/travis-ci/travis-ci")
+    it 'includes non-secure vars' do
+      expect(keys).to include('FOO')
     end
 
-    it "should escape the repository slug" do
+    describe 'for secure env jobs' do
+      before { payload[:job][:secure_env_enabled] = true }
+
+      it 'includes secure vars' do
+        expect(keys).to include('BAR')
+      end
+
+      it 'marks secure vars as secure' do
+        expect(vars.last).to be_secure
+      end
+
+      it 'taints secure var values' do
+        expect(vars.last.value).to be_tainted
+      end
+    end
+
+    describe 'for non-secure env jobs (pull requests)' do
+      before { payload[:job][:secure_env_enabled] = false }
+
+      it 'does not include secure vars' do
+        expect(keys).to_not include('BAR')
+      end
+    end
+  end
+
+  describe 'settings env vars' do
+    let(:vars) { super().select { |var| var.type == :settings } }
+
+    it 'includes non-secure vars' do
+      expect(keys).to include('BAM')
+    end
+
+    describe 'for secure env jobs' do
+      before { payload[:job][:secure_env_enabled] = true }
+
+      it 'includes secure vars' do
+        expect(keys).to include('BAZ')
+      end
+
+      it 'marks secure vars as secure' do
+        expect(vars.last).to be_secure
+      end
+
+      it 'taints secure var values' do
+        expect(vars.last.value).to be_tainted
+      end
+    end
+
+    describe 'for non-secure env jobs (pull requests)' do
+      before { payload[:job][:secure_env_enabled] = false }
+
+      it 'does not include secure vars' do
+        expect(keys).to_not include('BAZ')
+      end
+    end
+  end
+
+  it 'escapes TRAVIS_ vars' do
+    expect(vars.find { |var| var.key == 'TRAVIS_BRANCH' }.value).to eq('foo-\(dev\)')
+  end
+
+  describe 'TRAVIS_BUILD_DIR' do
+    it 'does not escape $HOME' do
+      expect(vars.find {|var| var.key == 'TRAVIS_BUILD_DIR'}.value).to eq('$HOME/build/travis-ci/travis-ci')
+    end
+
+    it 'escapes the repository slug' do
       payload[:repository][:slug] = 'travis-ci/travis-ci ci'
-      expect(vars.find {|var| var.key == 'TRAVIS_BUILD_DIR'}.value).to eq("$HOME/build/travis-ci/travis-ci\\ ci")
+      expect(vars.find {|var| var.key == 'TRAVIS_BUILD_DIR'}.value).to eq('$HOME/build/travis-ci/travis-ci\ ci')
     end
   end
 end
