2e03973dd1ccfc1a6d1f686ff74163d281fbd356
Sven Fuchs <me@svenfuchs.com>
2014-11-09 15:10:09 +0100

    shellescape all builtin env var values

--- lib/travis/build/env/builtin.rb
+++ lib/travis/build/env/builtin.rb
@@ -9,6 +9,8 @@ module Travis
 
         def vars
           env_vars.map do |key, value|
+            value = value.to_s.shellescape
+            value = [BUILD_DIR, value].join('/') if key == :TRAVIS_BUILD_DIR
             Var.new(key, value, type: :builtin)
           end
         end
@@ -22,32 +24,20 @@ module Travis
               CONTINUOUS_INTEGRATION: true,
               HAS_JOSH_K_SEAL_OF_APPROVAL: true,
               TRAVIS_PULL_REQUEST:    pull_request || false,
-              TRAVIS_SECURE_ENV_VARS: secure_env_vars? || false,
+              TRAVIS_SECURE_ENV_VARS: env.secure_env_vars? || false,
               TRAVIS_BUILD_ID:        build[:id],
               TRAVIS_BUILD_NUMBER:    build[:number],
-              TRAVIS_BUILD_DIR:       [BUILD_DIR, slug.shellescape].join('/'),
+              TRAVIS_BUILD_DIR:       repository[:slug],
               TRAVIS_JOB_ID:          job[:id],
               TRAVIS_JOB_NUMBER:      job[:number],
-              TRAVIS_BRANCH:          branch.shellescape,
+              TRAVIS_BRANCH:          job[:branch],
               TRAVIS_COMMIT:          job[:commit],
               TRAVIS_COMMIT_RANGE:    job[:commit_range],
-              TRAVIS_REPO_SLUG:       slug.shellescape,
+              TRAVIS_REPO_SLUG:       repository[:slug],
               TRAVIS_OS_NAME:         config[:os],
               TRAVIS_LANGUAGE:        config[:language],
               TRAVIS_TAG:             job[:tag],
             }
-          end
-
-          def slug
-            repository[:slug] || ''
-          end
-
-          def branch
-            job[:branch] || ''
-          end
-
-          def secure_env_vars?
-            env.secure_env_vars?
           end
       end
     end
