2bfe83910f05fc242d256d02b7b6b51d6d8a0c91
Sven Fuchs <me@svenfuchs.com>
2014-11-11 18:50:03 +0100

    use higher level builder dsl methods in firefox, extract install_dir, source_url and such

--- lib/travis/build/addons/firefox.rb
+++ lib/travis/build/addons/firefox.rb
@@ -11,36 +11,32 @@ module Travis
         def before_before_install
           sh.fold 'install_firefox' do
             sh.echo "Installing Firefox v#{version}", ansi: :yellow
-            sh.raw "mkdir -p #{HOME_DIR}/firefox-#{version}"
-            sh.raw "chown -R travis #{HOME_DIR}/firefox-#{version}"
-            sh.cmd "wget -O /tmp/firefox.tar.bz2 http://releases.mozilla.org/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2", assert: true, echo: true, timing: true, retry: true
-            sh.raw "pushd #{HOME_DIR}/firefox-#{version}"
-            sh.raw "tar xf /tmp/firefox.tar.bz2"
-            sh.raw "export PATH=#{HOME_DIR}/firefox-#{version}/firefox:\$PATH"
-            sh.raw "popd"
-            # sh.mkdir install_dir, echo: false, recursive: true, sudo: true, cmd: true
-            # sh.chown 'travis', install_dir, recursive: true, sudo: true, cmd: true
-            # sh.cmd "wget -O /tmp/firefox.tar.bz2 #{source_url}", assert: false, echo: true, timing: true, retry: true
-            # sh.cd install_dir, stack: true, echo: false
-            # sh.cmd "tar xf /tmp/firefox.tar.bz2"
-            # sh.cd :back, stack: true, echo: false
-            # sh.cmd "ln -sf #{install_dir}/firefox/firefox /usr/local/bin/firefox", sudo: true
-            # sh.cmd "ln -sf #{install_dir}/firefox/firefox-bin /usr/local/bin/firefox-bin", sudo: true
+            sh.mkdir install_dir, echo: false, recursive: true
+            sh.chown 'travis', install_dir, recursive: true
+            sh.cd install_dir, echo: false
+            sh.cmd "wget -O /tmp/#{filename} #{source_url}", echo: true, timing: true, retry: true
+            sh.cmd "tar xf /tmp/#{filename}"
+            sh.export 'PATH', "#{install_dir}/firefox:\$PATH", echo: false
+            sh.cd HOME_DIR, echo: false
           end
         end
 
         private
 
           def version
-            config.to_s #.shellescape
+            config.to_s.shellescape
           end
 
           def install_dir
             "#{HOME_DIR}/firefox-#{version}"
           end
 
+          def filename
+            "firefox-#{version}.tar.bz2"
+          end
+
           def source_url
-            "http://releases.mozilla.org/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
+            "http://releases.mozilla.org/pub/firefox/releases/#{version}/linux-x86_64/en-US/#{filename}"
           end
 
           def tmp_file
