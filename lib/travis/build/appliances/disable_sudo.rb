2b808da58db30812e1720056ac26c69328426edb
Hiro Asari <asari.ruby@gmail.com>
2014-11-24 20:29:51 -0500

    Tweak sudo-less job message

--- lib/travis/build/appliances/disable_sudo.rb
+++ lib/travis/build/appliances/disable_sudo.rb
@@ -4,7 +4,7 @@ module Travis
   module Build
     module Appliances
       class DisableSudo < Base
-        MSG = "This build is running on container-based infrastructure, which does not allow use of 'sudo', setuid and setguid executables.\nSee http://docs.travis-ci.com/user/workers/container-based-infrastructure/ for details."
+        MSG = "This job is running on container-based infrastructure, which does not allow use of 'sudo', setuid and setguid executables.\nSee http://docs.travis-ci.com/user/workers/container-based-infrastructure/ for details.\n"
         CMD = 'sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"'
 
         def apply
