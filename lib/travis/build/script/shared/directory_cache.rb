4853ca285dffb7d5e671f221845c21cd02d7c7a9
Sven Fuchs <me@svenfuchs.com>
2014-11-27 18:25:07 +0100

    fix directory_cache to not raise if s3 config is missing

--- lib/travis/build/script/shared/directory_cache.rb
+++ lib/travis/build/script/shared/directory_cache.rb
@@ -6,14 +6,18 @@ module Travis
     class Script
       module DirectoryCache
         def directory_cache
-          @directory_cache ||= cache_class.new(sh, data, cache_slug)
+          @directory_cache ||= begin
+            cache = cache_class.new(sh, data, cache_slug)
+            cache = Noop.new(sh, data, cache_slug) unless cache.valid? && use_directory_cache?
+            cache
+          end
         end
 
         def cache_class
-          type = data.cache_options[:type].to_s.capitalize
-          type = :Noop if type.empty? or !use_directory_cache?
-          raise ArgumentError, 'unknown caching mode %p' % type unless DirectoryCache.const_defined?(type, false)
-          DirectoryCache.const_get(type)
+          type = data.cache_options[:type] || :noop
+          name = type.to_s.capitalize
+          raise ArgumentError, 'unknown caching mode %p' % type unless DirectoryCache.const_defined?(name, false)
+          DirectoryCache.const_get(name)
         end
 
         def use_directory_cache?
--- spec/build/script/directory_cache_spec.rb
+++ spec/build/script/directory_cache_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Travis::Build::Script::DirectoryCache, :sexp do
   let(:options) { { fetch_timeout: 20, push_timeout: 30, type: 's3', s3: { bucket: 's3_bucket', secret_access_key: 's3_secret_access_key', access_key_id: 's3_access_key_id' } } }
-  let(:data)    { payload_for(:push, :ruby, config: { cache: config, bundler_args: '--path=foo/bar' }, cache_options: options) }
+  let(:data)    { payload_for(:push, :ruby, config: config, cache_options: options) }
   let(:sh)      { Travis::Shell::Builder.new }
   let(:sexp)    { script.sexp }
   let(:script)  { Travis::Build.script(data) }
   let(:cache)   { script.directory_cache }
 
   it_behaves_like 'compiled script' do
-    let(:config) { { directories: ['foo'] } }
+    let(:config) { { cache: { directories: ['foo'] } } }
     let(:cmds)   { ['cache.1', 'cache.2', 'casher fetch', 'casher add', 'casher push'] }
   end
 
@@ -19,8 +19,14 @@ describe Travis::Build::Script::DirectoryCache, :sexp do
     it { expect(cache).to be_a(Travis::Build::Script::DirectoryCache::Noop) }
   end
 
+  describe 'with caching enabled, but config missing' do
+    let(:config)  { { cache: { directories: ['foo'] } } }
+    let(:options) { { type: 's3' } }
+    it { expect(cache).to be_a(Travis::Build::Script::DirectoryCache::Noop) }
+  end
+
   describe 'uses S3 with caching enabled' do
-    let(:config) { { directories: ['foo'] } }
+    let(:config) { { cache: { directories: ['foo'] } } }
     it { expect(script).to be_use_directory_cache }
     it { expect(cache).to be_a(Travis::Build::Script::DirectoryCache::S3) }
   end
@@ -28,7 +34,7 @@ describe Travis::Build::Script::DirectoryCache, :sexp do
   # not quite sure where to put this atm, but there probably should be tests
   # specific to bundler caching
   describe 'with bundler caching enabled' do
-    let(:config) { 'bundler' }
+    let(:config) { { cache: 'bundler', bundler_args: '--path=foo/bar' } }
     it { expect(sexp).to include_sexp [:cmd, 'bundle clean', echo: true] }
   end
 end
