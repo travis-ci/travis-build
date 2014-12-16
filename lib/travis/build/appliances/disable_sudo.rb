bf5854876b92751fc0b06cd92a1f97f501f6586d
Hiro Asari <asari.ruby@gmail.com>
2014-12-16 14:56:01 -0500

    Split disable-sudo message into two strings
    So that they both appear in yellow

--- lib/travis/build/appliances/disable_sudo.rb
+++ lib/travis/build/appliances/disable_sudo.rb
@@ -4,11 +4,13 @@ module Travis
   module Build
     module Appliances
       class DisableSudo < Base
-        MSG = "\nThis job is running on container-based infrastructure, which does not allow use of 'sudo', setuid and setguid executables.\nSee http://docs.travis-ci.com/user/workers/container-based-infrastructure/ for details.\n"
+        MSG1 = "\nThis job is running on container-based infrastructure, which does not allow use of 'sudo', setuid and setguid executables.\n"
+        MSG2 = "See http://docs.travis-ci.com/user/workers/container-based-infrastructure/ for details.\n"
         CMD = 'sudo -n sh -c "sed -e \'s/^%.*//\' -i.bak /etc/sudoers && rm -f /etc/sudoers.d/travis && find / -perm -4000 -exec chmod a-s {} \; 2>/dev/null"'
 
         def apply
-          sh.echo MSG, ansi: :yellow
+          sh.echo MSG1, ansi: :yellow
+          sh.echo MSG2, ansi: :yellow
           sh.cmd CMD
         end
 
