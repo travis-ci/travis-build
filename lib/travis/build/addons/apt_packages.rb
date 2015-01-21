118f392a7483d88055a8201b06bc386c2fb9b017
Dan Buch <dan@travis-ci.org>
2015-01-21 16:18:52 +0100

    Initial idea around package install whitelisting

---
+++ lib/travis/build/addons/apt_packages.rb
@@ -0,0 +1,38 @@
+require 'travis/build/addons/base'
+
+module Travis
+  module Build
+    class Addons
+      class AptPackages < Base
+        SUPER_USER_SAFE = true
+
+        def after_prepare
+          sh.echo "Installing APT Packages (BETA)", ansi: :yellow
+          sh.fold 'apt_packages.0' do
+            whitelisted = []
+            config.each do |package|
+              if whitelist.include?(package)
+                whitelisted << package
+              else
+                sh.echo "Ignoring unknown or disallowed package #{package.inspect}"
+              end
+            end
+
+            sh.export 'DEBIAN_FRONTEND', 'noninteractive', echo: true
+            sh.cmd "apt-get -yq install #{whitelisted.join(' ')}"
+          end
+        end
+
+        private
+
+          def config
+            Array(super)
+          end
+
+          def whitelist
+            @whitelist ||= ENV['TRAVIS_BUILD_APT_WHITELIST'].to_s.split(/\s*,\s*/)
+          end
+      end
+    end
+  end
+end
