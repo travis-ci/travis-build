7c971756e657353adecab1cb58bf2ac875d6efe8
Dan Buch <dan@travis-ci.org>
2015-01-27 16:26:45 -0500

    Fix chruby integration by removing stray double quote

--- lib/travis/build/script/shared/chruby.rb
+++ lib/travis/build/script/shared/chruby.rb
@@ -4,7 +4,7 @@ module Travis
       module Chruby
         def setup
           super
-         setup_chruby if chruby?
+          setup_chruby if chruby?
         end
 
         def announce
@@ -23,7 +23,7 @@ module Travis
           end
 
           def setup_chruby
-            sh.echo 'BETA: Using chruby to select Ruby version. This is currently a beta feature and may change at any time."', ansi: :yellow
+            sh.echo 'BETA: Using chruby to select Ruby version. This is currently a beta feature and may change at any time.', ansi: :yellow
             sh.cmd 'curl -sLo ~/chruby.sh https://gist.githubusercontent.com/henrikhodne/a01cd7367b12a59ee051/raw/chruby.sh', echo: false
             sh.cmd 'source ~/chruby.sh', echo: false
             sh.cmd "chruby #{config[:ruby]}", timing: true
