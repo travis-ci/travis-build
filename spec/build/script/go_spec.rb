d6deb7e8ad56ecd83f3375a87be84ab85292b585
Dan Buch <dan@travis-ci.org>
2015-02-06 15:45:46 -0500

    Assert invalid gimme install URLs are ignored

--- spec/build/script/go_spec.rb
+++ spec/build/script/go_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Travis::Build::Script::Go, :sexp do
-  let(:data)   { payload_for(:push, :go) }
-  let(:script) { described_class.new(data) }
-  subject      { script.sexp }
-  it           { store_example }
+  let(:data)     { payload_for(:push, :go) }
+  let(:script)   { described_class.new(data) }
+  let(:defaults) { described_class::DEFAULTS }
+  subject        { script.sexp }
+  it             { store_example }
 
   it_behaves_like 'compiled script' do
     let(:code) { ['TRAVIS_LANGUAGE=go'] }
@@ -82,6 +83,15 @@ describe Travis::Build::Script::Go, :sexp do
 
   it 'announces gimme version' do
     should include_sexp [:cmd, 'gimme version', echo: true]
+  end
+
+  [
+    "https://raw.githubusercontent.com/meatballhat/gimme/master/gimme' ; curl -sL -o ~/bin/gimme 'https://gist.githubusercontent.com/meatballhat/e2baf03f7ffae8047ccd/raw/f6d89b63eadb5faeeeed5b1bcd63c3fb60df3900/breakout.sh",
+    'https://gist.githubusercontent.com/meatballhat/e2baf03f7ffae8047ccd/raw/f6d89b63eadb5faeeeed5b1bcd63c3fb60df3900/breakout.sh'
+  ].each do |url|
+    it "ignores invalid gimme_config.url #{url}" do
+      should include_sexp [:cmd, "curl -sL -o $HOME/bin/gimme '#{defaults[:gimme_config][:url]}'"]
+    end
   end
 
   it 'announces go env' do
