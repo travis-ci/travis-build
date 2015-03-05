12789a1fd7ff4b88d7650611a9c39c4dcde17cd1
Dan Buch <dan@travis-ci.org>
2015-03-05 17:02:01 -0500

    Add fold headers for sauce connect start/stop operations

--- lib/travis/build/addons/sauce_connect.rb
+++ lib/travis/build/addons/sauce_connect.rb
@@ -15,14 +15,18 @@ module Travis
           sh.export 'SAUCE_USERNAME', username, echo: false if username
           sh.export 'SAUCE_ACCESS_KEY', access_key, echo: false if access_key
 
-          sh.fold 'sauce_connect' do
+          sh.fold 'sauce_connect.start' do
+            sh.echo 'Starting Sauce Connect', echo: true, ansi: :yellow
             sh.cmd 'travis_start_sauce_connect', assert: false, echo: true, timing: true
             sh.export 'TRAVIS_SAUCE_CONNECT', 'true', echo: false
           end
         end
 
-        def finish
-          sh.cmd 'travis_stop_sauce_connect', assert: false, echo: true, timing: true
+        def after_after_script
+          sh.fold 'sauce_connect.stop' do
+            sh.echo 'Stopping Sauce Connect', echo: true, ansi: :yellow
+            sh.cmd 'travis_stop_sauce_connect', assert: false, echo: true, timing: true
+          end
         end
 
         private
