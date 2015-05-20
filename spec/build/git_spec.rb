76b71735ed523e3833365ad5a5083a286e12328c
Hiro Asari <asari.ruby@gmail.com>
2015-05-20 00:01:35 -0400

    Ensure normal git strategy on PR builds
    For PRs, the tarball for the merge commit does not exist, so
    cloning fails.
    
    See https://github.com/travis-ci/travis-ci/issues/3923

--- spec/build/git_spec.rb
+++ spec/build/git_spec.rb
@@ -8,4 +8,29 @@ describe Travis::Build::Git, :sexp do
   let(:rm_ssh_key)    { [:rm, '~/.ssh/source_rsa', force: true] }
 
   it { should include_sexp rm_ssh_key }
+
+  describe :checkout do
+    context 'when using "tarball" strategy' do
+      let(:api)      { 'https://api.github.com/repos/travis-ci/travis-ci' }
+      let(:url)      { "#{api}/tarball/313f61b" }
+      let(:file)     { 'travis-ci-travis-ci.tar.gz' }
+      let(:curl)     { "curl -o #{file} -L #{url}" }
+      let(:download) { [:cmd, curl, assert: true, echo: curl, retry: true, timing: true] }
+
+      before :each do
+        payload[:config][:git] = { strategy: 'tarball' }
+        payload[:repository][:api_url] = api
+      end
+
+      context 'when building a pull request' do
+        before :each do
+          payload[:job][:pull_request] = '22'
+        end
+
+        it { should include_sexp [:echo, "\ntarball strategy is not supported on pull request builds"] }
+        it { should_not include_sexp download }
+      end
+
+    end
+  end
 end
