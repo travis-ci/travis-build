fff9d5a15315c2299faafb666fdbedac22403613
Hiro Asari <asari.ruby@gmail.com>
2015-05-27 08:02:01 -0400

    Use temp file to store new hosts file content
    Because storing it in a variable removes newlines, so the resulting
    hosts file is invalid.

--- spec/build/addons/hosts_spec.rb
+++ spec/build/addons/hosts_spec.rb
@@ -17,7 +17,7 @@ describe Travis::Build::Addons::Hosts, :sexp do
 
   # it { should include_sexp [:cmd, "sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 'one.local\\ two.local'/' -i'.bak' /etc/hosts", sudo: true] }
   # it { should include_sexp [:cmd, "sed -e 's/^\\(::1.*\\)$/\\1 'one.local\\ two.local'/' -i'.bak' /etc/hosts", sudo: true] }
-  it { should include_sexp [:cmd, "HOSTS_FILE_CONTENT=$(sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 one.local\\ two.local/' /etc/hosts | sed -e 's/^\\(::1.*\\)$/\\1 one.local\\ two.local/')"] }
-  it { should include_sexp [:cmd, "echo $HOSTS_FILE_CONTENT | sudo tee /etc/hosts > /dev/null"] }
+  it { should include_sexp [:cmd, "sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 one.local\\ two.local/' /etc/hosts | sed -e 's/^\\(::1.*\\)$/\\1 one.local\\ two.local/' > /tmp/hosts"] }
+  it { should include_sexp [:cmd, "cat /tmp/hosts | sudo tee /etc/hosts > /dev/null"] }
 end
 
