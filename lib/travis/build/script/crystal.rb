cafee5ce3d2f13499e9fd71d7f8115c31b18d3ce
Will Leinweber <will@bitfission.com>
2015-06-09 06:52:19 -0700

    crystal: fix configure, make setup echo match others

--- lib/travis/build/script/crystal.rb
+++ lib/travis/build/script/crystal.rb
@@ -8,21 +8,20 @@ module Travis
           sh.cmd %q(sudo sh -c 'apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54')
           sh.cmd %q(sudo sh -c 'echo "deb http://dist.crystal-lang.org/apt crystal main" > /etc/apt/sources.list.d/crystal.list')
           sh.cmd %q(sudo sh -c 'apt-get update')
-        end
-
-        def setup
-          super
-
-          [ 'Crystal for Travis-CI is not officially supported, but is community maintained.',
-            'Please file any issues using the following link',
-            '  https://github.com/travis-ci/travis-ci/issues/new?labels=community:crystal',
-            'and mention \`@asterite\`, \`@jhass\`, \`@waj\` and \`@will\` in the issue'
-          ].each { |line| sh.echo(line, ansi: :green) }
 
           sh.fold 'crystal_install' do
             sh.echo 'Installing Crystal', ansi: :yellow
             sh.cmd %q(sudo apt-get install crystal')
           end
+        end
+
+        def setup
+          super
+
+          sh.echo 'Crystal for Travis-CI is not officially supported, but is community maintained.', ansi: :green
+          sh.echo 'Please file any issues using the following link', ansi: :green
+          sh.echo '  https://github.com/travis-ci/travis-ci/issues/new?labels=community:crystal', ansi: :green
+          sh.echo 'and mention \`@asterite\`, \`@jhass\`, \`@waj\` and \`@will\` in the issue', ansi: :green
         end
 
         def announce
