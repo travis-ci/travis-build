93f0743abecadc6f49540c65074f814cabdb77ba
Hiro Asari <asari.ruby@gmail.com>
2015-08-05 14:38:38 -0400

    Revert "Fix /etc/mavenrc to not step on user-defined MAVEN_OPTS"
    This reverts commit 243b876a3b7eddf829fad56751590412d843531f.
    
    Resolves
    https://github.com/travis-ci/travis-ci/issues/4621

--- spec/build/script/shared/appliances/fix_etc_mavenrc.rb
+++ spec/build/script/shared/appliances/fix_etc_mavenrc.rb
@@ -1,9 +1,7 @@
 shared_examples_for 'fix etc/mavenrc' do
-  let(:fix_m2_home) { "test -f /etc/mavenrc && sudo sed -e 's/M2_HOME=\\(.\\+\\)$/M2_HOME=${M2_HOME:-\\1}/' -i'.bak' /etc/mavenrc" }
-  let(:fix_maven_opts) { "test -f /etc/mavenrc && sudo sed -e 's/MAVEN_OPTS=\\(.\\+\\)$/MAVEN_OPTS=${MAVEN_OPTS:-\\1}/' -i'.bak' /etc/mavenrc" }
+  let(:fix_etc_mavenrc) { "test -f /etc/mavenrc && sudo sed -e 's/M2_HOME=\\(.\\+\\)$/M2_HOME=${M2_HOME:-\\1}/' -i'.bak' /etc/mavenrc" }
 
   it 'adds an sexp to fix /etc/mavenrc' do
-    should include_sexp [:raw, fix_m2_home]
-    should include_sexp [:raw, fix_maven_opts]
+    should include_sexp [:raw, fix_etc_mavenrc]
   end
 end
