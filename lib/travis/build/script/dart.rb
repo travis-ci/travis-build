6a16e6a4ee1b594e2fdaff26044d2b49ba1b2a33
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-10-09 22:54:08 +0200

    handle tests written with the new test package

--- lib/travis/build/script/dart.rb
+++ lib/travis/build/script/dart.rb
@@ -86,18 +86,29 @@ module Travis
         end
 
         def script
-
-          sh.fold 'test_runner_install' do
-            sh.echo 'Installing Test Runner', ansi: :yellow
-            sh.cmd "pub global activate test_runner"
+          # tests with test package
+          sh.if '[[ -d packages/test ]] || ( [[ -f .packages ]] && grep -q "^test:" .packages )', raw: true do
+            if with_content_shell
+              sh.cmd 'export DISPLAY=:99.0'
+              sh.cmd 'sh -e /etc/init.d/xvfb start'
+              sh.cmd "pub run test -p vm -p content-shell -p firefox"
+            else
+              sh.cmd "pub run test"
+            end
           end
+          # tests with test_runner for old tests written with unittest package
+          sh.elif '[[ -d packages/unittest ]] || ( [[ -f .packages ]] && grep -q "^unittest:" .packages )', raw: true do
+            sh.fold 'test_runner_install' do
+              sh.echo 'Installing Test Runner', ansi: :yellow
+              sh.cmd "pub global activate test_runner"
+            end
 
-          if with_content_shell
-            sh.cmd "xvfb-run -s '-screen 0 1024x768x24' pub global run test_runner --disable-ansi"
-          else
-            sh.cmd "pub global run test_runner --disable-ansi --skip-browser-tests"
+            if with_content_shell
+              sh.cmd "xvfb-run -s '-screen 0 1024x768x24' pub global run test_runner --disable-ansi"
+            else
+              sh.cmd "pub global run test_runner --disable-ansi --skip-browser-tests"
+            end
           end
-
         end
 
         private
