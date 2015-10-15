3db19f5259874412655406136ffb4a0aa6b56479
Steffen Kötte <steffen.koette@gmail.com>
2015-10-15 13:58:51 +0200

    add script for smalltalk-squeak support

---
+++ lib/travis/build/script/smalltalk_squeak.rb
@@ -0,0 +1,23 @@
+module Travis
+  module Build
+    class Script
+      class SmalltalkSqueak < Script
+        DEFAULTS = {}
+
+        def setup
+          super
+          sh.cmd 'sudo apt-get install --no-install-recommends libc6:i386 ' +
+                   'libuuid1:i386', retry: true
+          sh.cmd "export PROJECT_HOME=\"$(pwd)\""
+          sh.cmd "cd $HOME"
+          sh.cmd "wget -q -O filetreeCI.zip https://github.com/hpi-swa/filetreeCI/archive/master.zip"
+          sh.cmd "unzip -q -o filetreeCI.zip"
+          sh.cmd "cd filetreeCI-*"
+          sh.cmd "export FILETREE_CI_HOME=\"$(pwd)\""
+          sh.cmd "$FILETREE_CI_HOME/run.sh"
+        end
+
+      end
+    end
+  end
+end
