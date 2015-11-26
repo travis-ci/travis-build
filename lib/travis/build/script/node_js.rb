e5b998e0f4e38cc3842c5c979878c297054d81d8
Hiro Asari <asari.ruby@gmail.com>
2015-11-25 19:47:39 -0500

    Actually use C++11-only feature

--- lib/travis/build/script/node_js.rb
+++ lib/travis/build/script/node_js.rb
@@ -23,7 +23,7 @@ module Travis
         def announce
           super
           if iojs_3_plus?
-            sh.cmd 'echo "int main() {return 0;}" > foo.cpp', echo: false
+            sh.cmd 'echo -e "#include <array>\nstd::array<int, 1> arr = {0}; int main() {return 0;}" > foo.cpp', echo: false
             sh.raw "! ($CXX -std=c++11 -o /dev/null foo.cpp >&/dev/null || g++ -std=c++11 -o /dev/null foo.cpp >&/dev/null)" do
               sh.echo "Starting with io.js 3 and Node.js 4, building native extensions requires C++11-compatible compiler, which seems unavailable on this VM. Please read THIS DOC.", ansi: :yellow
             end
