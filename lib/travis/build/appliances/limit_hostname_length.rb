4705850f4bb0c9c0818df606dab7b76ee348cd5d
Hiro Asari <asari.ruby@gmail.com>
2016-01-04 12:28:55 -0500

    Drop '-f' from 'hostname -f'

--- lib/travis/build/appliances/limit_hostname_length.rb
+++ lib/travis/build/appliances/limit_hostname_length.rb
@@ -7,8 +7,10 @@ module Travis
         def_delegators :data, :job
 
         def apply
-          sh.raw 'sudo hostname "$(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '"', echo: true
-          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname -f | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts', echo: true
+          sh.echo "Truncating hostname. See https://github.com/travis-ci/travis-ci/issues/5227.", ansi: :yellow
+          sh.raw 'sudo hostname "$(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '"', echo: false
+          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts', echo: false
+          sh.cmd 'hostname', echo: true
         end
 
         def apply?
