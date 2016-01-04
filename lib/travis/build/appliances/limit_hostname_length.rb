45314eb9eecd50df6f532e532f1ca93bee8ffa62
Hiro Asari <asari.ruby@gmail.com>
2016-01-04 15:16:52 -0500

    Suppress tee output

--- lib/travis/build/appliances/limit_hostname_length.rb
+++ lib/travis/build/appliances/limit_hostname_length.rb
@@ -9,7 +9,7 @@ module Travis
         def apply
           sh.echo "Truncating hostname. See https://github.com/travis-ci/travis-ci/issues/5227.", ansi: :yellow
           sh.raw 'sudo hostname "$(hostname 2>/dev/null | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '" 2>/dev/null', echo: false
-          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts 2>/dev/null', echo: false
+          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts >&/dev/null', echo: false
           sh.cmd 'hostname', echo: true
         end
 
