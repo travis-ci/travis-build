a1e4915b9107522e6ae92e3ebee7da36b0c17ca1
Hiro Asari <asari.ruby@gmail.com>
2016-01-04 13:21:25 -0500

    Surpress hostname-related STDERR output

--- lib/travis/build/appliances/limit_hostname_length.rb
+++ lib/travis/build/appliances/limit_hostname_length.rb
@@ -8,8 +8,8 @@ module Travis
 
         def apply
           sh.echo "Truncating hostname. See https://github.com/travis-ci/travis-ci/issues/5227.", ansi: :yellow
-          sh.raw 'sudo hostname "$(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '"', echo: false
-          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts', echo: false
+          sh.raw 'sudo hostname "$(hostname 2>/dev/null | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '" 2>/dev/null', echo: false
+          sh.raw 'sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -d. -f1 | cut -d- -f1-2)-job-' + job[:id].to_s + '/" /etc/hosts | sudo tee /etc/hosts 2>/dev/null', echo: false
           sh.cmd 'hostname', echo: true
         end
 
--- spec/build/script/shared/appliances/limit_hostname_length.rb
+++ spec/build/script/shared/appliances/limit_hostname_length.rb
@@ -1,5 +1,5 @@
 shared_examples_for 'fix hostname' do
-  let(:limit_hostname_length) { "sudo hostname \"$(hostname | cut -d. -f1 | cut -d- -f1-2)-job-1\"" }
+  let(:limit_hostname_length) { "sudo hostname \"$(hostname 2>/dev/null | cut -d. -f1 | cut -d- -f1-2)-job-1\" 2>/dev/null" }
 
   it 'adds an sexp to shorten hostname' do
     should include_sexp [:raw, limit_hostname_length]
