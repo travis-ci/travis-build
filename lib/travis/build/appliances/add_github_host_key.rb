9e1ed2365c7c046ac37fb12482f411bd4d922e60
Hiro Asari <asari.ruby@gmail.com>
2016-01-07 12:52:47 -0500

    Do not echo ssh-keyscan command output

--- lib/travis/build/appliances/add_github_host_key.rb
+++ lib/travis/build/appliances/add_github_host_key.rb
@@ -5,7 +5,7 @@ module Travis
     module Appliances
       class AddGithubHostKey < Base
         def apply
-          sh.raw %(ssh-keyscan -t rsa,dsa -H github.com 2>&1 | tee -a #{Travis::Build::HOME_DIR}/.ssh/known_hosts)
+          sh.raw %(ssh-keyscan -t rsa,dsa -H github.com 2>&1 | tee -a #{Travis::Build::HOME_DIR}/.ssh/known_hosts), echo: false
         end
       end
     end
