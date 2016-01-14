e2c45c5352c8d9d3ed9b6de73e7f93122e05ac72
Hiro Asari <asari.ruby@gmail.com>
2016-01-14 13:40:17 -0500

    Ensure ~/.ssh/config exists

--- lib/travis/build/appliances/disable_ssh_roaming.rb
+++ lib/travis/build/appliances/disable_ssh_roaming.rb
@@ -7,6 +7,7 @@ module Travis
         def apply
           sh.cmd %(mkdir -p $HOME/.ssh)
           sh.cmd %(chmod 0700 $HOME/.ssh)
+          sh.cmd %(touch $HOME/.ssh/config)
           sh.cmd %(echo -e "Host *\n  UseRoaming no\n" | cat - $HOME/.ssh/config > $HOME/.ssh/config.tmp && mv $HOME/.ssh/config.tmp $HOME/.ssh/config)
         end
       end
