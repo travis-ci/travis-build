25ca76b5c67ac289d1a206630bbc299b76740e42
Hiro Asari <asari.ruby@gmail.com>
2016-02-04 10:30:36 -1000

    Install PostgreSQL *after* stopping service
    So the default port may be used in configuration

--- lib/travis/build/addons/postgresql.rb
+++ lib/travis/build/addons/postgresql.rb
@@ -11,11 +11,11 @@ module Travis
         DEFAULT_FALLBACK_PORT = 5433
 
         def after_prepare
-          install(version)
           sh.fold 'postgresql' do
             sh.export 'PATH', "/usr/lib/postgresql/#{version}/bin:$PATH", echo: false
             sh.echo "Starting PostgreSQL v#{version}", ansi: :yellow
             sh.cmd 'service postgresql stop', assert: false, sudo: true, echo: true, timing: true
+            install(version)
             sh.if "-d /var/ramfs && ! -d /var/ramfs/postgresql/#{version}", echo: false do
               sh.cmd "cp -rp /var/lib/postgresql/#{version} /var/ramfs/postgresql/#{version}", sudo: true, assert: false, echo: false, timing: false
             end
