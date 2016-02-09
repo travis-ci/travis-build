592f87448bdc31ab943e0ad97dc796a8cf6c1656
Fabio Niephaus <code@fniephaus.com>
2016-02-09 22:07:56 +0000

    Always add i386 architecture

--- lib/travis/build/script/smalltalk.rb
+++ lib/travis/build/script/smalltalk.rb
@@ -100,10 +100,7 @@ module Travis
             sh.fold 'install_packages' do
               sh.echo 'Installing dependencies', ansi: :yellow
 
-              sh.if '$(lsb_release -cs) != precise' do
-                sh.cmd 'sudo dpkg --add-architecture i386'
-              end
-
+              sh.cmd 'sudo dpkg --add-architecture i386'
               sh.cmd 'sudo apt-get update -qq', retry: true
               sh.cmd 'sudo apt-get install --no-install-recommends ' +
                      'libc6:i386 libuuid1:i386 libfreetype6:i386 libssl1.0.0:i386', retry: true
@@ -195,7 +192,6 @@ module Travis
               sh.cmd 'sudo ln -f -s /usr/lib/i386-lin-gnu/libstdc++.so.6 /usr/lib/i386-linux-gnu/libstdc++.so'
             end
             sh.if '$(lsb_release -cs) = trusty' do
-              sh.cmd 'sudo dpkg --add-architecture i386'
               gemstone_install_linux_dependencies
               sh.cmd 'sudo ln -f -s /usr/lib/i386-lin-gnu/libstdc++.so.6 /usr/lib/i386-linux-gnu/libstdc++.so'
             end
@@ -205,6 +201,7 @@ module Travis
             sh.fold 'gemstone_dependencies' do
               sh.echo 'Installing GemStone dependencies', ansi: :yellow
 
+              sh.cmd 'sudo dpkg --add-architecture i386'
               sh.cmd 'sudo apt-get update -qq', retry: true
               sh.cmd 'sudo apt-get install --no-install-recommends ' +
                      'libpam0g:i386 libssl1.0.0:i386 gcc-multilib ' +
