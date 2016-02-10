86a456bae239935016bb2c40ff5e1765212e4559
Hiro Asari <asari.ruby@gmail.com>
2016-02-10 08:21:49 -1000

    Show short version string for bash
    Resolves https://github.com/travis-ci/travis-ci/issues/5587

--- lib/travis/build/script/generic.rb
+++ lib/travis/build/script/generic.rb
@@ -5,7 +5,7 @@ module Travis
         DEFAULTS = {}
 
         def announce
-          sh.cmd "bash --version", echo: true, assert: true, timing: false
+          sh.cmd "bash -c 'echo $BASH_VERSION'", echo: true, assert: true, timing: false
         end
       end
     end
