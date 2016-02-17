675bd2c8c7b9a802ad51a7ab277cb97117f9d1db
Brandon Burton <brandon@inatree.org>
2016-02-16 20:53:04 -0800

    Make it go
    - Make only run when not on Darwin
    - Make it actually be used

--- lib/travis/build/appliances/update_glibc.rb
+++ lib/travis/build/appliances/update_glibc.rb
@@ -6,9 +6,12 @@ module Travis
       class UpdateGlibc < Base
         def apply
           sh.export 'DEBIAN_FRONTEND', 'noninteractive', echo: true
-          sh.cmd "sudo -E apt-get -yq update &>> ~/apt-get-update.log", echo: true, timing: true
-          sh.cmd 'sudo -E apt-get -yq --no-install-suggests --no-install-recommends ' \
-                "--force-yes install libc6}", echo: true, timing: true
+          sh.cmd <<--EOF
+if [ ! $(uname|grep Darwin) ]; then
+  sudo -E apt-get -yq update &>> ~/apt-get-update.log"
+  sudo -E apt-get -yq --no-install-suggests --no-install-recommends "--force-yes install libc6"
+fi
+          EOF
         end
       end
     end
