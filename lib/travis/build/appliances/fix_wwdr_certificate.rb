71c7b326027c964a2f3c3a9d104b6891d25b6b17
Hiro Asari <asari.ruby@gmail.com>
2016-02-16 20:26:44 -0600

    Polish fix_wwdr_certificate appliance
    * sh.cmd needs a single command
    * add code to apply the appliance
    
    Conflicts:
    	lib/travis/build/appliances/fix_wwdr_certificate.rb

--- lib/travis/build/appliances/fix_wwdr_certificate.rb
+++ lib/travis/build/appliances/fix_wwdr_certificate.rb
@@ -5,12 +5,14 @@ module Travis
     module Appliances
       class FixWwdrCertificate < Base
         def apply
-          sh.cmd %(if `command -v sw_vers`; then)
-          sh.cmd %(  echo im doin it)
-          sh.cmd %(  sudo security delete-certificate -Z 0950B6CD3D2F37EA246A1AAA20DFAADBD6FE1F75 /Library/Keychains/System.keychain)
-          sh.cmd %(  wget -q https://developer.apple.com/certificationauthority/AppleWWDRCA.cer)
-          sh.cmd %(  sudo security add-certificates -k /Library/Keychains/System.keychain AppleWWDRCA.cer)
-          sh.cmd %(fi)
+          sh.cmd <<-EOF
+if [ $(command -v sw_vers) ]; then
+  echo "Fix WWDRCA Certificate"
+  sudo security delete-certificate -Z 0950B6CD3D2F37EA246A1AAA20DFAADBD6FE1F75 /Library/Keychains/System.keychain
+  wget -q https://developer.apple.com/certificationauthority/AppleWWDRCA.cer
+  sudo security add-certificates -k /Library/Keychains/System.keychain AppleWWDRCA.cer
+fi
+          EOF
         end
       end
     end
--- lib/travis/build/script.rb
+++ lib/travis/build/script.rb
@@ -106,6 +106,7 @@ module Travis
           apply :fix_resolv_conf
           apply :fix_etc_hosts
           apply :fix_etc_mavenrc
+          apply :fix_wwdr_certificate
           apply :put_localhost_first
           apply :home_paths
           apply :disable_ssh_roaming
