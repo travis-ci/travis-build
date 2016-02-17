3042466e4630eb91886f40519baa4bdd0229704f
Hiro Asari <asari.ruby@gmail.com>
2016-02-17 14:03:11 -0600

    Tweak CVE-2015-7547 fix display

--- lib/travis/build/appliances/update_glibc.rb
+++ lib/travis/build/appliances/update_glibc.rb
@@ -5,13 +5,15 @@ module Travis
     module Appliances
       class UpdateGlibc < Base
         def apply
-          sh.export 'DEBIAN_FRONTEND', 'noninteractive', echo: true
-          sh.cmd <<-EOF
+          sh.fold "fix.CVE-2015-7547" do
+            sh.export 'DEBIAN_FRONTEND', 'noninteractive'
+            sh.cmd <<-EOF
 if [ ! $(uname|grep Darwin) ]; then
   sudo -E apt-get -yq update &>> ~/apt-get-update.log
   sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install libc6
 fi
-          EOF
+            EOF
+          end
         end
       end
     end
