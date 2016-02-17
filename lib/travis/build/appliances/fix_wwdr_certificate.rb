97e1d4b782b55b2b8c4c32c8ecf3f790a48d847f
emdantrim <emma@travis-ci.org>
2016-02-17 03:32:23 +0000

    set quiet on wget

--- lib/travis/build/appliances/fix_wwdr_certificate.rb
+++ lib/travis/build/appliances/fix_wwdr_certificate.rb
@@ -8,7 +8,7 @@ module Travis
           sh.cmd %(if `command -v sw_vers`; then)
           sh.cmd %(  echo im doin it)
           sh.cmd %(  sudo security delete-certificate -Z 0950B6CD3D2F37EA246A1AAA20DFAADBD6FE1F75 /Library/Keychains/System.keychain)
-          sh.cmd %(  wget https://developer.apple.com/certificationauthority/AppleWWDRCA.cer)
+          sh.cmd %(  wget -q https://developer.apple.com/certificationauthority/AppleWWDRCA.cer)
           sh.cmd %(  sudo security add-certificates -k /Library/Keychains/System.keychain AppleWWDRCA.cer)
           sh.cmd %(fi)
         end
