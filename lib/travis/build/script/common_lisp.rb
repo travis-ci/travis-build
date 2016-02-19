b84821628d5bc032f63a37ca08c1f6c68d37c9c0
Stelian Ionescu <sionescu@cddr.org>
2016-02-19 10:23:22 +0100

    Add maintainers

--- lib/travis/build/script/common_lisp.rb
+++ lib/travis/build/script/common_lisp.rb
@@ -1,3 +1,9 @@
+# Maintained by:
+# Thayne McCombs  @tmccombs <astrothayne@gmail.com>
+# Luis Oliveira   @luismbo  <luismbo@gmail.com>
+# SANO Masatoshi  @snmsts   <snmsts@gmail.com>
+# Stelian Ionescu @sionescu <sionescu@cddr.org>
+
 require 'tempfile'
 
 module Travis
@@ -17,6 +23,10 @@ Please either override the script: key
 
         def configure
           super
+          sh.echo "Common Lisp for Travis-CI is not ufficially supported, but community-maintained", ansi: :green
+          sh.echo "Please file any issues at https://github.com/travis-ci/travis-ci/issues"
+          sh.echo "and mention @tmccombs, @luismbo, @snmsts and @sionescu in the issue"
+          sh.echo "You can also use the mailing list travisci@common-lisp.net for general discussion or feature requests."
           sh.cmd 'sudo apt-get update'
           sh.cmd 'sudo apt-get -y install libc6:i386 libc6-dev libc6-dev-i386 libffi-dev libffi-dev:i386'
           sh.fold('roswell-install') do
