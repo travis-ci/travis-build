3e2de40b00a68fa994997e6bf8303edff1eb107d
Nicolas Viennot <nicolas@viennot.biz>
2016-02-23 18:40:22 -0500

    Phrasing

--- lib/travis/build/appliances/debug_tools/templates/travis_debug.sh
+++ lib/travis/build/appliances/debug_tools/templates/travis_debug.sh
@@ -11,7 +11,7 @@ TMATE="tmate -S /tmp/tmate.sock"
 $TMATE new-session -d '/bin/bash'
 $TMATE wait tmate-ready
 
-echo -e "${ANSI_YELLOW}Use any of the following connections to access the debugging environment:${ANSI_RESET}"
+echo -e "${ANSI_YELLOW}Use the following SSH command to access the debugging environment:${ANSI_RESET}"
 $TMATE display -p `echo -e "${ANSI_GREEN}#{tmate_ssh}${ANSI_RESET}"`
 
 if [ "$1" == "private" ]; then
