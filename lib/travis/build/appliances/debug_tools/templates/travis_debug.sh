06435890835317443b44fdfca5adc4a58cf4d6bb
Nicolas Viennot <nicolas@viennot.biz>
2016-02-23 18:54:28 -0500

    cleanup

--- lib/travis/build/appliances/debug_tools/templates/travis_debug.sh
+++ lib/travis/build/appliances/debug_tools/templates/travis_debug.sh
@@ -14,11 +14,6 @@ $TMATE wait tmate-ready
 echo -e "${ANSI_YELLOW}Use the following SSH command to access the debugging environment:${ANSI_RESET}"
 $TMATE display -p `echo -e "${ANSI_GREEN}#{tmate_ssh}${ANSI_RESET}"`
 
-if [ "$1" == "private" ]; then
-  # TODO detect ssh connection close instead of sleeping forever.
-  while sleep 60; do echo .; done
-else
-  mkfifo /tmp/travis_debug.pipe
-  $TMATE pipe-pane 'cat >> /tmp/travis_debug.pipe'
-  cat /tmp/travis_debug.pipe
-fi
+mkfifo /tmp/travis_debug.pipe
+$TMATE pipe-pane 'cat >> /tmp/travis_debug.pipe'
+cat /tmp/travis_debug.pipe
