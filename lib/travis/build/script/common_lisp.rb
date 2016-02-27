30993f270f4f73c8d768c9bdb9c7e456b892c0fd
SANO Masatoshi <snmsts@gmail.com>
2016-02-27 23:56:26 +0900

    fix typo

--- lib/travis/build/script/common_lisp.rb
+++ lib/travis/build/script/common_lisp.rb
@@ -23,7 +23,7 @@ Please either override the script: key
 
         def configure
           super
-          sh.echo "Common Lisp for Travis-CI is not ufficially supported, but community-maintained", ansi: :green
+          sh.echo "Common Lisp for Travis-CI is not officially supported, but community-maintained", ansi: :green
           sh.echo "Please file any issues at https://github.com/travis-ci/travis-ci/issues"
           sh.echo "and mention @tmccombs, @luismbo, @snmsts and @sionescu in the issue"
           sh.echo "You can also use the mailing list travisci@common-lisp.net for general discussion or feature requests."
