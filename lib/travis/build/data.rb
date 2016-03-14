96e6ac2d03e4bb5248f558a323393bd038119591
Hiro Asari <asari.ruby@gmail.com>
2016-03-14 08:50:07 -1000

    Fix queue name access

--- lib/travis/build/data.rb
+++ lib/travis/build/data.rb
@@ -59,7 +59,7 @@ module Travis
       end
 
       def cache_options
-        opts = data[:cache_settings].fetch(job[:queue].to_sym) || data[:cache_options] || {}
+        opts = data[:cache_settings].fetch(data[:queue].to_sym) || data[:cache_options] || {}
         Travis.logger.info("cache_settings: #{cache_settings.inspect}")
         opts
       end
