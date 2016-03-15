ef349dfd953ce6ee2a3e9a410885abfc73073eda
Hiro Asari <asari.ruby@gmail.com>
2016-03-14 15:11:36 -1000

    Fix bucket name key

--- lib/travis/build/script/shared/directory_cache/base.rb
+++ lib/travis/build/script/shared/directory_cache/base.rb
@@ -185,7 +185,7 @@ module Travis
               Location.new(
                 data_store_options.fetch(:scheme, 'https'),
                 data_store_options.fetch(:region, 'us-east-1'),
-                data_store_options.fetch(:bucket, ''),
+                data_store_options.fetch(:bucket_name, ''),
                 path,
                 host_proc
               )
