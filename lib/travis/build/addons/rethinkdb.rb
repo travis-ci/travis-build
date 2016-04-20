68d9b2ad575c5b9a2ed3e193820fb6b6441752bf
Daniel Mewes <daniel@rethinkdb.com>
2016-04-20 15:43:40 -0700

    Working OS check

--- lib/travis/build/addons/rethinkdb.rb
+++ lib/travis/build/addons/rethinkdb.rb
@@ -11,23 +11,25 @@ module Travis
 
         def after_prepare
           sh.fold 'rethinkdb' do
-            #if ENV['TRAVIS_OS_NAME'] != 'linux'
-            #  raise("The RethinkDB addon only works on Linux. $TRAVIS_OS_NAME is #{ENV['TRAVIS_OS_NAME']}.")
-            #end
-            sh.echo "Installing RethinkDB version #{rethinkdb_version}", ansi: :yellow
-            sh.cmd "service rethinkdb stop", sudo: true
-            sh.cmd "apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 #{RETHINKDB_GPG_KEY}", sudo: true
-            sh.cmd 'add-apt-repository "deb http://download.rethinkdb.com/apt $(lsb_release -cs) main"', sudo: true
-            sh.cmd "apt-get update -qq", assert: false, sudo: true
-            sh.cmd "package_version=#{rethinkdb_version}$(lsb_release -cs)"
-            sh.cmd "apt-get install -y -o Dpkg::Options::='--force-confnew' rethinkdb=$package_version", sudo: true, echo: true, timing: true
-            sh.echo "Installing RethinkDB default instance configuration"
-            sh.cmd "cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/default.conf", sudo: true
-            sh.echo "Starting RethinkDB v#{rethinkdb_version}", ansi: :yellow
-            sh.cmd "service rethinkdb start", sudo: true, assert: false, echo: true, timing: true
-            sh.export 'TRAVIS_RETHINKDB_VERSION', rethinkdb_version, echo: false
-            sh.export 'TRAVIS_RETHINKDB_PACKAGE_VERSION', '$package_version', echo: false
-            sh.cmd "rethinkdb --version", assert: false, echo: true
+            sh.if "ENV['TRAVIS_OS_NAME'] != 'linux'" do
+              sh.echo "The RethinkDB addon only works on Linux. $TRAVIS_OS_NAME is #{ENV['TRAVIS_OS_NAME']}.", ansi: :red
+            end
+            sh.else do
+              sh.echo "Installing RethinkDB version #{rethinkdb_version}", ansi: :yellow
+              sh.cmd "service rethinkdb stop", sudo: true
+              sh.cmd "apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 #{RETHINKDB_GPG_KEY}", sudo: true
+              sh.cmd 'add-apt-repository "deb http://download.rethinkdb.com/apt $(lsb_release -cs) main"', sudo: true
+              sh.cmd "apt-get update -qq", assert: false, sudo: true
+              sh.cmd "package_version=#{rethinkdb_version}$(lsb_release -cs)"
+              sh.cmd "apt-get install -y -o Dpkg::Options::='--force-confnew' rethinkdb=$package_version", sudo: true, echo: true, timing: true
+              sh.echo "Installing RethinkDB default instance configuration"
+              sh.cmd "cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/default.conf", sudo: true
+              sh.echo "Starting RethinkDB v#{rethinkdb_version}", ansi: :yellow
+              sh.cmd "service rethinkdb start", sudo: true, assert: false, echo: true, timing: true
+              sh.export 'TRAVIS_RETHINKDB_VERSION', rethinkdb_version, echo: false
+              sh.export 'TRAVIS_RETHINKDB_PACKAGE_VERSION', '$package_version', echo: false
+              sh.cmd "rethinkdb --version", assert: false, echo: true
+            end
           end
         end
 
