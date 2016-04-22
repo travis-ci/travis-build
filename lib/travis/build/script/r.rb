f65cb04042ef956f8084947af88b4d179924e503
Jim Hester <james.f.hester@gmail.com>
2016-04-22 07:48:44 -0400

    Update to 3.2.5

--- lib/travis/build/script/r.rb
+++ lib/travis/build/script/r.rb
@@ -470,10 +470,10 @@ module Travis
         def normalized_r_version
           v = config[:r].to_s
           case v
-          when 'release' then '3.2.4'
+          when 'release' then '3.2.5'
           when 'oldrel' then '3.1.3'
           when '3.1' then '3.1.3'
-          when '3.2' then '3.2.4'
+          when '3.2' then '3.2.5'
           when 'bioc-devel'
             config[:bioc_required] = true
             config[:bioc_use_devel] = true
