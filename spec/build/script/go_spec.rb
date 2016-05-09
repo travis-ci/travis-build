8bcb23143fd802b6bde36a2d6efa5dc6c34bac41
Hiro Asari <asari.ruby@gmail.com>
2016-05-09 06:33:59 +0200

    Test `go get -t` for Go 1.5, 1.6, and tip

--- spec/build/script/go_spec.rb
+++ spec/build/script/go_spec.rb
@@ -111,7 +111,7 @@ describe Travis::Build::Script::Go, :sexp do
     end
   end
 
-  %w(1 1.2 1.2.2 1.3).each do |recent_go_version|
+  %w(1 1.2 1.2.2 1.3 1.5 1.6 tip).each do |recent_go_version|
     describe "if no Makefile exists on #{recent_go_version}" do
       it 'installs with go get -t' do
         data[:config][:go] = recent_go_version
