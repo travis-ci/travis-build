902d380fb8cf46840624c83cac8393cf17213e06
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-05-25 17:27:55 +0200

    Create Sonarqube addon

---
+++ lib/travis/build/addons/sonarqube.rb
@@ -0,0 +1,38 @@
+require 'travis/build/addons/base'
+
+module Travis
+  module Build
+    class Addons
+      class Sonarqube < Base
+        SUPER_USER_SAFE = true
+        DEFAULT_SQ_HOST_URL = "https://nemo.sonarqube.org"
+        SCANNER_CLI_VERSION = "2.6.1"
+
+        def before_before_script
+          @scanner_home = "$HOME/.sonarscanner"
+          sh.fold 'sonarqube.install' do
+            sh.echo "Preparing SonarQube Scanner", echo: false, ansi: :yellow
+            install_sonar_scanner
+
+            sh.export 'SONAR_SCANNER_HOME', "#{@scanner_home}/sonar-scanner-#{SCANNER_CLI_VERSION}", echo: true
+            sh.export 'SONAR_SCANNER_OPTS', "\"$SONAR_SCANNER_OPTS -Dsonar.host.url=#{DEFAULT_SQ_HOST_URL}\"", echo: true
+            sh.export 'MAVEN_OPTS', "\"$MAVEN_OPTS -Dsonar.host.url=#{DEFAULT_SQ_HOST_URL}\"", echo: true
+            sh.export 'GRADLE_OPTS', "\"$GRADLE_OPTS -Dsonar.host.url=#{DEFAULT_SQ_HOST_URL}\"", echo: true
+            sh.export 'PATH', "\"$PATH:#{@scanner_home}/sonar-scanner-#{SCANNER_CLI_VERSION}/bin\"", echo: false
+          end
+        end
+        private
+
+        def install_sonar_scanner
+          scr = <<SH
+  rm -rf #{@scanner_home}
+  mkdir -p #{@scanner_home}
+  curl -sSLo #{@scanner_home}/sonar-scanner.zip http://repo1.maven.org/maven2/org/sonarsource/scanner/cli/sonar-scanner-cli/#{SCANNER_CLI_VERSION}/sonar-scanner-cli-#{SCANNER_CLI_VERSION}.zip
+  unzip #{@scanner_home}/sonar-scanner.zip -d #{@scanner_home}
+SH
+          sh.raw(scr, echo: false)
+        end
+      end
+    end
+  end
+end
---
+++ spec/build/addons/sonarqube_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Travis::Build::Addons::Sonarqube, :sexp do
+  let(:script) { stub('script') }
+  let(:config) { :true }
+  let(:data)   { payload_for(:push, :ruby, config: { addons: { sonarqube: config } }) }
+  let(:sh)     { Travis::Shell::Builder.new }
+  let(:addon)  { described_class.new(script, sh, Travis::Build::Data.new(data), config) }
+  subject      { sh.to_sexp }
+  before       { addon.before_before_script }
+
+  it_behaves_like 'compiled script' do
+    let(:code) { ['curl -sSLo $HOME/.sonarscanner/sonar-scanner.zip'] }
+  end
+
+  describe 'scanner installation' do
+    it { should include_sexp [:export, ['SONAR_SCANNER_HOME', '$HOME/.sonarscanner/sonar-scanner-2.6.1'], {:echo=>true}] }
+    it { should include_sexp [:export, ['SONAR_SCANNER_OPTS', "\"$SONAR_SCANNER_OPTS -Dsonar.host.url=https://nemo.sonarqube.org\""], {:echo=>true}] }
+    it { should include_sexp [:export, ['MAVEN_OPTS', "\"$MAVEN_OPTS -Dsonar.host.url=https://nemo.sonarqube.org\""], {:echo=>true}] }
+    it { should include_sexp [:export, ['GRADLE_OPTS', "\"$GRADLE_OPTS -Dsonar.host.url=https://nemo.sonarqube.org\""], {:echo=>true}] }
+    it { should include_sexp [:export, ['PATH', "\"$PATH:$HOME/.sonarscanner/sonar-scanner-2.6.1/bin\""]] }
+  end
+end
+
