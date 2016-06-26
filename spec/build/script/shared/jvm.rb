519ec9fa5290bf59f627c5d126c5530ae52db9b1
Benedikt Ritter <beneritter@gmail.com>
2016-06-26 16:50:15 +0200

    Add support for maven wrapper

--- spec/build/script/shared/jvm.rb
+++ spec/build/script/shared/jvm.rb
@@ -15,6 +15,11 @@ shared_examples_for 'a jvm build sexp' do
       expect(branch).to include_sexp([:cmd, 'gradle assemble', options])
     end
 
+    it 'runs `./mvnw install` if mvnw exists' do
+      branch = sexp_find(sexp, [:elif, '-f mvnw'])
+      expect(branch).to include_sexp([:cmd, './mvnw install -DskipTests=true -Dmaven.javadoc.skip=true -B -V', options])
+    end
+
     it 'runs `mvn install` if pom.xml exists' do
       branch = sexp_find(sexp, [:elif, '-f pom.xml'])
       expect(branch).to include_sexp([:cmd, 'mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V', options])
@@ -33,6 +38,11 @@ shared_examples_for 'a jvm build sexp' do
     it 'runs `gradle check` if build.gradle exists' do
       branch = sexp_find(sexp, [:elif, '-f build.gradle'])
       expect(branch).to include_sexp([:cmd, 'gradle check', options])
+    end
+
+    it 'runs `./mvnw test` if mvnw exists' do
+      branch = sexp_find(sexp, [:elif, '-f mvnw'])
+      expect(branch).to include_sexp([:cmd, './mvnw test -B', options])
     end
 
     it 'runs `mvn test` if pom.xml exists' do
