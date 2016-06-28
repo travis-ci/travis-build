144af92b395f34662e89b0c0a108cdca99faafe4
Hiro Asari <asari.ruby@gmail.com>
2016-06-28 09:10:37 -0400

    Skip updating initramfs config on OS X

--- spec/build/script/shared/appliances/disable_initramfs.rb
+++ spec/build/script/shared/appliances/disable_initramfs.rb
@@ -1,5 +1,5 @@
 shared_examples_for 'disables updating initramfs' do
-  let(:disable_initramfs) { %(echo update_initramfs=no | sudo tee -a /etc/initramfs-tools/update-initramfs.conf > /dev/null) }
+  let(:disable_initramfs) { %(if [ ! $(uname|grep Darwin) ]; then echo update_initramfs=no | sudo tee -a /etc/initramfs-tools/update-initramfs.conf > /dev/null; fi) }
 
   it 'disables updating initramfs' do
     should include_sexp [:raw, disable_initramfs]
