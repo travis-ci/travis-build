92fa94ae548a545adf5e3c03b335ad20d49f2436
Hiro Asari <asari.ruby@gmail.com>
2016-07-05 15:23:22 -0400

    Fix export value

--- lib/travis/build/appliances/clean_up_path.rb
+++ lib/travis/build/appliances/clean_up_path.rb
@@ -5,7 +5,7 @@ module Travis
     module Appliances
       class CleanUpPath < Base
         def apply
-          sh.export 'PATH', "echo $PATH | sed -e 's/::/:/'", echo: false
+          sh.export 'PATH', "$(echo $PATH | sed -e 's/::/:/')", echo: false
         end
       end
     end
