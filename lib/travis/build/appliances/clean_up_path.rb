dd85a9368413a0da1ead220e83ed7592d69f85e9
Hiro Asari <asari.ruby@gmail.com>
2016-07-05 15:15:12 -0400

    Introduce CleanUpPath appliance
    This appliance removes `::` from `$PATH`, so that the current
    directory is not on `$PATH`.
    
    See https://github.com/travis-ci/travis-ci/issues/5363
    
    The real fix for the issue is to fix the cookbooks, but this
    appliance will do while we wait for the update.
    When the update is rolled out, this addon will be a noop, and can
    be safely removed.

---
+++ lib/travis/build/appliances/clean_up_path.rb
@@ -0,0 +1,13 @@
+require 'travis/build/appliances/base'
+
+module Travis
+  module Build
+    module Appliances
+      class CleanUpPath < Base
+        def apply
+          sh.export 'PATH', "echo $PATH | sed -e 's/::/:/'", echo: false
+        end
+      end
+    end
+  end
+end
---
+++ spec/build/script/shared/appliances/clean_up_path.rb
@@ -0,0 +1,7 @@
+shared_examples_for 'cleans up $PATH' do
+  let(:fix_etc_hosts) { "sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 '`hostname`'/' -i'.bak' /etc/hosts" }
+
+  it 'adds an entry to /etc/hosts for localhost' do
+    should include_sexp [ :export, ['PATH', "echo $PATH | sed -e 's/::/:/'" ] ]
+  end
+end
