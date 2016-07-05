ccfa5185b0674ae60fe665115b93659be72794e4
Hiro Asari <asari.ruby@gmail.com>
2016-07-05 15:32:39 -0400

    Replace '::' everywhere

--- spec/build/script/shared/appliances/clean_up_path.rb
+++ spec/build/script/shared/appliances/clean_up_path.rb
@@ -2,6 +2,6 @@ shared_examples_for 'cleans up $PATH' do
   let(:fix_etc_hosts) { "sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 '`hostname`'/' -i'.bak' /etc/hosts" }
 
   it 'adds an entry to /etc/hosts for localhost' do
-    should include_sexp [ :export, ['PATH', "$(echo $PATH | sed -e 's/::/:/')" ] ]
+    should include_sexp [ :export, ['PATH', "$(echo $PATH | sed -e 's/::/:/g')" ] ]
   end
 end
