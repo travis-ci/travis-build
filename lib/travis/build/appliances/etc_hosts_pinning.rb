0ace168015e4b027d3db3fc41d82ad2b907e1bf0
Henrik Hodne <henrik@travis-ci.com>
2016-07-06 11:56:11 +0200

    Add feature to temporarily pin hostnames to IP addresses
    This is mainly useful as temporary workarounds if a certain mirror is
    broken, or something similar.

---
+++ lib/travis/build/appliances/etc_hosts_pinning.rb
@@ -0,0 +1,20 @@
+require 'shellwords'
+require 'travis/build/appliances/base'
+
+module Travis
+  module Build
+    module Appliances
+      class EtcHostsPinning < Base
+        def apply
+          ENV['ETC_HOSTS_PINNING'].split(',').each do |etchostsline|
+            sh.raw %(echo #{Shellwords.escape(etchostsline)} | sudo tee -a /etc/hosts)
+          end
+        end
+
+        def apply?
+          ENV.key?('ETC_HOSTS_PINNING')
+        end
+      end
+    end
+  end
+end
---
+++ spec/build/script/shared/appliances/etc_hosts_pinning.rb
@@ -0,0 +1,10 @@
+shared_examples_for '/etc/hosts pinning' do
+  before do
+    ENV['ETC_HOSTS_PINNING'] = '127.0.0.1 foo,0.0.0.0 bar'
+  end
+
+  it 'writes to /etc/hosts' do
+    should include_sexp [:raw, 'echo 127.0.0.1\\ foo | sudo tee -a /etc/hosts']
+    should include_sexp [:raw, 'echo 0.0.0.0\\ bar | sudo tee -a /etc/hosts']
+  end
+end
