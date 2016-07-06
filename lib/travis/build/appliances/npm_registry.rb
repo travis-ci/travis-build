9ab0792702951eb0bc6cba00e8d38511de7f64c3
Igor Wiedler <igor@travis-ci.org>
2016-07-06 14:27:47 +0200

    get npm_registry key from data top-level instead of config, we can feature toggle it in scheduler

--- lib/travis/build/appliances/npm_registry.rb
+++ lib/travis/build/appliances/npm_registry.rb
@@ -5,9 +5,9 @@ module Travis
     module Appliances
       class NpmRegistry < Base
         def apply
-          if config[:npm_registry]
+          if data[:npm_registry]
             sh.fold "npm_registry" do
-              sh.cmd "echo registry=#{config[:npm_registry]} > ~/.npmrc"
+              sh.cmd "echo registry=#{data[:npm_registry]} > ~/.npmrc"
             end
           end
         end
