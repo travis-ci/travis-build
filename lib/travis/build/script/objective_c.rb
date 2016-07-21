dd1662e7d741ec3ece7290b51e9c5639d45cbf38
Hiro Asari <asari.ruby@gmail.com>
2016-07-21 11:01:54 -0400

    Fix bash syntax error with default `install` in Objective-C builds
    `#elif` requires a reasonable conditional; without it, we fail with
    
    ```
    /Users/travis/build.sh: line 389: syntax error in conditional expression
    /Users/travis/build.sh: line 389: syntax error near `:echo=>'
    /Users/travis/build.sh: line 389: `      elif [[ {:assert=>true,
    :echo=>true, :timing=>true} ]]; then'
    ```

--- lib/travis/build/script/objective_c.rb
+++ lib/travis/build/script/objective_c.rb
@@ -67,7 +67,7 @@ module Travis
                   sh.cmd 'bundle exec pod install', retry: true
                 end
 
-                sh.elif do
+                sh.else do
                   sh.echo "Installing Pods with 'pod install'", ansi: :yellow
                   sh.cmd 'pod install', retry: true
                 end
