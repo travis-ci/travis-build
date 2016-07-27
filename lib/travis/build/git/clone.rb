ca4d2232f539b17908ba3822ef3840065b4a6891
Hiro Asari <asari.ruby@gmail.com>
2016-07-27 16:46:05 -0400

    Fix chmod invocation

--- lib/travis/build/git/clone.rb
+++ lib/travis/build/git/clone.rb
@@ -77,7 +77,7 @@ module Travis
           def write_netrc
             if data.prefer_https?
               sh.raw "echo -e \"machine github.com\n  login #{data[:oauth_token]}\\n\" > $HOME/.netrc"
-              sh.raw "chmod 0600"
+              sh.raw "chmod 0600 $HOME/.netrc"
             end
           end
 
