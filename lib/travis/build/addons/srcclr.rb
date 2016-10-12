a99f0729e8234db0ef8212ae468866ba98602bb1
Hiro Asari <asari.ruby@gmail.com>
2016-10-12 14:35:50 -0400

    Read SourceClear API token from config
    If it's not empty, it defers to SRCCLR_API_TOKEN.

--- lib/travis/build/addons/srcclr.rb
+++ lib/travis/build/addons/srcclr.rb
@@ -11,7 +11,24 @@ module Travis
         def before_finish
           sh.if('$TRAVIS_TEST_RESULT = 0') do
             sh.newline
-            sh.fold 'after_success' do
+            sh.fold 'srcclr' do
+              if api_token.empty?
+                sh.if "-z $SRCCLR_API_TOKEN" do
+                  sh.echo "SRCCLR_API_TOKEN is empty and api_token is not set in .travis.yml", ansi: :red
+                end
+                sh.else do
+                  sh.echo "Using SRCCLR_API_TOKEN", ansi: :yellow
+                end
+              else
+                sh.if "-n $SRCCLR_API_TOKEN" do
+                  sh.echo "SRCCLR_API_TOKEN is set and is used instead of api_token in .travis.yml", ansi: :yellow
+                end
+                sh.else do
+                  sh.echo "Using api_token in .travis.yml", ansi: :yellow
+                end
+              end
+
+              sh.export 'SRCCLR_API_TOKEN', "${SRCCLR_API_TOKEN:-#{api_token}}", echo: false
               sh.echo "Running SourceClear agent", ansi: :yellow
               debug_env = debug? ? "env DEBUG=1" : ""
 
@@ -22,6 +39,10 @@ module Travis
 
         def debug?
           config[:debug]
+        end
+
+        def api_token
+          config.fetch(:api_token, '')
         end
       end
     end
