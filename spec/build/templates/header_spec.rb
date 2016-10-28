328aece035466bc52fe2b9bd7a49cbc3081b1f94
Dan Buch <dan@travis-ci.org>
2016-10-28 10:25:14 -0400

    Add specs for internal ruby selection

--- spec/build/templates/header_spec.rb
+++ spec/build/templates/header_spec.rb
@@ -61,4 +61,78 @@ describe 'header.sh', integration: true do
       expect(bash_output).to match(/^#{api_function} is a function/)
     end
   end
+
+  describe 'internal ruby selection' do
+    let(:rubies) { [] }
+
+    let :bash_body do
+      <<-EOF.gsub(/^\s+> ?/, '')
+        > rvm() {
+        >   cat <<EORVM
+        > #{rubies.join("\n")}
+        > EORVM
+        > }
+        >
+        > echo $(travis_internal_ruby)
+      EOF
+    end
+
+    context 'with a typical selection of preinstalled rubies' do
+      let :rubies do
+        [
+          'ruby-1.7.19-p000',
+          'ruby-1.8.7-p000',
+          'ruby-1.8.7-p000',
+          'ruby-1.9.2-p000',
+          'ruby-1.9.3-p000',
+          'ruby-2.0.0 [ x86_64 ]',
+          'ruby-2.1.2 [ x86_64 ]',
+          'ruby-2.1.3 [ x86_64 ]',
+          'ruby-2.1.4 [ x86_64 ]',
+          'ruby-2.1.5 [ x86_64 ]',
+          'ruby-2.2.0 [ x86_64 ]',
+          'ruby-2.2.5 [ x86_64 ]',
+          'ruby-2.3.1 [ x86_64 ]'
+        ]
+      end
+
+      it 'selects the latest valid version' do
+        expect(bash_output.strip).to eq('2.2.5')
+      end
+    end
+
+    context 'when the most recent version of ruby is 1.9.3' do
+      let :rubies do
+        [
+          'ruby-1.7.19-p000',
+          'ruby-1.8.7-p000',
+          'ruby-1.8.7-p000',
+          'ruby-1.9.2-p000',
+          'ruby-1.9.3-p000'
+        ]
+      end
+
+      it 'selects 1.9.3' do
+        expect(bash_output.strip).to eq('1.9.3')
+      end
+    end
+
+    context 'when the most recent version of ruby is 2.1.10' do
+      let :rubies do
+        [
+          'ruby-1.9.3-p000',
+          'ruby-2.0.0 [ x86_64 ]',
+          'ruby-2.1.2 [ x86_64 ]',
+          'ruby-2.1.3 [ x86_64 ]',
+          'ruby-2.1.4 [ x86_64 ]',
+          'ruby-2.1.5 [ x86_64 ]',
+          'ruby-2.1.10 [ x86_64 ]'
+        ]
+      end
+
+      it 'selects 2.1.5' do
+        expect(bash_output.strip).to eq('2.1.5')
+      end
+    end
+  end
 end
