1fd023d5d920f4a81d8bb39e37bdc708e727ec90
Hiro Asari <asari.ruby@gmail.com>
2016-11-11 10:48:26 -0500

    Restructure macOS Sierra detection logic
    Instead of building a long bash `if` command, we build up an equivalent
    sexp so that the spec can remain reasonably close.

--- lib/travis/build/appliances/disable_ssh_roaming.rb
+++ lib/travis/build/appliances/disable_ssh_roaming.rb
@@ -5,14 +5,12 @@ module Travis
     module Appliances
       class DisableSshRoaming < Base
         def apply
-          sh.cmd <<-EOF
-if [ $(sw_vers -productVersion | cut -d . -f 2) -lt 12 ]; do
-  mkdir -p $HOME/.ssh
-  chmod 0700 $HOME/.ssh
-  touch $HOME/.ssh/config
-  echo -e "Host *\n  UseRoaming no\n" | cat - $HOME/.ssh/config > $HOME/.ssh/config.tmp && mv $HOME/.ssh/config.tmp $HOME/.ssh/config
-fi
-          EOF
+          sh.if "$(sw_vers -productVersion | cut -d . -f 2) -lt 12" do
+            sh.cmd %(mkdir -p $HOME/.ssh)
+            sh.cmd %(chmod 0700 $HOME/.ssh)
+            sh.cmd %(touch $HOME/.ssh/config)
+            sh.cmd %(echo -e "Host *\n  UseRoaming no\n" | cat - $HOME/.ssh/config > $HOME/.ssh/config.tmp && mv $HOME/.ssh/config.tmp $HOME/.ssh/config)
+          end
         end
       end
     end
