11e36ca10d4a871e4f0409f62a46e150ffdd3d47
Dan Buch <dan@travis-ci.org>
2016-12-15 13:45:01 -0500

    Add some explicit ENV.fetch for backward compat

--- lib/travis/build/config.rb
+++ lib/travis/build/config.rb
@@ -20,37 +20,47 @@ module Travis
       end
 
       define(
-        app_host: '',
+        api_token: ENV.fetch(
+          'TRAVIS_BUILD_API_TOKEN', ENV.fetch('API_TOKEN', '')
+        ),
+        app_host: ENV.fetch('TRAVIS_BUILD_APP_HOST', ''),
         apt_package_whitelist: {
-          precise: '',
-          trusty: ''
+          precise: ENV.fetch('TRAVIS_BUILD_APT_PACKAGE_WHITELIST_PRECISE', ''),
+          trusty: ENV.fetch('TRAVIS_BUILD_APT_PACKAGE_WHITELIST_TRUSTY', '')
         },
         apt_source_whitelist: {
-          precise: '',
-          trusty: ''
+          precise: ENV.fetch('TRAVIS_BUILD_APT_SOURCE_WHITELIST_PRECISE', ''),
+          trusty: ENV.fetch('TRAVIS_BUILD_APT_SOURCE_WHITELIST_TRUSTY', '')
         },
-        apt_whitelist_skip: '',
-        ghc_default: '7.8.4',
+        apt_whitelist_skip: ENV.fetch('TRAVIS_BUILD_APT_WHITELIST_SKIP', ''),
+        ghc_default: ENV.fetch('TRAVIS_BUILD_GHC_DEFAULT', '7.8.4'),
         gimme: {
-          force_reinstall: '',
-          url: 'https://raw.githubusercontent.com/travis-ci/gimme/v1.0.0/gimme'
+          force_reinstall: ENV.fetch('TRAVIS_BUILD_GIMME_FORCE_REINSTALL', ''),
+          url: ENV.fetch(
+            'TRAVIS_BUILD_GIMME_URL',
+            'https://raw.githubusercontent.com/travis-ci/gimme/v1.0.0/gimme'
+          )
         },
-        go_version: '1.7.4',
-        go_version_aliases: {
-          '1' => '1.7.4',
-          '1.0' => '1.0.3',
-          '1.0.x' => '1.0.3',
-          '1.1.x' => '1.1.2',
-          '1.2' => '1.2.2',
-          '1.2.x' => '1.2.2',
-          '1.3.x' => '1.3.3',
-          '1.4.x' => '1.4.3',
-          '1.5.x' => '1.5.4',
-          '1.6.x' => '1.6.3',
-          '1.7.x' => '1.7.4',
-          '1.x' => '1.7.4',
-          '1.x.x' => '1.7.4'
-        }.map { |k, v| "#{k}:#{v}" }.join(',')
+        go_version: ENV.fetch('TRAVIS_BUILD_GO_VERSION', '1.7.4'),
+        go_version_aliases: ENV.fetch(
+          'TRAVIS_BUILD_GO_VERSION_ALIASES', (
+            {
+              '1' => '1.7.4',
+              '1.0' => '1.0.3',
+              '1.0.x' => '1.0.3',
+              '1.1.x' => '1.1.2',
+              '1.2' => '1.2.2',
+              '1.2.x' => '1.2.2',
+              '1.3.x' => '1.3.3',
+              '1.4.x' => '1.4.3',
+              '1.5.x' => '1.5.4',
+              '1.6.x' => '1.6.3',
+              '1.7.x' => '1.7.4',
+              '1.x' => '1.7.4',
+              '1.x.x' => '1.7.4'
+            }.map { |k, v| "#{k}:#{v}" }.join(',')
+          )
+        )
       )
 
       default(
