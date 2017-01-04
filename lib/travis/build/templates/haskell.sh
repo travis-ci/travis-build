d63750dcde72d9955b6bb22090cecb8d9a4f2114
Dan Buch <dan@travis-ci.org>
2017-01-04 12:50:01 -0500

    Dynamic haskell touchups

--- lib/travis/build/templates/haskell.sh
+++ lib/travis/build/templates/haskell.sh
@@ -11,8 +11,7 @@ function travis_ghc_find() {
   local search="${1}"
   local v
   if [[ ! "${search}" ]]; then
-    echo "${TRAVIS_GHC_DEFAULT}"
-    return 1
+    echo -e "${ANSI_RED}No ghc version given.${ANSI_RESET}" >&2
   else
     for v in "${TRAVIS_GHC_ROOT}"/*/; do
       v=${v%%/}
@@ -25,20 +24,22 @@ function travis_ghc_find() {
         return 0
       fi
     done
-    echo "travis_ghc_find: error, no such version ${search}" >&2
-    echo "travis_ghc_find: using default version ${TRAVIS_GHC_VERSIONS}" >&2
-    echo "${TRAVIS_GHC_DEFAULT}"
-    return 1
+    echo -e "${ANSI_RED}No such ghc version '${search}'.${ANSI_RESET}" >&2
   fi
+  echo -e "${ANSI_YELLOW}Using default ghc version '${TRAVIS_GHC_DEFAULT}'.${ANSI_RESET}" >&2
+  echo "${TRAVIS_GHC_DEFAULT}"
+  return 1
 }
 
 function travis_ghc_install() {
   local ghc_version="${1}"
   local cabal_version="${2}"
   if [[ ! "${ghc_version}" ]]; then
+    echo -e "\n${ANSI_RED}No ghc version given.${ANSI_RESET}" >&2
     return 1
   fi
   if [[ ! "${cabal_version}" ]]; then
+    echo -e "\n${ANSI_RED}No cabal version given.${ANSI_RESET}" >&2
     return 1
   fi
   if ! sudo date &>/dev/null; then
@@ -46,16 +47,18 @@ function travis_ghc_install() {
     return 1
   fi
   if [[ ! -f '<%= root %>/etc/apt/sources.list.d/hvr-ghc.list' ]]; then
-    echo -e "\n${ANSI_GREEN}Adding ppa:hvr/ghc.${ANSI_RESET}"
+    echo -e "\n${ANSI_GREEN}Adding ppa:hvr/ghc.${ANSI_RESET}" >&2
     sudo apt-add-repository -yq ppa:hvr/ghc
   fi
   sudo apt-get update -yqq
   if sudo apt-get install -yq "ghc-${ghc_version}"; then
-    echo -e "\n${ANSI_GREEN}Successfully installed ghc-${ghc_version}.${ANSI_RESET}"
-    if sudo apt-get install -yq "cabal-install-${cabal_version}"; then
-      echo -e "\n${ANSI_GREEN}Successfully installed cabal-install-${cabal_version}.${ANSI_RESET}"
-      return 0
-    fi
+    echo -e "\n${ANSI_GREEN}Successfully installed 'ghc-${ghc_version}'.${ANSI_RESET}" >&2
+  else
+    return 1
+  fi
+  if sudo apt-get install -yq "cabal-install-${cabal_version}"; then
+    echo -e "\n${ANSI_GREEN}Successfully installed 'cabal-install-${cabal_version}'.${ANSI_RESET}" >&2
+    return 0
   fi
   return 1
 }
