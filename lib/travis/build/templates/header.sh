e1f097275d04be5f734c6f939a16b59c60714a80
Hiro Asari <asari.ruby@gmail.com>
2017-01-30 10:02:59 -0500

    Point to our documentation when secure var fails to be set

--- lib/travis/build/templates/header.sh
+++ lib/travis/build/templates/header.sh
@@ -61,7 +61,7 @@ travis_cmd() {
     if [[ -n $secure && $result -ne 0 ]]; then
       echo -e "${ANSI_RED}The previous command failed, possibly due to a malformed secure environment variable.${ANSI_CLEAR}
 ${ANSI_RED}Please be sure to escape special characters such as ' ' and '$'.${ANSI_CLEAR}
-${ANSI_RED}For more information, see http://www.tldp.org/LDP/abs/html/special-chars.html.${ANSI_CLEAR}"
+${ANSI_RED}For more information, see https://docs.travis-ci.com/user/encryption-keys.${ANSI_CLEAR}"
     fi
   fi
 
