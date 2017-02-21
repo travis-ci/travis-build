2b04abe3b2745ee002f5de40ebf9414ac53b8839
Dominic Jodoin <dominic@travis-ci.com>
2017-02-20 22:11:15 -0500

    Call apt-get update after clearing the cache (#964)
    Ensure apt-get update runs *after* sources are fixed

--- lib/travis/build/appliances/fix_rwky_redis.rb
+++ lib/travis/build/appliances/fix_rwky_redis.rb
@@ -6,10 +6,12 @@ module Travis
       class FixRwkyRedis < Base
         def apply
           command = <<-EOF
+            sudo rm -rf /var/lib/apt/lists/*
             for f in $(grep -l rwky/redis /etc/apt/sources.list.d/*); do
               sed 's,rwky/redis,rwky/ppa,g' $f > /tmp/${f##**/}
               sudo mv /tmp/${f##**/} /etc/apt/sources.list.d
             done
+            sudo apt-get update -qq 2>&1 >/dev/null
           EOF
           sh.cmd command, echo: false
         end
