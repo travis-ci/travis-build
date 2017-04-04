04116adc046c8a2c59d393d775a9d11dd5373e6f
Hiro Asari <asari.ruby@gmail.com>
2017-04-04 15:03:27 -0400

    Make #app_host private

--- lib/travis/build/script.rb
+++ lib/travis/build/script.rb
@@ -111,10 +111,6 @@ module Travis
         ! data.debug_options.empty?
       end
 
-      def app_host
-        @app_host ||= Travis::Build.config.app_host.to_s.strip.untaint
-      end
-
       private
 
         def config
@@ -238,6 +234,10 @@ module Travis
 
         def debug_enabled?
           Travis::Build.config.enable_debug_tools == '1'
+        end
+
+        def app_host
+          @app_host ||= Travis::Build.config.app_host.to_s.strip.untaint
         end
     end
   end
