364f9ec3c5022b13afb2b96c7c564c474c750d98
Sven Fuchs <me@svenfuchs.com>
2017-05-04 15:33:34 +0200

    Redirect stdin/stderr instead of setting up a new PTY to re-run the build script

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -4,18 +4,11 @@ require 'timeout'
 require_relative '../public/filter'
 
 describe Filter do
+  let(:cmd) { File.expand_path('../../public/filter.rb', __FILE__) }
   def filter(input, *secrets)
-    path = File.expand_path('../../public/filter.rb', __FILE__)
-    secrets = secrets.map { |s| "-s #{Shellwords.escape(s)}" }.join " "
-    `ruby #{path} echo\\ #{Shellwords.escape(input)} #{secrets}`.chomp
-  end
-
-  def with_timeout(command, timeout)
-    output = ""
-    Timeout.timeout(timeout) { Filter::Runner.new(command).read { |c| output << c} }
-    output
-  rescue Timeout::Error
-    output
+    args  = secrets.map { |s| "-s #{Shellwords.escape(s)}" }.join " "
+    input = Shellwords.escape(input)
+    `echo #{input} | ruby #{cmd} #{args}`.chomp
   end
 
   describe 'simple replacements' do
@@ -26,25 +19,16 @@ describe Filter do
     example { expect(filter('abcdef', 'a', 'bc', 'def')).to be == 'abc[secure]' }
   end
 
-  describe 'sets the status code correctly' do
-    example do
-      with_timeout('true', 1)
-      expect($?.exitstatus).to be == 0
-    end
-
-    example do
-      with_timeout('false', 1)
-      expect($?.exitstatus).to be == 1
-    end
-
-    example do
-      with_timeout('sleep 0.2; exit 128', 1)
-      expect($?.exitstatus).to be == 128
-    end
-  end
-
   it 'live streams' do
-    command = %q[ruby public/filter.rb 'ruby -e "print :foo; sleep 0.01; print :bar; sleep"']
-    expect(with_timeout(command, 2)).to be == 'foobar'
+    code = %(
+      print 'foo'
+      sleep 0.01
+      print 'bar'
+      sleep 0.01
+      print 'baz'
+      sleep 0.01
+    )
+    output = `ruby -e "#{code}" | ruby #{cmd} -s bar`
+    expect(output).to be == 'foo[secure]baz'
   end
 end
