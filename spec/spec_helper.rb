8bd81eac750b67fa09f016471964872e7ecc9bd1
Sven Fuchs <me@svenfuchs.com>
2017-05-16 15:39:47 +0200

    add an incremental rollout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ integration_enabled = ENV['INTEGRATION_SPECS'] == '1'
 ENV['TOP'] = `git rev-parse --show-toplevel`.strip if integration_enabled
 
 RSpec.configure do |c|
+  c.include SpecHelpers::Logger
   c.include SpecHelpers::Payload
   c.include SpecHelpers::Node, :include_node_helpers
   c.include SpecHelpers::Sexp, :sexp
