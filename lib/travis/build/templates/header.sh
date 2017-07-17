d49724dc1f6dcae3e3cc96342aa1e54bc97c0e2e
Joep van Delft <joep@travis-ci.org>
2017-07-17 15:59:30 +0200

    Fix typo

--- lib/travis/build/templates/header.sh
+++ lib/travis/build/templates/header.sh
@@ -176,7 +176,7 @@ travis_terminate() {
       && command exec 1>&9 2>&9 9>&- \
       && sync
   pgrep -u $USER | grep -v -w $$ > $TRAVIS_TMPDIR/pids_after
-  kill -9 $(awk 'NR==FNR{a[$1]++;next};!($1 in a)' <$TRAVIS_TMPDIR/pids_{before,after})
+  kill -9 $(awk 'NR==FNR{a[$1]++;next};!($1 in a)' $TRAVIS_TMPDIR/pids_{before,after}) || true
   pkill -9 -P $$ &> /dev/null || true
   exit $1
 }
