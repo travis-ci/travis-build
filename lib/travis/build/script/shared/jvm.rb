ea2fc72db607dc78309bc86474bfdc50f4e38ade
Katherine Daniels <kdaniels@travis-ci.org>
2017-08-04 15:26:06 +0200

    Add the actual /bin to the /usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/kdaniels/travis/trvs/bin:/usr/local/opt/go/libexec/bin

--- lib/travis/build/script/shared/jvm.rb
+++ lib/travis/build/script/shared/jvm.rb
@@ -18,7 +18,7 @@ module Travis
           CLEANUPS.each do |find_arg|
             sh.raw "find #{find_arg[:directory]} -name #{find_arg[:glob]} -delete 2>/dev/null"
           end
-          sh.export 'PATH', '$JAVA_HOME:$PATH'
+          sh.export 'PATH', '$JAVA_HOME/bin:$PATH'
         end
 
         def install
