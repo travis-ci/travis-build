d74a828885b878c87b7932de7c369b9b6f0ecf80
Joep van Delft <joep@travis-ci.org>
2017-10-09 11:39:17 +0200

    Update test description

--- spec/build/script/shared/appliances/rm_mongodb32_source.rb
+++ spec/build/script/shared/appliances/rm_mongodb32_source.rb
@@ -1,5 +1,5 @@
 shared_examples_for 'remove mongodb 3.2 source' do
-  it 'adds an entry to /etc/hosts for localhost' do
+  it 'removes mongodb source due to an outdated key' do
     should include_sexp [:cmd, 'rm -f /etc/apt/sources.list.d/mongodb-3.2.list', echo: false, assert: false, sudo: true]
   end
 end
