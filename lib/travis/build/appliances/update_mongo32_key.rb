bacdca44db3e3f7be37391d6b78e7984d86b7e51
Bogdana Vereha <bogdana.vereha@gmail.com>
2017-10-10 14:17:06 +0200

    Fix class name

--- lib/travis/build/appliances/update_mongo32_key.rb
+++ lib/travis/build/appliances/update_mongo32_key.rb
@@ -3,7 +3,7 @@ require 'travis/build/appliances/base'
 module Travis
   module Build
     module Appliances
-      class RmMongodb32Source < Base
+      class UpdateMongodb32Key < Base
         def apply
           sh.if "$(command -v lsb_release)" do
             sh.cmd 'apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927', echo: false, assert: false, sudo: true
