cd6c69fe65f559ace7cec39958c2f1ed53b0a46c
Bogdana Vereha <bogdana.vereha@gmail.com>
2017-10-10 12:34:29 +0200

    Remove previous mongo32 source removal and spec

--- spec/build/script/shared/appliances/rm_mongodb32_source.rb
+++
@@ -1,5 +0,0 @@
-shared_examples_for 'remove mongodb 3.2 source' do
-  it 'removes mongodb source due to an outdated key' do
-    should include_sexp [:cmd, 'rm -f /etc/apt/sources.list.d/mongodb-3.2.list', echo: false, assert: false, sudo: true]
-  end
-end
