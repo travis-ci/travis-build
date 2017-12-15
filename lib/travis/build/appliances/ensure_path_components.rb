985787e07cad63b7b4310a8c8f994cf189b9d79f
Hiro Asari <asari.ruby@gmail.com>
2017-12-15 08:35:32 -0500

    Find path component in `$PATH` with `=~`
    To prevent potential leak to STDOUT

--- lib/travis/build/appliances/ensure_path_components.rb
+++ lib/travis/build/appliances/ensure_path_components.rb
@@ -13,7 +13,7 @@ module Travis
           COMPONENTS.each do |pc|
             sh.cmd <<-EOF
 pc=#{pc}
-if [[ -n $pc && -z $(echo :$PATH: | grep :$pc:) ]]; then export PATH=$PATH:$pc; fi
+if [[ -n $pc && :$PATH: =~ :$pc: ]]; then export PATH=$PATH:$pc; fi
 unset pc
             EOF
           end
