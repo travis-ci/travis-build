e886a353e9e78050edd18c6a5deb209bacbf0287
Joep van Delft <joep@travis-ci.org>
2018-01-15 18:03:07 +0100

    Move nonblock to own time and place

--- lib/travis/build/appliances/nonblock_pipe.rb
+++ lib/travis/build/appliances/nonblock_pipe.rb
@@ -6,7 +6,25 @@ module Travis
       class NonblockPipe < Base
 
         def apply
-          sh.cmd '[[ $TRAVIS_FILTERED = redirect_io ]] && python ~/nonblock.py'
+          command = <<-EOF
+if [[ $TRAVIS_FILTERED = redirect_io ]]; then
+  cat <<\\EOPY >~/nonblock.py
+import os
+import sys
+import fcntl
+
+flags_stdout = fcntl.fcntl(sys.stdout, fcntl.F_GETFL)
+fcntl.fcntl(sys.stdout, fcntl.F_SETFL, flags_stdout&~os.O_NONBLOCK)
+
+flags_stderr = fcntl.fcntl(sys.stderr, fcntl.F_GETFL)
+fcntl.fcntl(sys.stderr, fcntl.F_SETFL, flags_stderr&~os.O_NONBLOCK)
+EOPY
+  python ~/nonblock.py
+  rm ~/nonblock.py
+fi
+EOF
+
+          sh.cmd command
         end
 
       end
