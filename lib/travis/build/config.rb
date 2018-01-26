8ee5ace69b6e99341085f747f1d1ac117dad47e0
Dan Buch <dan@meatballhat.com>
2018-01-26 09:43:19 -0500

    Bump fallback gimme install URL to v1.3.0

--- lib/travis/build/config.rb
+++ lib/travis/build/config.rb
@@ -42,7 +42,7 @@ module Travis
         gimme: {
           url: ENV.fetch(
             'TRAVIS_BUILD_GIMME_URL',
-            'https://raw.githubusercontent.com/travis-ci/gimme/v1.2.0/gimme'
+            'https://raw.githubusercontent.com/travis-ci/gimme/v1.3.0/gimme'
           )
         },
         go_version: ENV.fetch('TRAVIS_BUILD_GO_VERSION', '1.9'),
