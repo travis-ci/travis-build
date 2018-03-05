6a0634fd23609c911bab966060a66f643cb0fd8d
Dan Buch <dan@meatballhat.com>
2018-03-05 14:49:36 -0500

    Use a bash while loop + vars to retry downloads
    while waiting for network

--- lib/travis/build/templates/header.sh
+++ lib/travis/build/templates/header.sh
@@ -270,12 +270,12 @@ travis_download() {
   local dst="${2}"
 
   if curl --version &>/dev/null; then
-    curl -fsSL --connect-timeout=5 --retry=9 --retry-delay=1 -o "${dst}" "${src}" 2>/dev/null
+    curl -fsSL --connect-timeout=5 -o "${dst}" "${src}" 2>/dev/null
     return $?
   fi
 
   if wget --version &>/dev/null; then
-    wget --connect-timeout=5 --tries=10 --wait=1 -q "${src}" -O "${dst}" 2>/dev/null
+    wget --connect-timeout=5 -q "${src}" -O "${dst}" 2>/dev/null
     return $?
   fi
 
@@ -283,12 +283,21 @@ travis_download() {
 }
 
 travis_wait_for_network() {
-  local url="<%= app_host %>/empty.txt"
+  local count=1
+  local url="http://<%= app_host %>/empty.txt"
   url="${url}?job_id=${1:-${TRAVIS_JOB_ID}}"
   url="${url}&repo=${2:-${TRAVIS_REPO_SLUG}}"
-  echo "$(date +%s) start  ${url}" >"<%= home %>/.travis-network.log"
-  travis_download "${url}" /dev/null
-  echo "$(date +%s) finish ${url}" >>"<%= home %>/.travis-network.log"
+
+  while [[ "${count}" -lt 10 ]]; do
+    echo "$(date +%s) start url=${url} count=${count}" >>"<%= home %>/.travis-network.log"
+    if ! travis_download "${url}?count=${count}" /dev/null; then
+      echo "$(date +%s) finish url=${url} count=${count}" >>"<%= home %>/.travis-network.log"
+      return
+    fi
+    count=$((count + 1))
+    echo "$(date +%s) retry url=${url} count=${count}" >>"<%= home %>/.travis-network.log"
+    sleep 1
+  done
 }
 
 decrypt() {
