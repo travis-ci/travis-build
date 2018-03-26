03d5ecf62e3a57b1f6fea6c3a95c032a949ff8f3
Hiro Asari <asari.ruby@gmail.com>
2018-03-26 15:00:55 -0400

    Fix raw command indentation
    For readability

--- lib/travis/build/addons/apt_retries.rb
+++ lib/travis/build/addons/apt_retries.rb
@@ -12,21 +12,20 @@ module Travis
 
         def before_configure
           sh.echo "Configuring default apt-get retries", ansi: :yellow
-          set_retries = <<-EOF
+          sh.raw <<~EOF
           if [[ -d /var/lib/apt/lists && -n $(command -v apt-get) ]]; then
-            cat <<-EOS > 99apt
-Acquire {
-  ForceIPv4 "1";
-  Retries "5";
-  https {
-    Timeout "30";
-  };
-};
-EOS
-            sudo mv 99apt /etc/apt/apt.conf.d
+            cat <<-EOS > 99-travis-build-retries
+          Acquire {
+            ForceIPv4 "1";
+            Retries "5";
+            https {
+              Timeout "30";
+            };
+          };
+          EOS
+            sudo mv 99-travis-build-retries /etc/apt/apt.conf.d
           fi
           EOF
-          sh.cmd set_retries
         end
       end
     end
