e698965039cf340abf86a4d8883c325a0e63cdae
Dan Buch <dan@meatballhat.com>
2018-04-03 15:32:51 -0400

    Disable all non-core APT sources by default
    and remove various hacks/workarounds that accomplished a subset of this
    functionality over the years ...

---
+++ lib/travis/build/appliances/disable_apt_sources.rb
@@ -0,0 +1,27 @@
+require 'travis/build/appliances/base'
+
+module Travis
+  module Build
+    module Appliances
+      class DisableAptSources < Base
+        def apply
+          sh.raw <<~BASH
+            travis_disable_apt_sources() {
+              if [[ ! -d /etc/apt/sources.list.d ]]; then
+                return
+              fi
+              local nullglob_state
+              nullglob_state="$(shopt -p nullglob)"
+              shopt -s nullglob
+              for source_file in /etc/apt/sources.list.d/*.list; do
+                sudo mv "${source_file}" "${source_file}.save"
+              done
+              eval "${nullglob_state}"
+            }
+          BASH
+          sh.cmd 'travis_disable_apt_sources'
+        end
+      end
+    end
+  end
+end
