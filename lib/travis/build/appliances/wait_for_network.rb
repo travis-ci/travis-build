4d51cb8e6380b60a65019ff09679324876b4a62f
Sven Fuchs <me@svenfuchs.com>
2018-04-28 17:30:55 +0200

    refactor network availability check

--- lib/travis/build/appliances/wait_for_network.rb
+++ lib/travis/build/appliances/wait_for_network.rb
@@ -4,55 +4,58 @@ module Travis
   module Build
     module Appliances
       class WaitForNetwork < Base
-        def apply
+        FUNC = <<~func
+          travis_wait_for_network() {
+            local wait_retries="${1}"
+            local count=0
+            shift
+            local urls=("${@}")
 
-          sh.raw <<~BASHSNIP
-            travis_wait_for_network() {
-              local wait_retries="${1}"
-              local count=0
-              shift
-              local urls=("${@}")
-
-              while [[ "${count}" -lt "${wait_retries}" ]]; do
-                local confirmed=0
-                for url in "${urls[@]}"; do
-                  if travis_download "${url}" /dev/null; then
-                    confirmed=$((confirmed + 1))
-                  fi
-                done
-
-                if [[ "${#urls[@]}" -eq "${confirmed}" ]]; then
-                  echo -e "${ANSI_GREEN}Network availability confirmed.${ANSI_RESET}"
-                  return
+            while [[ "${count}" -lt "${wait_retries}" ]]; do
+              local confirmed=0
+              for url in "${urls[@]}"; do
+                if travis_download "${url}" /dev/null; then
+                  confirmed=$((confirmed + 1))
                 fi
-
-                count=$((count + 1))
-                sleep 1
               done
 
-              echo -e "${ANSI_RED}Timeout waiting for network availability.${ANSI_RESET}"
-            }
-          BASHSNIP
+              if [[ "${#urls[@]}" -eq "${confirmed}" ]]; then
+                echo -e "${ANSI_GREEN}Network availability confirmed.${ANSI_RESET}"
+                return
+              fi
 
-          sh.cmd %W[
-            travis_wait_for_network
-              '#{wait_retries}' '#{check_urls.join("' '")}'
-          ].join(' ').untaint, echo: false
+              count=$((count + 1))
+              sleep 1
+            done
+
+            echo -e "${ANSI_RED}Timeout waiting for network availability.${ANSI_RESET}"
+          }
+        func
+
+        def apply
+          sh.raw FUNC
+          sh.cmd cmd.untaint, echo: false
         end
 
-        private def check_urls
-          @check_urls ||= Travis::Build.config.network.check_urls.map do |tmpl|
-            tmpl % {
-              app_host: app_host,
-              job_id: data.job[:id],
-              repo: data.slug
-            }
+        private
+
+          def cmd
+            "travis_wait_for_network #{retries} #{quote(urls).join(' ')}"
           end
-        end
 
-        private def wait_retries
-          @wait_retries ||= Integer(Travis::Build.config.network.wait_retries)
-        end
+          def urls
+            urls = Travis::Build.config.network.check_urls
+            args = { app_host: app_host, job_id: data.job[:id], repo: data.slug }
+            urls.map { |tmpl| tmpl % args }
+          end
+
+          def quote(strs)
+            strs.map { |str| "'#{str}'" }
+          end
+
+          def retries
+            Integer(Travis::Build.config.network.wait_retries)
+          end
       end
     end
   end
