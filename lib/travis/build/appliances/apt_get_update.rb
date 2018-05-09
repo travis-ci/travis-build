475f0ef62b34b7f7f729844b3047d8507d916dfd
Sven Fuchs <me@svenfuchs.com>
2018-05-09 11:05:02 +0200

    add debug options via rollout

--- lib/travis/build/appliances/apt_get_update.rb
+++ lib/travis/build/appliances/apt_get_update.rb
@@ -7,10 +7,21 @@ module Travis
         def apply
           sh.cmd <<-EOF
             sudo rm -rf /var/lib/apt/lists/*
-            sudo apt-get update -qq >/dev/null 2>&1
+            sudo apt-get update #{'-qq  >/dev/null 2>&1' unless debug?}
           EOF
         end
+
+        private
+
+          def debug?
+            Travis::Rollout.matches?(:debug_apt, repo: slug)
+          end
+
+          def slug
+            data.repository[:slug].to_s
+          end
       end
     end
   end
 end
+
