b6768080e637fdded1e8477d4ec72e99ceef0a62
Sven Fuchs <me@svenfuchs.com>
2018-05-09 12:50:27 +0200

    optionally use a mirror for apt-get

--- lib/travis/build/appliances/apt_get_update.rb
+++ lib/travis/build/appliances/apt_get_update.rb
@@ -5,13 +5,30 @@ module Travis
     module Appliances
       class AptGetUpdate < Base
         def apply
-          sh.cmd <<-EOF
-            sudo rm -rf /var/lib/apt/lists/*
-            sudo apt-get update #{'-qq  >/dev/null 2>&1' unless debug?}
-          EOF
+          use_mirror if use_mirror?
+          apt_get_update
         end
 
         private
+
+          def apt_get_update
+            sh.cmd <<-EOF
+              sudo rm -rf /var/lib/apt/lists/*
+              sudo apt-get update #{'-qq  >/dev/null 2>&1' unless debug?}
+            EOF
+          end
+
+          def use_mirror?
+            !!mirror
+          end
+
+          def use_mirror
+            sh.cmd "sudo sed -i 's_http://archive.ubuntu.com/ubuntu/_#{mirror.dup.untaint}_' /etc/apt/sources.list"
+          end
+
+          def mirror
+            ENV['APT_GET_MIRROR_UBUNTU']
+          end
 
           def debug?
             Travis::Rollout.matches?(:debug_apt, repo: slug)
