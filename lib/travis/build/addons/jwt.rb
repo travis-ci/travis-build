284255427fae9a66e0bc4fa35ff4aa4a2f15407e
Hiro Asari <asari.ruby@gmail.com>
2018-05-31 13:24:18 -0400

    Make JWT addon noop

--- lib/travis/build/addons/jwt.rb
+++ lib/travis/build/addons/jwt.rb
@@ -8,33 +8,7 @@ module Travis
         SUPER_USER_SAFE = true
 
         def before_before_install
-          sh.echo "JWT addon will be deprecated on April 17, 2018. Please read our announcement at https://blog.travis-ci.com/2018-01-23-jwt-addon-is-deprecated", ansi: :red
-
-          tokens = {}
-          Array(config).each do |secret|
-            pull_request = self.data.pull_request ? self.data.pull_request : ""
-            now = Time.now.to_i()
-            payload = {
-              "iss" => "Travis CI, GmbH",
-              "slug" => self.data.slug,
-              "pull-request" => pull_request,
-              "exp" => now+5400,
-              "iat" => now
-            }
-            begin
-              key, secret = secret.split('=').map(&:strip)
-              tokens[key] = JWT.encode(payload, secret)
-            rescue Exception
-              sh.echo "There was an error while encoding JWT. If the secret is encrypted, ensure that it is encrypted correctly.", ansi: :yellow
-            end
-          end
-          return if tokens.empty?
-          sh.fold 'addons_jwt' do
-            sh.echo 'Initializing JWT', ansi: :yellow
-            tokens.each do |key, val|
-              sh.export key, val, echo: false
-            end
-          end
+          sh.echo "JWT addon has been deprecated. Please read our announcement at https://blog.travis-ci.com/2018-01-23-jwt-addon-is-deprecated", ansi: :red
         end
       end
     end
