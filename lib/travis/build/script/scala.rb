6fc0f547ba90745e4cbc6537cab56a5dd9d3f9c5
Arya Irani <arya.irani@gmail.com>
2018-06-29 11:28:22 -0400

    bump sbt-extras version for sbt 1.x support

--- lib/travis/build/script/scala.rb
+++ lib/travis/build/script/scala.rb
@@ -11,7 +11,7 @@ module Travis
         }
 
         SBT_PATH = '/usr/local/bin/sbt'
-        SBT_SHA  = '4ad1b8a325f75c1a66f3fd100635da5eb28d9c91'
+        SBT_SHA  = 'd559cf7c483dc775c28f3f760246cb0b476dbf02'
         SBT_URL  = "https://raw.githubusercontent.com/paulp/sbt-extras/#{SBT_SHA}/sbt"
 
         def configure
