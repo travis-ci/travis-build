75443e715f210773a1d864597654bf9d5022c7de
Joep van Delft <joepvd@users.noreply.github.com>
2018-07-05 16:44:52 +0200

    Revert "xenial services"

--- lib/travis/build/addons/postgresql.rb
+++ lib/travis/build/addons/postgresql.rb
@@ -5,11 +5,10 @@ module Travis
   module Build
     class Addons
       class Postgresql < Base
-
-        include Template
-
         SUPER_USER_SAFE = true
-        TEMPLATES_PATH = File.expand_path('../../templates', __FILE__)
+
+        DEFAULT_PORT = 5432
+        DEFAULT_FALLBACK_PORT = 5433
 
         def after_prepare
           sh.if '"$TRAVIS_OS_NAME" != linux' do
@@ -17,8 +16,12 @@ module Travis
           end
           sh.else do
             sh.fold 'postgresql' do
-              sh.raw(template('postgresql.sh', version: version), echo: false, timing: false)
-              sh.cmd 'travis_setup_postgresql', echo: true, timing: true
+              sh.echo "Starting PostgreSQL v#{version}", ansi: :yellow
+              sh.export 'PATH', "/usr/lib/postgresql/#{version}/bin:$PATH", echo: false
+              stop_postgres
+              setup_ramfs
+              start_postgres
+              setup_db
             end
           end
         end
@@ -27,6 +30,37 @@ module Travis
 
           def version
             config.to_s.shellescape
+          end
+
+          def stop_postgres
+            sh.if '"$TRAVIS_INIT" == systemd' do
+              sh.cmd 'systemctl stop postgresql', assert: false, sudo: true, echo: true, timing: true
+            end
+            sh.else do
+              sh.cmd 'service postgresql stop', assert: false, sudo: true, echo: true, timing: true
+            end
+          end
+
+          def start_postgres
+            sh.if '"$TRAVIS_INIT" == systemd' do
+              sh.cmd "systemctl start postgresql@#{version}-main", assert: false, sudo: true, echo: true, timing: true
+            end
+            sh.else do
+              sh.cmd "service postgresql start #{version}", assert: false, sudo: true, echo: true, timing: true
+            end
+          end
+
+          def setup_db
+            [DEFAULT_PORT, DEFAULT_FALLBACK_PORT].each do |pgport|
+              sh.cmd "sudo -u postgres createuser -s -p #{pgport} travis &>/dev/null", assert: false, echo: true, timing: true
+              sh.cmd "sudo -u postgres createdb -O travis -p #{pgport} travis &>/dev/null", assert: false, echo: true, timing: true
+            end
+          end
+
+          def setup_ramfs
+            sh.if "-d /var/ramfs && ! -d /var/ramfs/postgresql/#{version}", echo: false do
+              sh.cmd "cp -rp /var/lib/postgresql/#{version} /var/ramfs/postgresql/#{version}", sudo: true, assert: false, echo: false, timing: false
+            end
           end
       end
     end
