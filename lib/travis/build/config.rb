2d618153ec0c0908f736e2107718889d63a13100
Dominic Jodoin <dominic@travis-ci.com>
2018-08-06 13:33:13 -0400

    Update gimme to support `.x` Go versions

--- lib/travis/build/config.rb
+++ lib/travis/build/config.rb
@@ -40,7 +40,7 @@ module Travis
         gimme: {
           url: ENV.fetch(
             'TRAVIS_BUILD_GIMME_URL',
-            'https://raw.githubusercontent.com/travis-ci/gimme/v1.3.0/gimme'
+            'https://raw.githubusercontent.com/travis-ci/gimme/v1.5.0/gimme'
           )
         },
         go_version: ENV.fetch('TRAVIS_BUILD_GO_VERSION', '1.10.x'),
