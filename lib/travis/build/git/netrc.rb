d0276fe8b20eb3a3a7f57e9d7966bd1bdc4d4ed6
Hiro Asari <asari.ruby@gmail.com>
2018-10-05 12:56:14 -0400

    Set netrc file name correctly

--- lib/travis/build/git/netrc.rb
+++ lib/travis/build/git/netrc.rb
@@ -4,15 +4,15 @@ module Travis
       class Netrc < Struct.new(:sh, :data)
         def apply
           sh.fold 'git.netrc' do
-            sh.echo "Using ${TRAVIS_HOME}/.netrc to clone repository.", ansi: :yellow
+            sh.echo "Using ${TRAVIS_HOME}/#{netrc_filename} to clone repository.", ansi: :yellow
             sh.newline
-            sh.raw "echo -e \"#{netrc_content}\" > ${TRAVIS_HOME}/.netrc"
-            sh.raw "chmod 0600 ${TRAVIS_HOME}/.netrc"
+            sh.raw "echo -e \"#{netrc_content}\" > ${TRAVIS_HOME}/#{netrc_filename}"
+            sh.raw "chmod 0600 ${TRAVIS_HOME}/#{netrc_filename}"
           end
         end
 
         def delete
-          sh.raw "rm -f ${TRAVIS_HOME}/.netrc"
+          sh.raw "rm -f ${TRAVIS_HOME}/#{netrc_filename}"
         end
 
         private
@@ -23,6 +23,10 @@ module Travis
             else
               "machine #{data.source_host}\\n  login #{data.token}\\n"
             end
+          end
+
+          def netrc_filename
+            data.config[:os].to_s.downcase == 'windows' ? '_netrc' : '.netrc'
           end
       end
     end
