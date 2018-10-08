c603de0643f056f46c05a64ae06dee9b0a1c5bed
Hiro Asari <asari.ruby@gmail.com>
2018-10-08 19:11:04 -0400

    Skip io.js warning on Windows

--- lib/travis/build/script/node_js.rb
+++ lib/travis/build/script/node_js.rb
@@ -36,7 +36,7 @@ module Travis
 
         def announce
           super
-          if iojs_3_plus?
+          if iojs_3_plus? && !is_win?
             sh.cmd 'echo -e "#include <array>\nstd::array<int, 1> arr = {0}; int main() {return 0;}" > /tmp/foo-$$.cpp', echo: false
             sh.raw "if ! ($CXX -std=c++11 -o /dev/null /tmp/foo-$$.cpp >&/dev/null || g++ -std=c++11 -o /dev/null /tmp/foo-$$.cpp >&/dev/null); then"
             sh.echo "Starting with io.js 3 and Node.js 4, building native extensions requires C++11-compatible compiler, which seems unavailable on this VM. Please read https://docs.travis-ci.com/user/languages/javascript-with-nodejs#Node.js-v4-(or-io.js-v3)-compiler-requirements.", ansi: :yellow
