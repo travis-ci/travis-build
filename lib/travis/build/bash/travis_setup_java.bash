8cfd686433f743c01f3fbb4cf90a4d95363d4d88
Joep van Delft <joep@travis-ci.org>
2018-10-15 10:27:24 +0200

    Remove echos

--- lib/travis/build/bash/travis_setup_java.bash
+++ lib/travis/build/bash/travis_setup_java.bash
@@ -5,17 +5,14 @@ travis_setup_java() {
   version="$3"
   jdkpath="$(travis_find_jdk_path "$jdk" "$vendor" "$version")"
   if [[ -z "$jdkpath" ]]; then
-    echo No path was found matching the jdk, attempting to acquire it
     travis_install_jdk "$vendor" "$version"
   elif compgen -G "${jdkpath%/*}/$(travis_jinfo_file "$vendor" "$version")" &>/dev/null &&
     declare -f jdk_switcher &>/dev/null; then
-    echo 'An apt-managed jdk was found matching the target jdk'
     travis_cmd "jdk_switcher use \"$jdk\"" --echo --assert
     if [[ -f ~/.bash_profile.d/travis_jdk.bash ]]; then
       sed -i '/export \(PATH\|JAVA_HOME\)=/d' ~/.bash_profile.d/travis_jdk.bash
     fi
   else
-    echo Using locally available jdk
     export JAVA_HOME="$jdkpath"
     export PATH="$JAVA_HOME/bin:$PATH"
     if [[ -f ~/.bash_profile.d/travis_jdk.bash ]]; then
