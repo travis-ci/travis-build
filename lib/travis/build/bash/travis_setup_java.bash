252e31ad4a156b59bc97f26eadc3d98fe68c75ae
Joep van Delft <joep@travis-ci.org>
2018-10-23 20:22:22 +0200

    Special case available jvms on osx

--- lib/travis/build/bash/travis_setup_java.bash
+++ lib/travis/build/bash/travis_setup_java.bash
@@ -5,6 +5,17 @@ travis_setup_java() {
   version="$3"
   jdkpath="$(travis_find_jdk_path "$jdk" "$vendor" "$version")"
   if [[ -z "$jdkpath" ]]; then
+    if [[ "$TRAVIS_OS_NAME" == osx ]]; then
+      # No action is necessary under special conditions
+      if [[ "$vendor" == oracle ]]; then
+        [[ "$version" == 8 ]] &&
+          java -version 2>&1 | grep -qE "^java version \"1\.8\." &&
+          return
+        [[ "$version" == 10 ]] &&
+          java -version 2>&1 | grep -qE "^java version \"10\." &&
+          return
+      fi
+    fi
     travis_install_jdk "$vendor" "$version"
   elif compgen -G "${jdkpath%/*}/$(travis_jinfo_file "$vendor" "$version")" &>/dev/null &&
     declare -f jdk_switcher &>/dev/null; then
