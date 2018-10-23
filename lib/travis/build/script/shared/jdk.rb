3dccca8283c6611fa8dc7dc798bd143c6cad7921
Joep van Delft <joepvd@users.noreply.github.com>
2018-10-23 21:28:57 +0200

    Revert "Java adjustments"

--- lib/travis/build/script/shared/jdk.rb
+++ lib/travis/build/script/shared/jdk.rb
@@ -2,33 +2,38 @@ module Travis
   module Build
     class Script
       module Jdk
-
         OPENJDK_ALTERNATIVE = {
           'oraclejdk10' => 'openjdk10'
         }
 
         def configure
           super
-          return unless specifies_jdk?
 
           if jdk_deprecated?
             sh.terminate 2, "#{jdk} is deprecated. See https://www.oracle.com/technetwork/java/javase/eol-135779.html for more details. Consider using #{OPENJDK_ALTERNATIVE[jdk]} instead.", ansi: :red
           end
 
-          return if jdk == 'default'
+          if uses_jdk?
+            if use_install_jdk?(config[:jdk])
+              download_install_jdk
 
-          vendor, version = jdk_info(jdk)
-
-          sh.echo
-          sh.fold 'install_jdk' do
-            sh.echo "Installing #{jdk}", ansi: :yellow
-            sh.raw("travis_setup_java #{jdk} #{vendor} #{version}", timing: true)
+              sh.if "-f install-jdk.sh" do
+                sh.export "JAVA_HOME", "${TRAVIS_HOME}/#{jdk}"
+                sh.cmd "bash install-jdk.sh #{install_jdk_args config[:jdk]} --target $JAVA_HOME --workspace #{cache_dir}", echo: true, assert: true
+                sh.export "PATH", "$JAVA_HOME/bin:$PATH"
+                sh.raw 'set +e', echo: false
+              end
+            else
+              sh.if '"$(command -v jdk_switcher &>/dev/null; echo $?)" == 0' do
+                sh.cmd "jdk_switcher use #{config[:jdk]}", assert: true, echo: true, timing: false
+              end
+            end
           end
         end
 
         def export
           super
-          sh.export 'TRAVIS_JDK_VERSION', config[:jdk], echo: false if specifies_jdk?
+          sh.export 'TRAVIS_JDK_VERSION', config[:jdk], echo: false if uses_jdk?
         end
 
         def setup
@@ -38,10 +43,7 @@ module Travis
             sh.export 'TERM', 'dumb'
           end
 
-          sh.if '"$TRAVIS_DIST" == precise || "$TRAVIS_DIST" == trusty' do
-            sh.echo "Disabling Gradle daemon", ansi: :yellow
-            sh.cmd 'mkdir -p ~/.gradle && echo "org.gradle.daemon=false" >> ~/.gradle/gradle.properties', echo: true, timing: false
-          end
+          sh.cmd 'mkdir -p ~/.gradle && echo "org.gradle.daemon=false" >> ~/.gradle/gradle.properties', echo: false, timing: false
 
           correct_maven_repo
         end
@@ -55,31 +57,46 @@ module Travis
         end
 
         def cache_slug
-          return super unless specifies_jdk?
+          return super unless uses_jdk?
           super << '--jdk-' << config[:jdk].to_s
         end
 
         private
-          def jdk
-            config[:jdk].gsub(/\s/,'')
-          end
 
           def uses_java?
             true
           end
 
-          def specifies_jdk?
+          def uses_jdk?
             !!config[:jdk]
           end
 
-          def jdk_info(jdk)
-            m = jdk.match(/(?<vendor>[a-z]+)-?(?<version>.+)?/)
-            if m[:vendor]. start_with? 'oracle'
-              vendor = 'oracle'
-            elsif m[:vendor].start_with? 'openjdk'
-              vendor = 'openjdk'
-            end
-            [ vendor, m[:version] ]
+          def jdk
+            config[:jdk].gsub(/\s/,'')
+          end
+
+          def use_install_jdk?(jdk)
+            ! install_jdk_args(jdk).empty?
+          end
+
+          def download_install_jdk
+            return if app_host.empty?
+            sh.cmd "curl -sf -O https://#{app_host}/files/install-jdk.sh"
+          end
+
+          def install_jdk_args(jdk)
+            args_for = {
+              # OpenJDK
+              'openjdk-ea'   => '-L GPL',
+              'openjdk9'     => '-F 9  -L GPL',
+              'openjdk10'    => '-F 10 -L GPL',
+              'openjdk11'    => '-F 11 -L GPL',
+              # OracleJDK
+              'oraclejdk-ea' => '-L BCL',
+              'oraclejdk10'  => '-F 10 -L BCL',
+              'oraclejdk11'  => '-F 11 -L BCL',
+            }
+            args_for.fetch(jdk, '')
           end
 
           def cache_dir
@@ -93,7 +110,7 @@ module Travis
           end
 
           def jdk_deprecated?
-            specifies_jdk? && OPENJDK_ALTERNATIVE.keys.include?(jdk)
+            uses_jdk? && OPENJDK_ALTERNATIVE.keys.include?(jdk)
           end
       end
     end
