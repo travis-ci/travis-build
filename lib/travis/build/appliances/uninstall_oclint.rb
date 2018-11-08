52c6b6cc1d537e52c8ae6f0906944d6fcaa3d173
Hiro Asari <asari.ruby@gmail.com>
2018-11-08 09:38:54 -0500

    Uninstall oclint when running on Mac
    This package is interfering with other more important packages such
    as gcc. So we remove it on the fly.

---
+++ lib/travis/build/appliances/uninstall_oclint.rb
@@ -0,0 +1,17 @@
+require 'travis/build/appliances/base'
+
+module Travis
+  module Build
+    module Appliances
+      class UninstallOclint < Base
+        def apply
+          sh.if "$(command -v brew)" do
+            sh.echo "Uninstalling oclint to prevent interference with other packages.", ansi: :yellow
+            sh.echo "If you need oclint, you must explicitly install it.", ansi: :yellow
+            sh.cmd "brew cask uninstall oclint", assert: false
+          end
+        end
+      end
+    end
+  end
+end
