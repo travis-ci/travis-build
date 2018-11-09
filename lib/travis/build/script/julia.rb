ef83ffa949842947fceb60b5f174f99932c73937
Simon Byrne <simonbyrne@gmail.com>
2018-11-09 10:50:58 -0800

    don't silence installer, remove powershell flags

--- lib/travis/build/script/julia.rb
+++ lib/travis/build/script/julia.rb
@@ -56,7 +56,7 @@ module Travis
             when 'windows'
               sh.cmd %Q{curl -A "$CURL_USER_AGENT" -s -L --retry 7 -o julia-installer.exe '#{julia_url}'}
               sh.cmd 'chmod +x julia-installer.exe'
-              sh.cmd './julia-installer.exe /S /D=C:\julia -NoNewWindow -Wait'
+              sh.cmd './julia-installer.exe /D=C:\julia'
               sh.cmd 'export PATH="${PATH};C:\julia\bin\"'
             else
               sh.failure "Operating system not supported: #{config[:os]}"
