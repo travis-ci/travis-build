5b6a57c8830a8cc62682bd872165ff2abcd31b4c
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-11-13 10:31:42 +0100

    remove the single quotes

--- spec/build/script/shared/appliances/fix_mvn_settings_xml.rb
+++ spec/build/script/shared/appliances/fix_mvn_settings_xml.rb
@@ -1,6 +1,6 @@
 shared_examples_for 'fix ~/.m2/settings.xml' do
   it 'updates ~/.m2/settings.xml' do
-    should include_sexp [:cmd, "sed -i '.bak1' -e 's|https://nexus.codehaus.org/snapshots/|https://oss.sonatype.org/content/repositories/codehaus-snapshots/|g' ~/.m2/settings.xml"]
-    should include_sexp [:cmd, "sed -i '.bak2' -e 's|https://repository.apache.org/releases/|https://repository.apache.org/content/repositories/releases/|g' ~/.m2/settings.xml"]
+    should include_sexp [:cmd, "sed -i .bak1 -e 's|https://nexus.codehaus.org/snapshots/|https://oss.sonatype.org/content/repositories/codehaus-snapshots/|g' ~/.m2/settings.xml"]
+    should include_sexp [:cmd, "sed -i .bak2 -e 's|https://repository.apache.org/releases/|https://repository.apache.org/content/repositories/releases/|g' ~/.m2/settings.xml"]
   end
 end
