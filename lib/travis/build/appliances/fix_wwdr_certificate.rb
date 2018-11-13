5d1f98662cc8b26407b157da526694e41d2d3965
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-11-13 09:16:12 +0100

    also pipe away the add-certificate output

--- lib/travis/build/appliances/fix_wwdr_certificate.rb
+++ lib/travis/build/appliances/fix_wwdr_certificate.rb
@@ -8,7 +8,7 @@ module Travis
           sh.if "$(command -v sw_vers)" do
             sh.cmd "sudo security delete-certificate -Z 0950B6CD3D2F37EA246A1AAA20DFAADBD6FE1F75 /Library/Keychains/System.keychain &>/dev/null", assert: false, echo: false
             sh.cmd "wget -q https://developer.apple.com/certificationauthority/AppleWWDRCA.cer", assert: false, echo: false
-            sh.cmd "sudo security add-certificates -k /Library/Keychains/System.keychain AppleWWDRCA.cer", assert: false, echo: false
+            sh.cmd "sudo security add-certificates -k /Library/Keychains/System.keychain AppleWWDRCA.cer &>/dev/null", assert: false, echo: false
           end
         end
       end
