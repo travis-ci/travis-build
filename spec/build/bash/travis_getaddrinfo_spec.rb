d849f33b5b2ce8223e9b810dd6a781b9c00483a8
Dan Buch <dan@meatballhat.com>
2018-11-15 17:23:53 -0500

    Add bash+ruby func for ipv4 DNS lookups
    so that we can get addresses for names without having a tool like `host`
    or `dig` installed.

---
+++ spec/build/bash/travis_getaddrinfo_spec.rb
@@ -0,0 +1,35 @@
+describe 'travis_getaddrinfo', integration: true do
+  include SpecHelpers::BashFunction
+
+  let :stubs do
+    <<~BASH
+      rvm() {
+        while [[ "${1}" != do ]]; do
+          shift
+        done
+        shift
+        "${@}"
+      }
+      travis_internal_ruby() {
+        echo '2.5.3'
+      }
+    BASH
+  end
+
+  it 'is valid bash' do
+    expect(run_script('travis_getaddrinfo', '')[:truth]).to be true
+  end
+
+  it 'can run successfully' do
+    result = run_script(
+      'travis_getaddrinfo',
+      "#{stubs}\ntravis_getaddrinfo www.google.com",
+      image: 'ruby:2.5.3',
+    )
+    expect(result[:truth]).to be true
+    response = result[:out].read.strip.split($NL).first
+    expect(response).to_not be_nil
+    expect(response).to_not be_empty
+    expect(Addrinfo.tcp(response, 80).ip?).to be true
+  end
+end
