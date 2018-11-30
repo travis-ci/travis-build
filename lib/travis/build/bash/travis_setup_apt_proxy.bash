ac8d30702d0273821734a84cc58e44e1c5b23e8c
Dan Buch <dan@meatballhat.com>
2018-11-30 09:12:57 -0500

    Remove the bogus APT https proxy config value of "false"

--- lib/travis/build/bash/travis_setup_apt_proxy.bash
+++ lib/travis/build/bash/travis_setup_apt_proxy.bash
@@ -21,7 +21,6 @@ travis_setup_apt_proxy() {
   (
     cat <<EOCONF
 Acquire::http::Proxy "${TRAVIS_APT_PROXY}";
-Acquire::https::Proxy false;
 EOCONF
   ) | sudo tee "${dest_dir}/99-travis-apt-proxy" &>/dev/null
 }
