e0a19f1ebb75ca8213511d513d79d505e8afae00
AJ Bowen <aj@soulshake.net>
2018-12-19 10:49:08 -0600

    This test should fail

--- spec/build/script/shared/appliances/enable_ipv4_forwarding.rb
+++ spec/build/script/shared/appliances/enable_ipv4_forwarding.rb
@@ -1,8 +1,7 @@
 shared_examples_for 'enables ipv4 forwarding by adding a file in /etc/sysctl' do
-  let(:ipv4_enabled) { "sudo sed -e 's/^\\([0-9a-f:]\\+\\) localhost/\\1/' -i'.bak' /etc/hosts" }
 
   it 'adds a file in /etc/sysctl' do
-    should include_sexp [:cmd, 'echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf > /dev/null', echo: false, assert: false, sudo: true]
+    should include_sexp [:cmd, 'echo "net.xXXXXXXXX.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/99-travis-enable-ipv4-forwarding.conf > /dev/null', echo: false, assert: false, sudo: true]
   end
 
   context "when sudo is unavailable" do
