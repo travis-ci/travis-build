738c3c9b2afbda38edbcf31a4119154e3761ce45
Hiro Asari <asari.ruby@gmail.com>
2019-01-08 09:41:16 -0500

    Remove `join` logic from RVM_GPG_KEYS
    The *KEYS constant should be keys, not a string.

--- lib/travis/build/script/shared/rvm.rb
+++ lib/travis/build/script/shared/rvm.rb
@@ -23,7 +23,7 @@ module Travis
         RVM_GPG_KEYS = %w(
           409B6B1796C275462A1703113804BB82D39DC0E3
           7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-        ).join(" ")
+        )
 
         def export
           super
@@ -64,7 +64,7 @@ module Travis
           end
 
           def import_gpg_key
-            sh.cmd "command gpg2 --version &>/dev/null && gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys #{RVM_GPG_KEYS}",
+            sh.cmd "command gpg2 --version &>/dev/null && gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys #{RVM_GPG_KEYS.join(" ")}",
               echo: "Importing new GPG keys for RVM", assert: false
           end
 
