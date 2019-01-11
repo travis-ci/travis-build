6956aac424247a17ce3b12397038d58044c05502
Dan Buch <dan@meatballhat.com>
2019-01-11 10:55:04 -0500

    Add specs for travis_script_go

--- spec/build/bash/travis_script_go_spec.rb
+++ spec/build/bash/travis_script_go_spec.rb
@@ -4,4 +4,43 @@ describe 'travis_script_go', integration: true do
   it 'is valid bash' do
     expect(run_script('travis_script_go', '')[:truth]).to be true
   end
+
+  it 'runs make when a makefile is present' do
+    result = run_script(
+      'travis_script_go',
+      <<~BASH
+      travis_cmd() {
+        TRAVIS_CMD_RAN+=("${*}")
+      }
+      TRAVIS_CMD_RAN=()
+      touch Makefile
+
+      travis_script_go -v
+
+      echo "${TRAVIS_CMD_RAN[@]}"
+      BASH
+    )
+
+    expect(result[:err].read.strip).to eq ''
+    expect(result[:out].read).to match(/\bmake\b/)
+  end
+
+  it 'runs "go test" when makefile is not present' do
+    result = run_script(
+      'travis_script_go',
+      <<~BASH
+      travis_cmd() {
+        TRAVIS_CMD_RAN+=("${*}")
+      }
+      TRAVIS_CMD_RAN=()
+
+      travis_script_go -v
+
+      echo "${TRAVIS_CMD_RAN[@]}"
+      BASH
+    )
+
+    expect(result[:err].read.strip).to eq ''
+    expect(result[:out].read).to match(/\bgo test -v \.\/\.\.\./)
+  end
 end
