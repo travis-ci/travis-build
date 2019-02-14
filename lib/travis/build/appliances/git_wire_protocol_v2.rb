491a59ac2256deb32da1211657be423b845460e3
Dominic Jodoin <dominic@travis-ci.com>
2019-02-14 16:47:14 -0500

    No need to check Git version in the end.

--- lib/travis/build/appliances/git_wire_protocol_v2.rb
+++ lib/travis/build/appliances/git_wire_protocol_v2.rb
@@ -4,15 +4,7 @@ module Travis
   module Build
     module Appliances
       class GitWireProtocolV2 < Base
-
-        GIT_VERSION = /(\d)\.(\d+)\.(\d+)/
-        GIT_WIRE_PROTOCOL_V2_MAJOR = 2
-        GIT_WIRE_PROTOCOL_V2_MINOR = 18
-        
         def apply
-          git_version = `git version`
-          major, minor, build = git_version.match(GIT_VERSION).captures
-          if major.to_i => GIT_WIRE_PROTOCOL_V2_MAJOR && minor.to_i => GIT_WIRE_PROTOCOL_V2_MINOR
             sh.cmd "git config --global protocol.version 2", assert: false, echo: true
           end
         end
