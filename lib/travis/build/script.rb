3bd3e051e413ccde7e938212c7e3a5f9edc561e7
Dominic Jodoin <dominic@travis-ci.com>
2019-02-15 09:09:20 -0500

    Untaint custom_archive string

--- lib/travis/build/script.rb
+++ lib/travis/build/script.rb
@@ -183,7 +183,7 @@ module Travis
       def lang_archive_prefix(lang, bucket)
         custom_archive = ENV["TRAVIS_BUILD_LANG_ARCHIVES_#{lang}".upcase]
         
-        custom_archive || "s3.amazonaws.com/#{bucket}"
+        custom_archive.output_safe || "s3.amazonaws.com/#{bucket}"
       end
 
       def debug_build_via_api?
