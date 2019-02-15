e809f6f3f8ede90e5772f8572ebced4af5d057e2
Dominic Jodoin <dominic@travis-ci.com>
2019-02-15 15:30:51 -0500

    It's not supported on xcode9.3-moar too

--- lib/travis/build/appliances/git_wire_protocol_v2.rb
+++ lib/travis/build/appliances/git_wire_protocol_v2.rb
@@ -7,7 +7,7 @@ module Travis
 
         def apply?
           # Git version on `xcode9.3` image doesn't support protocol v2
-          data[:config][:osx_image].to_s.empty? || data[:config][:osx_image] != 'xcode9.3'
+          data[:config][:osx_image].to_s.empty? || !%w[xcode9.3 xcode9.3-moar].include? data[:config][:osx_image]
         end
 
         def apply
