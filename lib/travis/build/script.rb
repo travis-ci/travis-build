78622f6004493da3476f4dd208f898b915d1e0e7
Hiro Asari <asari.ruby@gmail.com>
2019-03-12 12:00:22 -0400

    Introduce TRAVIS_LANGUAGE_ARCHIVE_HOST
    This environment variable determines where we fetch language archives
    from.
    
    The valid values are either `gcs` or `s3`; all other values default
    to `s3`.
    
    Each language can still override these values by setting
    `$TRAVIS_BUILD_LANG_ARCHIVES_${LANG}`.

--- lib/travis/build/script.rb
+++ lib/travis/build/script.rb
@@ -188,8 +188,18 @@ module Travis
 
       def lang_archive_prefix(lang, bucket)
         custom_archive = ENV["TRAVIS_BUILD_LANG_ARCHIVES_#{lang}".upcase]
+        unless custom_archive.to_s.empty?
+          return custom_archive.output_safe
+        end
 
-        custom_archive.to_s.empty? ? "storage.googleapis.com/travis-ci-language-archives/#{lang}" : custom_archive.output_safe
+        case Travis::Build.config.lang_archive_host
+        when 'gcs'
+          "language-archives.travis-ci.com/#{lang}"
+        when 's3'
+          "s3.amazonaws.com/#{bucket}"
+        else
+          "s3.amazonaws.com/#{bucket}" # explicitly state default
+        end
       end
 
       def debug_build_via_api?
--- spec/build/script/python_spec.rb
+++ spec/build/script/python_spec.rb
@@ -79,7 +79,7 @@ describe Travis::Build::Script::Python, :sexp do
 
     it "downloads archive" do
       branch = sexp_find(sexp, [:then])
-      expect(branch).to include_sexp [:raw, "archive_url=https://storage.googleapis.com/travis-ci-language-archives/python/binaries/${travis_host_os}/${travis_rel_version}/$(uname -m)/python-#{version}.tar.bz2"]
+      expect(branch).to include_sexp [:raw, "archive_url=https://s3.amazonaws.com/travis-python-archives/binaries/${travis_host_os}/${travis_rel_version}/$(uname -m)/python-#{version}.tar.bz2"]
     end
 
     context 'and using a custom archive url' do
@@ -90,6 +90,21 @@ describe Travis::Build::Script::Python, :sexp do
         ENV['']
         branch = sexp_find(sexp, [:then])
         expect(branch).to include_sexp [:raw, "archive_url=https://cdn.of.lots.of.python.stuff/binaries/${travis_host_os}/${travis_rel_version}/$(uname -m)/python-#{version}.tar.bz2"]
+      end
+    end
+
+    context 'and using gcs as language archive host' do
+      before :each do
+        @old_lang_archive_host = Travis::Build.config.lang_archive_host
+        Travis::Build.config.lang_archive_host = 'gcs'
+      end
+
+      after { Travis::Build.config.lang_archive_host = @old_lang_archive_host }
+
+      it "downloads archive" do
+        ENV['']
+        branch = sexp_find(sexp, [:then])
+        expect(branch).to include_sexp [:raw, "archive_url=https://language-archives.travis-ci.com/python/binaries/${travis_host_os}/${travis_rel_version}/$(uname -m)/python-#{version}.tar.bz2"]
       end
     end
   end
