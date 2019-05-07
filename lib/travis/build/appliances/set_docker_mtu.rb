7d5bf83bcbe7a506042c33a70b6d3eac0380bfd6
Hiro Asari <asari.ruby@gmail.com>
2019-05-07 10:58:33 -0400

    Suppress mtu update output

--- lib/travis/build/appliances/set_docker_mtu.rb
+++ lib/travis/build/appliances/set_docker_mtu.rb
@@ -9,18 +9,20 @@ module Travis
         end
 
         def apply
-          sh.raw <<-EOF
+          sh.fold "docker_mtu" do
+            sh.raw <<-EOF
 sudo test -f /etc/docker/daemon.json
 if [[ $? = 0 ]]; then
   echo '[{"op":"add","path":"/mtu","value":1460}]' > mtu.jsonpatch
   sudo jsonpatch /etc/docker/daemon.json mtu.jsonpatch > daemon.json
   sudo mv daemon.json /etc/docker/daemon.json
 else
-  echo '{"mtu":1460}' | sudo tee /etc/docker/daemon.json
+  echo '{"mtu":1460}' | sudo tee /etc/docker/daemon.json > /dev/null
 fi
 
 sudo service docker restart
-          EOF
+            EOF
+          end
         end
       end
     end
