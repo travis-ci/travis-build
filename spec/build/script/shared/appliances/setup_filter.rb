a5172573ab16d1cd2f6492e4a24331cd154e48c6
anarosas <a.rosas10@gmail.com>
2019-06-12 11:07:33 -0400

    Add spec to verify secret filtering is running on other OS

--- spec/build/script/shared/appliances/setup_filter.rb
+++ spec/build/script/shared/appliances/setup_filter.rb
@@ -1,14 +1,23 @@
 shared_examples_for 'setup filter' do
+  let(:filter) {[:echo, 'Secrets are not currently filtered on Windows, please be careful', ansi: 'yellow']}
 
-  describe 'runs on windows' do
-    let(:filter) {[:echo, 'Secrets are not currently filtered on Windows, please be careful', ansi: 'yellow']}
-
+  describe 'skips on Windows' do
     before do
       data[:config][:os] = 'windows'
     end
 
     describe 'skips secrets filtering' do
       it {should include_sexp filter}
+    end
+  end
+
+  describe 'runs on Linux' do
+    before do
+      data[:config][:os] = 'linux'
+    end
+
+    describe 'skips secrets filtering' do
+      it {should_not include_sexp filter}
     end
   end
 end
