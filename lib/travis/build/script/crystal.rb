38e7276fa6b788a8df9573ef876a1a9ef18392b8
anarosas <a.rosas10@gmail.com>
2019-06-12 16:48:45 -0400

    Use crystal snap for nightly version

--- lib/travis/build/script/crystal.rb
+++ lib/travis/build/script/crystal.rb
@@ -11,18 +11,29 @@ module Travis
 
             case config[:os]
             when 'linux'
-              version = select_apt_version
-              return unless version
+              validate_version
+              if config[:crystal] == 'nightly'
+                sh.cmd %Q(sudo snap install crystal --classic --edge)
+              else
+                version = {
+                  url: "https://dist.crystal-lang.org/apt",
+                  key: {
+                    url: "https://dist.crystal-lang.org/rpm/RPM-GPG-KEY",
+                    fingerprint: "5995C83CD754BE448164192909617FD37CC06B54"
+                  },
+                  package: "crystal"
+                }
 
-              sh.cmd %Q(curl -sSL '#{version[:key][:url]}' > "${TRAVIS_HOME}/crystal_repository_key.asc")
-              sh.if %Q("$(gpg --with-fingerprint "${TRAVIS_HOME}/crystal_repository_key.asc" | grep "Key fingerprint" | cut -d "=" -f2 | tr -d " ")" != "#{version[:key][:fingerprint]}") do
-                sh.failure "The repository key needed to install Crystal did not have the expected fingerprint. Your build was aborted."
+                sh.cmd %Q(curl -sSL '#{version[:key][:url]}' > "${TRAVIS_HOME}/crystal_repository_key.asc")
+                sh.if %Q("$(gpg --with-fingerprint "${TRAVIS_HOME}/crystal_repository_key.asc" | grep "Key fingerprint" | cut -d "=" -f2 | tr -d " ")" != "#{version[:key][:fingerprint]}") do
+                  sh.failure "The repository key needed to install Crystal did not have the expected fingerprint. Your build was aborted."
+                end
+                sh.cmd %q(sudo sh -c "apt-key add '${TRAVIS_HOME}/crystal_repository_key.asc'")
+
+                sh.cmd %Q(sudo sh -c 'echo "deb #{version[:url]} crystal main" > /etc/apt/sources.list.d/crystal-nightly.list')
+                sh.cmd 'travis_apt_get_update'
+                sh.cmd %Q(sudo apt-get install -y #{version[:package]} libgmp-dev)
               end
-              sh.cmd %q(sudo sh -c "apt-key add '${TRAVIS_HOME}/crystal_repository_key.asc'")
-
-              sh.cmd %Q(sudo sh -c 'echo "deb #{version[:url]} crystal main" > /etc/apt/sources.list.d/crystal-nightly.list')
-              sh.cmd 'travis_apt_get_update'
-              sh.cmd %Q(sudo apt-get install -y #{version[:package]} libgmp-dev)
             when 'osx'
               if config[:crystal] && config[:crystal] != "latest"
                 sh.failure %Q(Specifying Crystal version is not yet supported by the macOS environment)
@@ -64,28 +75,9 @@ module Travis
 
         private
 
-        def select_apt_version
-          key = {
-            url: "https://dist.crystal-lang.org/rpm/RPM-GPG-KEY",
-            fingerprint: "5995C83CD754BE448164192909617FD37CC06B54"
-          }
-
-          case config[:crystal]
-          when nil, "latest"
-            {
-              url: "https://dist.crystal-lang.org/apt",
-              key: key,
-              package: "crystal"
-            }
-          when "nightly"
-            {
-              url: "https://nightly.crystal-lang.org/apt",
-              key: key,
-              package: "crystal-nightly"
-            }
-          else
+        def validate_version
+          if config[:crystal] != 'latest' && config[:crystal] != 'nightly' && !config[:crystal].nil?
             sh.failure %Q("#{config[:crystal]}" is an invalid version of Crystal.\nView valid versions of Crystal at https://docs.travis-ci.com/user/languages/crystal/)
-            nil
           end
         end
 
