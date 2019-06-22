ea2889d51b3757544860cf0db7bb2585d09761e4
Simon Byrne <simonbyrne@gmail.com>
2019-06-22 10:58:06 -0700

    Disable `release` Julia version and add version `1` (#1447)

--- spec/build/script/julia_spec.rb
+++ spec/build/script/julia_spec.rb
@@ -12,7 +12,7 @@ describe Travis::Build::Script::Julia, :sexp do
   it_behaves_like 'a build script sexp'
 
   it 'sets TRAVIS_JULIA_VERSION' do
-    should include_sexp [:export, ['TRAVIS_JULIA_VERSION', 'release']]
+    should include_sexp [:export, ['TRAVIS_JULIA_VERSION', '1']]
   end
 
   it 'downloads and installs Julia' do
