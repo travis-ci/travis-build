30d513655bc7700ece1cec9e43d647eee1341fc3
Hiro Asari <asari.ruby@gmail.com>
2019-06-26 12:59:52 -0400

    Add workspaces specs
    And store an example

--- spec/build/script/workspace_spec.rb
+++ spec/build/script/workspace_spec.rb
@@ -7,11 +7,23 @@ describe Travis::Build::Script::Workspace, :sexp do
   let(:sexp)    { script.sexp }
   let(:script)  { Travis::Build.script(data) }
   let(:cache)   { script.directory_cache }
+  subject       { script.sexp }
 
   it_behaves_like 'compiled script' do
-    let(:config) { { workspaces: { use: "foo", create: { name: "foo" } } } }
+    let(:config) { { workspaces: { use: "foo", create: { name: "bar", paths: ['build'] } } } }
     let(:cmds)   { ['workspaces_use'] }
     let(:cmds)   { ['workspaces_create'] }
   end
 
+  describe 'when workspace name contains shell env var' do
+    let(:config) { { workspaces: { use: "${TRAVIS_OS_NAME}", create: { name: "ws-${TRAVIS_OS_NAME}", paths: ['build'] } } } }
+    it "interprets the env var" do
+      store_example
+      should include_sexp [
+        :cmd,
+        %r[\$CASHER_DIR/bin/casher --name ws-%24%7BTRAVIS_OS_NAME%7D workspace push https://s3.amazonaws.com/s3_bucket/workspaces/1/ws-\\\$\\\{TRAVIS_OS_NAME\\\}.tgz\\\?X-Amz-Algorithm\\\=AWS4-HMAC-SHA256\\\&X-Amz-Credential\\\=s3_access_key_id\\\%2F.*\\\%2Fus-east-1\\\%2Fs3\\\%2Faws4_request\\\&X-Amz-Date\\\=\d{8}T\d{6}Z\\\&X-Amz-Expires\\\=30\\\&X-Amz-Signature\\\=[a-f0-9]+\\\&X-Amz-SignedHeaders\\\=host],
+        timing: true
+      ]
+    end
+  end
 end
