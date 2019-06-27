ae58590bf267610bd4ab537a83c479f7c3f70cba
Hiro Asari <asari.ruby@gmail.com>
2019-06-27 16:21:49 -0400

    Untaint maven mirror string

--- lib/travis/build/appliances/maven_central_mirror.rb
+++ lib/travis/build/appliances/maven_central_mirror.rb
@@ -3,7 +3,7 @@ module Travis
   module Build
     module Appliances
       class MavenCentralMirror < Base
-        GOOGLE_MAVEN_MIRROR = Travis::Build.config.maven_central_mirror
+        GOOGLE_MAVEN_MIRROR = Travis::Build.config.maven_central_mirror.output_safe
         def apply
           sh.raw bash('travis_maven_central_mirror')
           sh.raw "travis_maven_central_mirror #{GOOGLE_MAVEN_MIRROR}"
