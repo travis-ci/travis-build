60295ff594188ca1b861d3cd0c6a8548dd0fe9b0
Tahsin Hasan <tahsin352@yahoo.com>
2019-09-05 13:40:59 +0600

    disable network availability check

--- lib/travis/build/config.rb
+++ lib/travis/build/config.rb
@@ -141,7 +141,8 @@ module Travis
         ).split(/,/),
         dump_backtrace: ENV.fetch(
           'TRAVIS_BUILD_DUMP_BACKTRACE', ENV.fetch('DUMP_BACKTRACE', 'false')
-        ).to_bool
+        ).to_bool,
+        network_available: ENV.fetch('TRAVIS_WAIT_FOR_NETWORK_CHECK', 'true').to_bool
       )
 
       default(
