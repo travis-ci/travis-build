b48c1bfe6345b2cb54e5e98db030e3959ec844e4
Damian Szymanski <damian@assembla.com>
2019-09-16 14:51:39 +0200

    Don't print sudo warnings

--- lib/travis/build/appliances/put_localhost_first.rb
+++ lib/travis/build/appliances/put_localhost_first.rb
@@ -10,9 +10,9 @@ module Travis
           # remove lines with 127.0.0.1
           sh.raw %q(sed '/^127\.0\.0\.1/d' /etc/hosts > /tmp/hosts_sans_127_0_0_1)
           # reconstruct /etc/hosts
-          sh.raw %q(cat /tmp/hosts_sans_127_0_0_1 | sudo tee /etc/hosts > /dev/null)
-          sh.raw %q(echo -n "127.0.0.1 localhost " | sudo tee -a /etc/hosts > /dev/null)
-          sh.raw %q({ cat /tmp/hosts_127_0_0_1; echo; } | sudo tee -a /etc/hosts > /dev/null)
+          sh.raw %q(cat /tmp/hosts_sans_127_0_0_1 | sudo tee /etc/hosts &> /dev/null)
+          sh.raw %q(echo -n "127.0.0.1 localhost " | sudo tee -a /etc/hosts &> /dev/null)
+          sh.raw %q({ cat /tmp/hosts_127_0_0_1; echo; } | sudo tee -a /etc/hosts &> /dev/null)
         end
       end
     end
--- spec/build/script/shared/appliances/fix_etc_hosts.rb
+++ spec/build/script/shared/appliances/fix_etc_hosts.rb
@@ -1,5 +1,5 @@
 shared_examples_for 'fix etc/hosts' do
-  let(:fix_etc_hosts) { "sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 '`hostname`'/' -i'.bak' /etc/hosts" }
+  let(:fix_etc_hosts) { "sudo sed -e 's/^\\(127\\.0\\.0\\.1.*\\)$/\\1 '`hostname`'/' -i'.bak' /etc/hosts 2> /dev/null" }
 
   it 'adds an entry to /etc/hosts for localhost' do
     should include_sexp [:raw, fix_etc_hosts]
