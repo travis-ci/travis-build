36b2d5a0801daf8b1c87d72ee6efeb1885b61eec
artur-rupp <arturrupp@travis-ci.org>
2019-11-06 10:04:40 +0100

    support for java11 arm64

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -3,6 +3,18 @@ travis_install_jdk() {
   jdk="$1"
   vendor="$2"
   version="$3"
+  if [ ${TRAVIS_CPU_ARCH} == 'arm64' ];then
+    travis_install_jdk_compiled $version
+  else
+    travis_install_jdk_ext_provider "$jdk" "$vendor" "$version"
+  fi
+}
+
+travis_install_jdk_ext_provider(){
+  local url vendor version license jdk certlink
+  jdk="$1"
+  vendor="$2"
+  version="$3"
   if [[ "$vendor" == openjdk ]]; then
     license=GPL
   elif [[ "$vendor" == oracle ]]; then
@@ -25,3 +37,31 @@ travis_install_jdk() {
   # shellcheck disable=2088
   travis_cmd "~/bin/install-jdk.sh --target \"$JAVA_HOME\" --workspace \"$TRAVIS_HOME/.cache/install-jdk\" --feature \"$version\" --license \"$license\"$certlink" --echo --assert
 }
+
+
+travis_install_jdk_compiled(){
+  local JAVA_URL
+  local JAVA_VERSION
+  local UNAME_ARCH
+  UNAME_ARCH=$(uname -m)
+  JAVA_VERSION="$1"
+  JAVA_URL="https://travis-java-archives.s3.amazonaws.com/binaries/$(source /usr/lib/os-release;echo $ID)/$(source /usr/lib/os-release;echo $VERSION_ID)/${UNAME_ARCH}/linux-openjdk${JAVA_VERSION}.tar.bz2"
+
+  travis_cmd "sudo mkdir -p /usr/local/lib/openjdk${JAVA_VERSION}"
+
+  sudo curl -s ${JAVA_URL} | sudo tar xjf - -C /usr/local/lib/openjdk${JAVA_VERSION} --strip-components 1
+  if [ $? -ne 0 ];then
+    echo "${ANSI_RED}Could not download java ${JAVA_VERSION} for arch ${UNAME_ARCH} ${ANSI_RESET}"
+    travis_terminate 2
+  fi
+
+  echo "if [[ -d /usr/local/lib/openjdk${JAVA_VERSION} ]]; then
+  export JAVA_HOME=/usr/local/lib/openjdk${JAVA_VERSION}
+  export PATH="\$JAVA_HOME/bin:\$PATH"
+fi
+" > /home/travis/.bash_profile.d/travis-java.bash
+  sudo chmod 644 /home/travis/.bash_profile.d/travis-java.bash
+  sudo chown travis:travis /home/travis/.bash_profile.d/travis-java.bash
+}
+
+
