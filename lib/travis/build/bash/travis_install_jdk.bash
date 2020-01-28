fef513b68b3b68606b128d7012109c8ac2e1a708
Ivan Pozdeev <vano@mail.mipt.ru>
2020-01-29 00:28:06 +0300

    fix shellcheck false positive

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -55,6 +55,7 @@ travis_install_jdk_package() {
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
   fi
   # `realpath` is preinstalled in Ubuntu Xenial+ and OSX 10.11+ Homebrew
+  # shellcheck disable=SC2016
   travis_cmd 'export JAVA_HOME="$(realpath -Pm "$(which javac)/../../")"' --echo
   # no need to alter PATH because `adoptopenjdk` installs executables with `update-alternatives`
 }
