d7c28c277a3685f4442fe0ccc92bd86a55e680af
gabriel-arc <gabriel@travis-ci.org>
2020-02-11 13:58:55 +0100

    curl insecure if ssl verification disabled

--- lib/travis/build/script/shared/directory_cache/base.rb
+++ lib/travis/build/script/shared/directory_cache/base.rb
@@ -301,7 +301,11 @@ module Travis
               cmd_opts = {retry: true, assert: false, echo: 'Installing caching utilities'}
               if casher_branch == 'production'
                 static_file_location = "https://#{app_host}/files/#{name}".output_safe
-                sh.cmd curl_cmd(flags, location, static_file_location), cmd_opts
+                app_host_flags = flags
+                if Travis::Build.config.ssl.verify == false
+                  app_host_flags += '-k'
+                end
+                sh.cmd curl_cmd(app_host_flags, location, static_file_location), cmd_opts
                 sh.if "$? -ne 0" do
                   cmd_opts[:echo] = "Installing caching utilities from the Travis CI server (#{static_file_location}) failed, failing over to using GitHub (#{remote_location})"
                   sh.cmd curl_cmd(flags, location, remote_location), cmd_opts
