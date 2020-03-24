44b2ff1c11ec16d6ebde6114ac1ed03e1ac4aa1f
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-03-24 15:03:01 -0400

    Add MATLAB language

---
+++ lib/travis/build/script/matlab.rb
@@ -0,0 +1,39 @@
+module Travis
+    module Build
+      class Script
+        class Matlab < Script
+          MATLAB_INSTALLER_LOCATION = 'https://ssd.mathworks.com/supportfiles/ci/ephemeral-matlab/v0/install.sh'.freeze
+          MATLAB_EXECUTABLE = 'matlab -batch'.freeze
+          MATLAB_COMMAND = "results = runtests('IncludeSubfolders',true," \
+                              "'IncludeReferencedProjects',true), " \
+                              'assert(all(~[results.Failed]))'.freeze
+
+          DEFAULTS = {
+            matlab: 'latest'
+          }.freeze
+
+          def export
+            super
+            sh.export 'TRAVIS_MATLAB_VERSION', config[:matlab].to_s.shellescape,
+                      echo: false
+          end
+
+          def configure
+            super
+
+            # Invoke the ephemeral MATLAB installer that will make a MATLAB available
+            # on the system PATH
+            sh.raw "wget -qO- --retry-connrefused #{MATLAB_INSTALLER_LOCATION}" \
+                   ' | sudo -E bash'
+          end
+
+          def script
+            super
+
+            # By default, invoke the default MATLAB 'runtests' command
+            sh.cmd "#{MATLAB_EXECUTABLE} \"#{MATLAB_COMMAND}\""
+          end
+        end
+      end
+    end
+  end
---
+++ spec/build/script/matlab_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Travis::Build::Script::Matlab, :sexp do
+  let(:data)        { payload_for(:push, :matlab) }
+  let(:script)      { described_class.new(data)   }
+  let(:installer)   { Travis::Build::Script::Matlab::MATLAB_INSTALLER_LOCATION }
+  let(:executable)  { Travis::Build::Script::Matlab::MATLAB_EXECUTABLE }
+  let(:command)     { Travis::Build::Script::Matlab::MATLAB_COMMAND }
+
+  subject           { script.sexp }
+  it                { store_example }
+
+  it_behaves_like 'a bash script'
+
+  it 'sets TRAVIS_MATLAB_VERSION to the latest version of MATLAB' do
+    should include_sexp [:export, %w[TRAVIS_MATLAB_VERSION latest]]
+  end
+
+  context 'it uses the MATLAB installer' do
+    it 'by discreetly downloading/piping it to a shell' do
+      should include_sexp [:raw, "wget -qO- --retry-connrefused #{installer} "\
+                           '| sudo -E bash']
+    end
+  end
+
+  it 'runs the default MATLAB command' do
+    should include_sexp [:cmd, "#{executable} \"#{command}\"",
+                         echo: true, timing: true]
+  end
+end
