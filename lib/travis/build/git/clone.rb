6c7b3902b5d06e3ff1cceafae7eef1dc04b6eaac
Artur Kremens <artur@assembla.com>
2020-04-10 13:30:48 +0200

    Fix bb pull requests (one repo)

--- lib/travis/build/git/clone.rb
+++ lib/travis/build/git/clone.rb
@@ -122,8 +122,10 @@ module Travis
               sh.cmd "#{git_cmd} fetch upstream", assert: true, retry: true
               sh.cmd "#{git_cmd} merge --squash upstream/#{pull_request_head_branch}", assert: true, retry: true
             else
-              sh.cmd "#{git_cmd} checkout -qb #{pull_request_head_branch}", timing: false
-              sh.cmd "#{git_cmd} merge --squash #{branch}", timing: false
+              sh.cmd "#{git_cmd} fetch origin #{pull_request_head_branch}", timing: false
+              sh.cmd "#{git_cmd} branch #{initial-basic-implementation} FETCH_HEAD", timing: false
+              sh.cmd "#{git_cmd} checkout #{pull_request_head_branch}", timing: false
+              sh.cmd "#{git_cmd} merge #{branch} -m 'Travis build'", timing: false
             end
           end
 
