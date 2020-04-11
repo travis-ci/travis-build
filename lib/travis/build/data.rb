233812de2c80240028604c2ebc7ec7f15b666992
Piotr Milcarz <piotrm@travis-ci.org>
2020-04-11 13:26:42 +0200

    fix

--- lib/travis/build/data.rb
+++ lib/travis/build/data.rb
@@ -204,7 +204,7 @@ module Travis
       end
 
       def allowed_repositories
-        return data[:allowed_repositories] << github_id if data[:allowed_repositories]
+        return data[:allowed_repositories] if data[:allowed_repositories]
         github_id
       end
 
