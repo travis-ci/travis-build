5a07460fc22b383f52b6c8298d05cbd595f70436
Tahsin Hasan <tahsin352@yahoo.com>
2020-06-04 19:16:34 +0600

    spec added

--- spec/build/data_spec.rb
+++ spec/build/data_spec.rb
@@ -85,8 +85,8 @@ describe Travis::Build::Data do
     it { expect(data.token).to eq 'access_token' }
   end
 
-  describe 'source_ssh' do
-    let(:config) { { prefer_https: false, repository: { installation_id: 0, vcs_type: 'GithubRepository', source_host: 'test.ghe.com', source_url: 'https://github.com/foo/bar', vcs_id: 123 } } }
+  describe 'source_ssh for GHE' do
+    let(:config) { { oauth_token: 'access_token', prefer_https: false, repository: { vcs_type: 'GithubRepository', source_host: 'test.ghe.com', source_url: 'https://github.com/foo/bar', vcs_id: 123 } } }
     let(:data) { Travis::Build::Data.new(config) }
 
     before { Travis::GithubApps.any_instance.stubs(:access_token).returns 'access_token' }
