a4c21d97190da2d8f692f870527d7626d7cb0582
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-10-22 10:49:49 -0400

    Add headings for setup steps

--- lib/travis/build/script/matlab.rb
+++ lib/travis/build/script/matlab.rb
@@ -30,9 +30,13 @@ module Travis
           end
 
           sh.fold 'matlab_install' do
+            sh.echo 'Installing MATLAB system dependencies', ansi: :yellow
+
             # Execute helper script to install runtime dependencies
             sh.raw "wget -qO- --retry-connrefused #{MATLAB_DEPS_LOCATION}" \
                   ' | sudo -E bash -s -- $TRAVIS_MATLAB_VERSION'
+
+            sh.echo 'Setting up MATLAB', ansi: :yellow
 
             # Invoke the ephemeral MATLAB installer that will make a MATLAB available
             # on the system PATH
