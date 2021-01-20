d6db0232b2076bedd07f6fd335bcb21996a0b56a
Ivan Pozdeev <vano@mail.mipt.ru>
2021-01-20 22:52:02 +0300

    Support Ubuntu Focal

--- lib/travis/build/script/csharp.rb
+++ lib/travis/build/script/csharp.rb
@@ -145,6 +145,9 @@ View valid versions of \"dotnet\" at https://docs.travis-ci.com/user/languages/c
               sh.elif '$(lsb_release -cs) = bionic' do
                 sh.cmd "sudo sh -c \"echo 'deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic main' > /etc/apt/sources.list.d/dotnet-official.list\"", assert: true
               end
+              sh.elif '$(lsb_release -cs) = focal' do
+                sh.cmd "sudo sh -c \"echo 'deb [arch=amd64] https://packages.microsoft.com/ubuntu/20.04/prod focal main' > /etc/apt/sources.list.d/dotnet-official.list\"", assert: true
+              end
               sh.else do
                 sh.failure "The version of this operating system is not supported by .NET Core. View valid versions at https://docs.travis-ci.com/user/languages/csharp/"
               end
