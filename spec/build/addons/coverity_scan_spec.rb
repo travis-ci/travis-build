b23267b7e064d2c161fd6af8565dd4ebe34c3878
Stanislav Kolotinskiy <stanislav@assembla.com>
2021-10-28 16:44:17 +0300

    Exit on Coverity scan failure
    Use provided shebang when eval'ing build script

--- spec/build/addons/coverity_scan_spec.rb
+++ spec/build/addons/coverity_scan_spec.rb
@@ -2,16 +2,14 @@ require 'spec_helper'
 
 describe Travis::Build::Addons::CoverityScan, :sexp do
   let(:script) { stub('script') }
-  let(:config) { {} }
+  let(:config) { { project: { name: 'test' } } }
   let(:data)   { payload_for(:push, :ruby, config: { addons: { coverity_scan: config } }) }
   let(:sh)     { Travis::Shell::Builder.new }
   let(:addon)  { described_class.new(script, sh, Travis::Build::Data.new(data), config) }
   subject      { sh.to_sexp }
   before       { addon.script }
 
-  # it_behaves_like 'compiled script' do
-  #   let(:code) { ['CODECLIMATE_REPO_TOKEN=1234'] }
-  # end
-
-  xit 'needs specs!'
+  it { is_expected.to include_sexp [:cmd, 'curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh | COVERITY_SCAN_PROJECT_NAME="$PROJECT_NAME" COVERITY_SCAN_NOTIFICATION_EMAIL="${COVERITY_SCAN_NOTIFICATION_EMAIL:-}" COVERITY_SCAN_BUILD_COMMAND="${COVERITY_SCAN_BUILD_COMMAND:-}" COVERITY_SCAN_BUILD_COMMAND_PREPEND="${COVERITY_SCAN_BUILD_COMMAND_PREPEND:-}" COVERITY_SCAN_BRANCH_PATTERN=${COVERITY_SCAN_BRANCH_PATTERN:-} /bin/sh', { echo: true }] }
+  it { is_expected.to include_sexp [:raw, 'result=$?'] }
+  it { is_expected.to include_sexp [:if, '$result -ne 0', [:then, [:cmds, [[:raw, "echo -e \"\e[33;1mSkipping build_coverity due to script error\e[0m\""], [:raw, 'exit 1']]]]] }
 end
