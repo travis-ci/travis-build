b2a6afd6b4bfa70383bd248d2f654382e16c9eb0
Arek Rus <arek@travis-ci.org>
2022-04-06 13:10:44 +0200

    use temurin jdk

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -5,7 +5,7 @@ travis_install_jdk() {
   version="$3"
 
   case "${TRAVIS_CPU_ARCH}" in
-  "arm64" | "s390x" | "ppc64le")
+  "arm64" | "s390x" | "ppc64le" | "amd64")
     travis_install_jdk_package "$version"
     ;;
   *)
@@ -52,8 +52,8 @@ travis_install_jdk_package() {
     if dpkg-query -l adoptopenjdk* >/dev/null 2>&1; then
       dpkg-query -l adoptopenjdk* | grep adoptopenjdk | awk '{print $2}' | xargs sudo dpkg -P
     fi
-    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
-    sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+    wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -
+    sudo add-apt-repository --yes https://packages.adoptium.net/artifactory/deb
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s "$PACKAGE"*
