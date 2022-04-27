42fccd77ed64f0a504e801d1e8ac28bc1555634e
AndriiMysko <usandrewmisko@gmail.com>
2022-04-27 16:25:35 +0300

    Fix tests

--- lib/travis/build/bash/travis_install_jdk.bash
+++ lib/travis/build/bash/travis_install_jdk.bash
@@ -51,15 +51,13 @@ travis_install_jdk_package() {
   if ! dpkg -s "$PACKAGE" >/dev/null 2>&1; then
     if dpkg-query -l temurin* >/dev/null 2>&1; then
       dpkg-query -l temurin* | grep temurin | awk '{print $2}' | xargs sudo dpkg -P
-    fi    
+    fi
     wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -
     sudo add-apt-repository --yes https://packages.adoptium.net/artifactory/deb
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     travis_cmd "export JAVA_HOME=/usr/lib/jvm/temurin-${JAVA_VERSION}-jdk-${TRAVIS_CPU_ARCH}" --echo
-    travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
-    echo $PATH
-    echo $JAVA_HOME
+    travis_cmd "export PATH=\"$JAVA_HOME/bin:$PATH\"" --echo
     sudo update-java-alternatives -s "$PACKAGE"*
   fi
 }
