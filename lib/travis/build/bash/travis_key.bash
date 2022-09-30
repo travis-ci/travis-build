ccd51dde87dec9fb7d908381ab03db7653cfdddc
Andrii Mysko <usandrewmisko@gmail.com>
2022-09-30 16:55:57 +0300

    Update travis_key.bash

--- lib/travis/build/bash/travis_key.bash
+++ lib/travis/build/bash/travis_key.bash
@@ -2,10 +2,10 @@ travis_key() {
   local var_name="TRAVIS_$1"
   if [[ ! -z ${!var_name+x} ]]
   then
-    echo ${!var_name} | base64 --decode > $2
-    chmod 0600 $2
+    echo "${!var_name}" | base64 --decode > "$2"
+    chmod 0600 "$2"
   else
-    echo "Required keys." . $1 . " was not found. Please verify your account settings or correct build configuration."
+    echo "Required keys " "$1" " was not found. Please verify your account settings or correct build configuration."
     exit 1
   fi
 }
