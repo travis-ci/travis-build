23394caec89bd0586a6d3042b61ec20d676dd274
gabriel-arc <gabriel@travis-ci.org>
2022-12-06 10:32:54 +0100

    shfmt fix

--- lib/travis/build/bash/travis_key.bash
+++ lib/travis/build/bash/travis_key.bash
@@ -1,8 +1,7 @@
 travis_key() {
   local var_name="TRAVIS_$1"
-  if [[ ! -z ${!var_name+x} ]]
-  then
-    echo "${!var_name}" | base64 --decode > "$2"
+  if [[ ! -z ${!var_name+x} ]]; then
+    echo "${!var_name}" | base64 --decode >"$2"
     chmod 0600 "$2"
   else
     echo "Required keys " "$1" " was not found. Please verify your account settings or correct build configuration."
