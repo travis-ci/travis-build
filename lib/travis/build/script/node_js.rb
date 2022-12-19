9db077736abda1bf6cfce272dd4b415c8e969df2
Qasim Abdullah <89213175+qasim-at-tci@users.noreply.github.com>
2022-12-19 17:05:28 +0500

    spin is no longer supported in npm v9
    https://github.blog/changelog/2022-10-24-npm-v9-0-0-released/#%E2%9A%A0%EF%B8%8F-notable-breaking-changes

--- lib/travis/build/script/node_js.rb
+++ lib/travis/build/script/node_js.rb
@@ -160,7 +160,7 @@ module Travis
           end
 
           def npm_disable_spinner
-            sh.cmd 'npm config set spin false', echo: false, timing: false
+            sh.cmd 'npm config set progress false', echo: false, timing: false
           end
 
           def npm_disable_strict_ssl
