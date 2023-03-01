932cc1739493780f31e1ff76ac38da0415128115
Maciej Kempin <makemp@users.noreply.github.com>
2023-03-01 16:46:27 +0000

    Prd gpu support (#2067)
    * tensor flow
    
    * blender
    
    * fixing aliases
    
    ---------
    
    Co-authored-by: Maciej Kempin <maciej.kempin@gmail.com>

---
+++ lib/travis/build/addons/tensor_flow.rb
@@ -0,0 +1,34 @@
+require 'shellwords'
+require 'travis/build/addons/base'
+
+module Travis
+  module Build
+    class Addons
+      class TensorFlow < Base
+        ALLOWED_VERSIONS = %w[2.11 1.15].freeze
+
+        def after_prepare
+          sh.fold 'tenser_flow' do
+            if version.nil?
+              sh.echo "Invalid version '#{raw_version}' given. Valid versions are: #{ALLOWED_VERSIONS.join(', ')}",
+                      ansi: :red
+              return
+            end
+            sh.echo "Installing TenserFlow version: #{version}", ansi: :yellow
+            sh.cmd "pip install 'tensorflow==#{version}' --force-reinstall", sudo: false
+          end
+        end
+
+        private
+
+        def raw_version
+          config.to_s.strip.shellescape
+        end
+
+        def version
+          ALLOWED_VERSIONS.include?(raw_version) ? raw_version : nil
+        end
+      end
+    end
+  end
+end
