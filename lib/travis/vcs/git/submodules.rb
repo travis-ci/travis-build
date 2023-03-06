29651c8dd9f4fb14a49f763f9a61860cbf3284d0
Qasim Abdullah <89213175+qasim-at-tci@users.noreply.github.com>
2023-03-06 19:34:34 +0500

    Quiet cloning the submodules
    Logscans mark submodules cloning SHAs output as high entropy strings, adding the `--quiet` flag would help suppress that behavior.

--- lib/travis/vcs/git/submodules.rb
+++ lib/travis/vcs/git/submodules.rb
@@ -8,7 +8,7 @@ module Travis
           sh.if '-f .gitmodules' do
             sh.fold 'git.submodule' do
               sh.file '~/.ssh/config', "Host github.com\n\tStrictHostKeyChecking no\n", append: true
-              sh.cmd "git submodule update --init --recursive #{update_args}".strip, assert: true, retry: true
+              sh.cmd "git submodule update --init --recursive --quiet #{update_args}".strip, assert: true, retry: true
             end
           end
         end
