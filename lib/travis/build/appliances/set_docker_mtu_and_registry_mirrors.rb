ca62053e9f55071f5589f977f0ada3b6d135e97b
GbArc <gabriel.arczynski@devtactics.net>
2023-05-31 12:46:34 +0200

    mtu changed to 1350

--- lib/travis/build/appliances/set_docker_mtu_and_registry_mirrors.rb
+++ lib/travis/build/appliances/set_docker_mtu_and_registry_mirrors.rb
@@ -16,11 +16,11 @@ module Travis
             sh.raw <<-EOF
 sudo test -f /etc/docker/daemon.json
 if [[ $? = 0 ]]; then
-  echo '[{"op":"add","path":"/mtu","value":1460}]' > mtu.jsonpatch
+  echo '[{"op":"add","path":"/mtu","value":1350}]' > mtu.jsonpatch
   sudo jsonpatch /etc/docker/daemon.json mtu.jsonpatch > daemon.json
   sudo mv daemon.json /etc/docker/daemon.json
 else
-  echo '{"mtu":1460}' | sudo tee /etc/docker/daemon.json > /dev/null
+  echo '{"mtu":1350}' | sudo tee /etc/docker/daemon.json > /dev/null
 fi
 
 if curl --connect-timeout 1 -fsSL -o /dev/null \
