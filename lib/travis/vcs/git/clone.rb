01f3fcbea2da2894400a8129fca1e681bc4a69b8
Arek Rus <57007917+r-arek@users.noreply.github.com>
2023-09-15 11:55:52 +0200

    Update clone.rb

--- lib/travis/vcs/git/clone.rb
+++ lib/travis/vcs/git/clone.rb
@@ -13,6 +13,8 @@ module Travis
               github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
               github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
               github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+              git-k8s-staging-frank.assembla.app ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDS7DhDYjc1FB5qAmC3FHgLv6TNPdLOBjqjFI6LfWiQ5rsyguqAThumA28aOSH0zgYRxitxKDHmEhhy85RVcj6mXRa8zsG91AF6AxDkAsC38U+vbWQkx2pIN2ne/JH0SsmL+gs6liS1ocMkwBcSeMYEarQ6m7/veknr0ZMiAvhqY3T40C9B4VC8TTJFVhLjW+hrG39zxmz44T8oxXgVKVslNLQA5lHaSMxvLBQ5NestipBQOrEfT6dXK9VfnDXQKpUlCwjYD/qmyPa2EF9F4oI4V64UehkLFxZsVUc3u0agdphXwl+95QdHkzsKA27fEMflD/ZUSEhD3gs5ArtgmmPh
+              git-k8s-staging-frank.assembla.app ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHJ4vnKVPkC8Krj4R5V07HLvzUsD2IDujxMQI+6D5G21sq+LAABEeQj+dRzC/OPYSIdzIEE/21fHJlInXDqujyQ=
             EOF
             clone_or_fetch
             sh.cd dir
