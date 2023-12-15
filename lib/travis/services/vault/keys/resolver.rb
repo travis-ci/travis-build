503973e898700b320e2b70253995b99f72f6a973
GbArc <gabriel.arczynski@devtactics.net>
2023-12-15 11:25:09 +0100

    vault secrets fetching update

--- lib/travis/services/vault/keys/resolver.rb
+++ lib/travis/services/vault/keys/resolver.rb
@@ -17,16 +17,23 @@ module Travis
         def call
           return if paths.blank?
 
+          namespace = nil
           vault_secrets = []
+          secrets = appliance.vault[:secrets]
+          namespace = secrets[:namespace].find { |el| el.is_a?(Hash) && el&.dig(:name) }&.dig(:name) if secrets.include?(:namespace)
 
           paths.each do |path|
-            secret_data = Keys.const_get(version.upcase).resolve(path, vault)
+            parts = path.split('/',2)
+            mount = parts&.first
+            path = parts&.last
+            secret_data = Keys.const_get(version.upcase).resolve(namespace, mount, path, vault)
             if secret_data.present?
               secret_name = path.split('/').last
               secret_data.each do |key, value|
                 env_name = key
                 env_name = [secret_name, env_name].join('_') if true # To-Do: Make the prepend customizable from .travis.yml
                 env_name = (path.split('/') << env_name).join('_') if false # To-Do: Make the prepend customizable from .travis.yml
+                env_name.gsub!(/[^0-9a-zA-Z]/,'_')
                 export(env_name.upcase, %("#{value}"), echo: false, secure: true)
                 vault_secrets << value
               end
