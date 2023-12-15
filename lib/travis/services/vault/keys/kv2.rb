b25b6862a3e3ffd0604b2174ecac7a172517a589
GbArc <gabriel.arczynski@devtactics.net>
2023-12-15 11:25:09 +0100

    vault secrets fetching update

--- lib/travis/services/vault/keys/kv2.rb
+++ lib/travis/services/vault/keys/kv2.rb
@@ -4,8 +4,8 @@ module Travis
   module Vault
     class Keys
       class KV2
-        def self.resolve(path, vault)
-          response = RestClient.get("#{vault[:api_url]}/v1/secret/data/#{path}", 'X-Vault-Token': vault[:token])
+        def self.resolve(namespace, mount ,path, vault)
+          response = RestClient.get("#{vault[:api_url]}/v1/#{mount}/data/#{path}", 'X-Vault-Token': vault[:token], 'X-Vault-Namespace': namespace ? namespace : "")
           JSON.parse(response.body).dig('data', 'data') if response.code == 200
         rescue RestClient::ExceptionWithResponse, SocketError
           nil
