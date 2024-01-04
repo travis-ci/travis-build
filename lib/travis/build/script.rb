6289df7e24412310677a31e6fb6bfeb2251bdc00
piccadilly circus <baize.aspect0t@icloud.com>
2024-01-04 11:24:03 +0500

    Adding debug log

--- lib/travis/build/script.rb
+++ lib/travis/build/script.rb
@@ -132,6 +132,7 @@ module Travis
       end
 
       def compile(ignore_taint = false)
+        raise 'oops'
         nodes = sexp
         Shell.generate(nodes, ignore_taint)
       rescue Travis::Shell::Generator::TaintedOutput => to
@@ -452,6 +453,7 @@ module Travis
         end
 
         def show_compile_error_msg(exception, event)
+          puts "Exception Occurred when trying to compile #{exception.class}. Message: #{exception.message}. Backtrace:  \n #{exception.backtrace.join("\n")}"
           @sh = Shell::Builder.new
           error_message_ary(exception, event).each { |line| sh.raw "echo -e \"\033[31;1m#{line}\033[0m\"" }
           sh.raw "exit 2"
