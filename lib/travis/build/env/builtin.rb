0ce8a79f257eaf27122815d5684c935733938cb2
Andrii Mysko <andrii.mysko@devtactics.net>
2024-05-08 09:48:35 +0300

    [BFSY-305] Use commit message from the job if available (#2114)
    * Use commit message from the job if available

--- lib/travis/build/env/builtin.rb
+++ lib/travis/build/env/builtin.rb
@@ -44,7 +44,7 @@ module Travis
               TRAVIS_JOB_WEB_URL:     "https://#{data[:host]}/#{repository[:slug]}/jobs/#{job[:id]}",
               TRAVIS_BRANCH:          job[:branch],
               TRAVIS_COMMIT:          job[:commit],
-              TRAVIS_COMMIT_MESSAGE: '$(test -d .git && git log --format=%B -n 1 | head -c 32768)',
+              TRAVIS_COMMIT_MESSAGE:  Shellwords.escape(job[:commit_message]) || '$(test -d .git && git log --format=%B -n 1 | head -c 32768)',
               TRAVIS_COMMIT_RANGE:    job[:commit_range],
               TRAVIS_REPO_SLUG:       repository[:slug],
               TRAVIS_OSX_IMAGE:       config[:osx_image],
