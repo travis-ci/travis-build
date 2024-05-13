e348eb9fd37a624d54f14b14a66b20732dfd870c
GbArc <gabriel.arczynski@devtactics.net>
2024-05-13 11:59:52 +0200

    support for ecdsa and eddsa

--- lib/travis/build/data/ssh_key.rb
+++ lib/travis/build/data/ssh_key.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'ssh_data'
 
 module Travis
   module Build
@@ -29,7 +30,27 @@ module Travis
             OpenSSL::Digest::MD5.new(public_ssh_rsa).hexdigest.scan(/../).join(':')
           end
         rescue OpenSSL::PKey::RSAError
+          @fingerprint ||= handle_nonrsa(value)
         end
+
+        def handle_nonrsa(key)
+          parsed_key = SSHData::PrivateKey.parse_openssh(key)
+          return nil unless parsed_key && parsed_key.length
+
+          bytes = if parsed_key[0]&.public_key.respond_to?(:public_key_bytes)
+                    parsed_key[0]&.public_key.public_key_bytes
+                  elsif parsed_key[0]&.public_key.respond_to?(:pk)
+                    parsed_key[0]&.public_key.pk
+                  else
+                    nil
+                  end
+          return nil unless bytes
+
+          OpenSSL::Digest::MD5.new(bytes).hexdigest.scan(/../).join(':')
+        rescue SSHData::DecodeError
+          nil
+        end
+
       end
     end
   end
