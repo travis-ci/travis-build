b5b59087dc9938741ef872f75a9555e0995c4b1b
Piotr Milcarz <piotr.milcarz@gmail.com>
2024-05-24 14:03:02 +0200

    debug stuff

--- lib/travis/build/stages/conditional.rb
+++ lib/travis/build/stages/conditional.rb
@@ -10,6 +10,7 @@ module Travis
           result = Custom.new(script, name).run
           unless result.empty?
             sh.if(condition) do
+              puts "result: #{result}"
               result
             end
           end
@@ -18,10 +19,12 @@ module Travis
         private
 
           def condition
+            puts "TRAVIS_TEST_RESULT: #{TRAVIS_TEST_RESULT}"
             "$TRAVIS_TEST_RESULT #{operator} 0"
           end
 
           def operator
+            puts "name: #{name}"
             name == :after_success ? '=' : '!='
           end
       end
