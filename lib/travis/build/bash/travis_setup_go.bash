a4b664b4b426b9df5726431fd4811d6c2c0c3f0f
Konrad Wrzos <konrad@travis-ci.org>
2024-10-04 17:38:33 +0200

    Adding default GO installation part

--- lib/travis/build/bash/travis_setup_go.bash
+++ lib/travis/build/bash/travis_setup_go.bash
@@ -12,15 +12,25 @@ travis_setup_go() {
     return 86
   fi
 
-  # shellcheck source=/dev/null
+https://go.dev/dl/go1.23.2.linux-arm64.tar.gz
 
-  travis_cmd "export GOPATH=\"${TRAVIS_HOME}/gopath\"" --echo
-  travis_cmd "export PATH=\"${TRAVIS_HOME}/gopath/bin:${PATH}\"" --echo
-  travis_cmd "export GO111MODULE=\"${GO111MODULE}\"" --echo
+  # Check if Go is installed
+  if ! command -v go &> /dev/null; then
+    echo "Go is not installed. Installing default Go..."
+    wget https://go.dev/dl/go1.23.2.linux-${TRAVIS_CPU_ARCH}.tar.gz
+    sudo tar -C /usr/local -xzf go1.23.2.linux-${TRAVIS_CPU_ARCH}.tar.gz
+    travis_cmd "PATH=${PATH}:/usr/local/go/bin"
+  else
+    echo "Go is already installed correctly. Moving on..."
+  fi
+
+  travis_cmd "export GOPATH=\"${TRAVIS_HOME}/gopath\""
+  travis_cmd "export PATH=\"${TRAVIS_HOME}/gopath/bin:${PATH}\""
+  travis_cmd "export GO111MODULE=\"${GO111MODULE}\"" 
 
   go install "golang.org/dl/go${go_version}@latest"
   "go${go_version}" download
   sudo ln -s "${TRAVIS_HOME}/gopath/bin/go${go_version}" "${TRAVIS_HOME}/gopath/bin/go"
-  travis_cmd "export GOROOT=$(go"${go_version}" env GOROOT)" --echo
-  travis_cmd "export PATH=${GOROOT}/bin:${PATH}" --echo
+  travis_cmd "export GOROOT=$(go"${go_version}" env GOROOT)"
+  travis_cmd "export PATH=${GOROOT}/bin:${PATH}"
 }
