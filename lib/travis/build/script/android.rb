b3b1453e94eb2e5ac31d0cc8b408ba038568b829
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-28 09:47:36 +0100

    android sdk path added

--- lib/travis/build/script/android.rb
+++ lib/travis/build/script/android.rb
@@ -11,16 +11,17 @@ module Travis
         def setup
           super
 
+          # Set Android SDK environment variables and export them
+          set_android_environment_variables
+
           if build_tools_desired.empty?
             sh.echo "No build-tools version specified in android.components. Consider adding one of the following:", ansi: :yellow
-            sh.cmd "sdkmanager --list | awk '/build-tools/ {print $1}'", echo: false, timing: false
+            sh.cmd "sdkmanager --list | grep 'build-tools' | cut -d'|' -f1", echo: false, timing: false
             sh.echo "The following versions are preinstalled:", ansi: :yellow
             sh.cmd "for v in $(ls #{android_sdk_build_tools_dir} | sort -r 2>/dev/null); do echo build-tools-$v; done; echo", echo: false, timing: false
           end
 
           install_sdk_components unless components.empty?
-
-          ensure_tools_bin_path
         end
 
         def script
@@ -40,14 +41,31 @@ module Travis
 
         private
 
+          def set_android_environment_variables
+            # Determine Android SDK home
+            android_home = ENV['ANDROID_HOME'] || '/usr/local/android-sdk'
+            sh.export 'ANDROID_HOME', android_home
+            
+            # Set path to sdkmanager based on specified structure
+            sdkmanager_path = "#{android_home}/android-sdk/cmdline-tools/latest/cmdline-tools/bin"
+            
+            # Add paths to PATH
+            sh.export 'PATH', "#{sdkmanager_path}:#{android_home}/android-sdk/tools:#{android_home}/android-sdk/tools/bin:#{android_home}/android-sdk/platform-tools:$PATH"
+            
+            # Create directory structure if it doesn't exist
+            sh.cmd "mkdir -p #{sdkmanager_path}", echo: false
+          end
+
           def install_sdk_components
             sh.fold 'android.install' do
               sh.echo 'Installing Android dependencies'
+              
+              # Accepting licenses preemptively - required for non-interactive installation
+              sh.cmd 'yes | sdkmanager --licenses >/dev/null || true', echo: true
+              
               components.each do |name|
                 sh.cmd install_sdk_component(name)
               end
-              # Accepting licenses - required for some installations
-              sh.cmd 'yes | sdkmanager --licenses', echo: true
             end
           end
 
@@ -55,10 +73,19 @@ module Travis
             # Convert name from format "build-tools-31.0.0" to "build-tools;31.0.0" for sdkmanager
             sdk_name = if name =~ /^build-tools-(.+)$/
                          "build-tools;#{$1}"
+                       elsif name =~ /^platform-tools-(.+)$/
+                         "platform-tools"
+                       elsif name =~ /^tools-(.+)$/
+                         "tools"
+                       elsif name =~ /^platforms-android-(.+)$/
+                         "platforms;android-#{$1}"
+                       elsif name =~ /^system-images-android-(.+)-(.+)-(.+)$/
+                         "system-images;android-#{$1};#{$2};#{$3}"
                        else
                          name
                        end
-            "sdkmanager \"#{sdk_name}\""
+            
+            "sdkmanager '#{sdk_name}' --verbose"
           end
 
           def build_tools_desired
@@ -69,25 +96,10 @@ module Travis
             end.compact
           end
 
-          def ensure_tools_bin_path
-            # Determine the path to cmdline-tools using ANDROID_HOME if available
-            tools_bin_path = if ENV['ANDROID_HOME']
-                               File.join(ENV['ANDROID_HOME'], 'cmdline-tools', 'latest', 'bin')
-                             else
-                               '/usr/local/android-sdk/android-sdk/cmdline-tools/latest/cmdline-tools/bin/sdkmanager'
-                             end
-            sh.if "$(echo :$PATH: | grep -v :#{tools_bin_path}:)" do
-              sh.export "PATH", "#{tools_bin_path}:$PATH"
-            end
-          end
-
           def android_sdk_build_tools_dir
-            # Determine build-tools directory based on ANDROID_HOME
-            if ENV['ANDROID_HOME']
-              File.join(ENV['ANDROID_HOME'], 'build-tools')
-            else
-              '/usr/local/android-sdk/build-tools'
-            end
+            # Get build-tools directory based on ANDROID_HOME with nested android-sdk directory
+            android_home = ENV['ANDROID_HOME'] || '/usr/local/android-sdk'
+            File.join(android_home, 'android-sdk', 'build-tools')
           end
 
           def components
@@ -101,7 +113,6 @@ module Travis
           def android_config
             config[:android] || {}
           end
-
       end
     end
   end
