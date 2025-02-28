91b5159b1e6de45df3453804c51589f6a1655f10
kamildevtactics <kamil.maludzinski@devtactics.net>
2025-02-28 16:11:29 +0100

    android.rb fix

--- lib/travis/build/script/android.rb
+++ lib/travis/build/script/android.rb
@@ -14,9 +14,6 @@ module Travis
           # Set Android SDK environment variables and export them
           set_android_environment_variables
 
-          # Ensure correct permissions for Android SDK
-          sh.cmd "chmod -R 777 /usr/local/android-sdk", echo: true
-
           if build_tools_desired.empty?
             sh.echo "No build-tools version specified in android.components. Consider adding one of the following:", ansi: :yellow
             sh.cmd "sdkmanager --list | grep 'build-tools' | cut -d'|' -f1", echo: false, timing: false
