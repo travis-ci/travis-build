6b5655ebd64839a843453ce98cc4bef8612efecc
GbArc <gabriel.arczynski@devtactics.net>
2025-05-14 08:30:04 +0200

    removed Done msg when creating image

--- lib/travis/build/bash/travis_custom_image.bash
+++ lib/travis/build/bash/travis_custom_image.bash
@@ -1,4 +1,6 @@
 travis_custom_image() {
+  : "${TRAVIS_TEST_RESULT:=86}"
   sudo mv /etc_backup /etc
   echo -e "\\nCreating custom image ${TRAVIS_CREATED_CUSTOM_IMAGE_NAME}"
+  travis_terminate "${TRAVIS_TEST_RESULT}"
 }
