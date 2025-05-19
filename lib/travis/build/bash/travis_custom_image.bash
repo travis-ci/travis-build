05301c11672b555276ca3e9ee04a458f70e15a57
GbArc <gabriel.arczynski@devtactics.net>
2025-05-19 13:44:24 +0200

    cleanup updates

--- lib/travis/build/bash/travis_custom_image.bash
+++ lib/travis/build/bash/travis_custom_image.bash
@@ -1,6 +1,7 @@
 travis_custom_image() {
   : "${TRAVIS_TEST_RESULT:=86}"
-  sudo mv /etc_backup /etc
+  sudo bash -c "rm -rf /etc && mv /etc_backup /etc && rm -f /etc/google_instance_id /etc/boto.cfg && rm -rf ${TRAVIS_BUILD_DIR}"
+  echo -e "\\nBuild done with result: ${TRAVIS_TEST_RESULT}"
   echo -e "\\nCreating custom image ${TRAVIS_CREATED_CUSTOM_IMAGE_NAME}"
   travis_terminate "${TRAVIS_TEST_RESULT}"
 }
