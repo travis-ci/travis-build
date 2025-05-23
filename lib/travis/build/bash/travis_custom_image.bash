d4aebe4377c9296f5983ad069e75612a8bdd9852
GbArc <gabriel.arczynski@devtactics.net>
2025-05-23 11:07:38 +0200

    image name and id in build details, build status check:

--- lib/travis/build/bash/travis_custom_image.bash
+++ lib/travis/build/bash/travis_custom_image.bash
@@ -2,6 +2,8 @@ travis_custom_image() {
   : "${TRAVIS_TEST_RESULT:=86}"
   sudo bash -c "rm -rf /etc && mv /etc_backup /etc && rm -f /etc/google_instance_id /etc/boto.cfg && rm -rf ${TRAVIS_BUILD_DIR}"
   echo -e "\\nBuild done with result: ${TRAVIS_TEST_RESULT}"
-  echo -e "\\nCreating custom image ${TRAVIS_CREATED_CUSTOM_IMAGE_NAME}"
+  if [ ${TRAVIS_TEST_RESULT} = 0 ] || [[ ${TRAVIS_ALLOW_FAILURE} == 'true' ]];then
+    echo -e "\\nCreating custom image ${TRAVIS_CREATED_CUSTOM_IMAGE_NAME}"
+  fi
   travis_terminate "${TRAVIS_TEST_RESULT}"
 }
