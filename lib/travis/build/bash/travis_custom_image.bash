569660e62c6934e39b7df4462e0a12fb98bd1bc7
Dominik <dominik.alberski@gmail.com>
2025-08-18 15:22:00 +0200

    PRD custom images (#2168)
    * custom images
    
    * ghc
    
    * removed endless loop (#2158)
    
    * corrected custom image name env
    
    * ghc
    
    * footer change, custom image cleanup update (#2161)
    
    * removed Done msg when creating image
    
    * cleanup updates
    
    * image name and id in build details, build status check:
    
    * fmt
    
    ---------
    
    Co-authored-by: GbArc <gabriel.arczynski@devtactics.net>
    
    ---------
    
    Co-authored-by: GbArc <gabriel.arczynski@devtactics.net>
    Co-authored-by: gbarc-dt <133209247+gbarc-dt@users.noreply.github.com>
    Co-authored-by: gabriel-arc <57348209+GbArc@users.noreply.github.com>

---
+++ lib/travis/build/bash/travis_custom_image.bash
@@ -0,0 +1,9 @@
+travis_custom_image() {
+  : "${TRAVIS_TEST_RESULT:=86}"
+  sudo bash -c "rm -rf /etc && mv /etc_backup /etc && rm -f /etc/google_instance_id /etc/boto.cfg && rm -rf ${TRAVIS_BUILD_DIR}"
+  echo -e "\\nBuild done with result: ${TRAVIS_TEST_RESULT}"
+  if [ ${TRAVIS_TEST_RESULT} = 0 ] || [[ ${TRAVIS_ALLOW_FAILURE} == 'true' ]]; then
+    echo -e "\\nCreating custom image ${TRAVIS_CREATED_CUSTOM_IMAGE_NAME}"
+  fi
+  travis_terminate "${TRAVIS_TEST_RESULT}"
+}
